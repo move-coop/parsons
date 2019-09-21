bba7a03db4b72af96cd4469ddb3cd16af00988fc
Justin Burchard <jburchard@gmail.com>
2019-09-20 23:55:45 -0500

    Finish locations.

--- parsons/ngpvan/locations.py
+++ parsons/ngpvan/locations.py
@@ -1,7 +1,8 @@
+import logging
 
+logger = logging.getLogger(__name__)
 
 class Locations(object):
-    """Class for '/locations' end points."""
 
     def __init__(self, van_connection):
 
@@ -9,11 +10,11 @@ class Locations(object):
 
     def get_locations(self, name=None):
         """
-        Get locations and optionally filter by name
+        Get locations.
 
         `Args:`
             name: str
-                Filters to Locations with names that contain the given input
+                Filter locations by name.
         `Returns:`
             Parsons Table
                 See :ref:`parsons-table` for output options.
@@ -28,11 +29,11 @@ class Locations(object):
 
     def get_location(self, location_id):
         """
-        Get a location object
+        Get a location.
 
         `Args:`
             location_id: int
-                A valid location id
+                The location id.
         `Returns:`
             Parsons Table
                 See :ref:`parsons-table` for output options.
@@ -43,23 +44,23 @@ class Locations(object):
         return self._unpack_loc(self.connection.request(url))
 
     def create_location(self, name, address_line1=None, address_line2=None, city=None,
-                        state=None, zipcode=None):
+                        state=None, zip_code=None):
         """
-        Find or Create a location. If location already exists, will return location id.
+        Find or create a location. If location already exists, will return location id.
 
         `Args:`
             name: str
-                A name for this Location, no longer than 50 characters
+                A name for this location, no longer than 50 characters.
             address_line1: str
-                Optional; First line of a Street Address
+                First line of a street address.
             address_line2: str
-                Optional; Second line of a Street Address
+                Second line of a street address.
             city: str
-                Optional; City or Town
+                City or town name.
             state: str
-                Optional; Two or three character State or Province code (e.g., MN, ON, NSW, etc.)
-            zipcode: str
-                Optional; ZIP, ZIP+4, Postal Code, Post code, etc.
+                Two or three character state or province code (e.g., MN, ON, NSW, etc.).
+            zip_code: str
+                ZIP, ZIP+4, Postal Code, Post code, etc.
             `Returns:`
                 A location id
         """
@@ -72,18 +73,18 @@ class Locations(object):
                         'addressLine2': address_line2,
                         'city': city,
                         'stateOrProvince': state,
-                        'zipOrPostalCode': zipcode
+                        'zipOrPostalCode': zip_code
                     }}
 
         return self.connection.request(url, req_type='POST', post_data=location, raw=True)
 
     def delete_location(self, location_id):
         """
-        Delete a location object
+        Delete a location.
 
         `Args:`
-            location_id:
-                A valid location id
+            location_id: int
+                The location id
         `Returns:`
             ``200: OK`` if successful and ``404 Not Found`` if location not found
         """

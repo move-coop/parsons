3b502a778c4deeb96530fc7f98f85c8c56829139
Justin Burchard <jburchard@gmail.com>
2019-10-07 13:44:57 -0500

    Codes.

--- parsons/ngpvan/codes.py
+++ parsons/ngpvan/codes.py
@@ -20,31 +20,27 @@ class Codes(object):
         `Args:`
             name : str
                 Filter by name of code.
-            supported_entities : str
+            supported_entities: str
                 Filter by supported entities.
-            parent_code_id : str
+            parent_code_id: str
                 Filter by parent code id.
-            code_type : str
+            code_type: str
                 Filter by code type.
         `Returns:`
             Parsons Table
                 See :ref:`parsons-table` for output options.
         """
 
-        args = {'name': name,
-                'supportedEntities': supported_entities,
-                'parentCodeId': parent_code_id,
-                'codeType': code_type,
-                '$top': 200
-                }
+        params = {'name': name,
+                  'supportedEntities': supported_entities,
+                  'parentCodeId': parent_code_id,
+                  'codeType': code_type,
+                  '$top': 200
+                  }
 
-        url = self.connection.uri + 'codes'
-
-        c = self.connection.request_paginate(url, args=args)
-        logger.debug(c)
-        logger.info(f'Found {c.num_rows} codes.')
-
-        return c
+        tbl = Table(self.connection.get_request('codes', params=params))
+        logger.info(f'Found {tbl.num_rows} codes.')
+        return tbl
 
     def get_code(self, code_id):
         """
@@ -71,18 +67,13 @@ class Codes(object):
         Get code types.
 
         `Returns:`
-            Parsons Table
-                See :ref:`parsons-table` for output options.
+            list
+                A list of code types.
         """
 
-        url = self.connection.uri + 'codeTypes'
-
-        ct = Table(petl.fromcolumns([self.connection.request(url, raw=True).json()],
-                                    header=['code_type']))
-        logger.debug(ct)
-        logger.info(f'Found {ct.num_rows} code types.')
-
-        return ct
+        lst = self.connection.get_request('codeTypes')
+        logger.info(f'Found {len(lst)} code types.')
+        return lst
 
     def create_code(self, name=None, parent_code_id=None, description=None,
                     code_type='SourceCode', supported_entities=None):
@@ -222,15 +213,10 @@ class Codes(object):
         Get code supported entities.
 
         `Returns:`
-            Parsons Table
-                See :ref:`parsons-table` for output options.
+            list
+                A list of code supported entities.
         """
 
-        url = self.connection.uri + 'codes/supportedEntities'
-
-        cse = Table(petl.fromcolumns([self.connection.request(url, raw=True).json()],
-                                     header=['supported_entities']))
-        logger.debug(cse)
-        logger.info(f'Found {cse.num_rows} code supported entities.')
-
-        return cse
+        lst = self.connection.get_request('codes/supportedEntities')
+        logger.info(f'Found {len(lst)} code supported entities.')
+        return lst

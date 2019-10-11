87d719dc386665a914e27e4582374ff22cc4dbc6
Justin Burchard <jburchard@gmail.com>
2019-10-10 20:53:09 -0500

    Move activist code method.

--- parsons/ngpvan/activist_codes.py
+++ parsons/ngpvan/activist_codes.py
@@ -7,10 +7,6 @@ logger = logging.getLogger(__name__)
 
 
 class ActivistCodes(object):
-    """
-        .. note::
-            To apply or remove activist codes, use the :meth:`toggle_activist_code` method.
-    """
 
     def __init__(self, van_connection):
 
@@ -43,4 +39,48 @@ class ActivistCodes(object):
 
         r = self.connection.get_request(f'activistCodes/{activist_code_id}')
         logger.info(f'Found activist code {activist_code_id}.')
+        return r
+
+    def toggle_activist_code(self, id, activist_code_id, action, id_type='vanid',
+                             result_code_id=None, contact_type_id=None, input_type_id=None,
+                             date_canvassed=None):
+        """
+        Apply or remove an activist code to or from a person.
+
+        `Args:`
+            id: str
+                A valid person id
+            activist_code_id: int
+                A valid activist code id
+            action: str
+                Either 'apply' or 'remove'
+            id_type: str
+                A known person identifier type available on this VAN instance
+                such as ``dwid``
+            result_code_id : int
+                `Optional`; Specifies the result code of the response. If
+                not included,responses must be specified. Conversely, if
+                responses are specified, result_code_id must be null. Valid ids
+                can be found by using the :meth:`get_canvass_responses_result_codes`
+            contact_type_id : int
+                `Optional`; A valid contact type id
+            input_type_id : int
+                `Optional`; Defaults to 11 (API Input)
+            date_canvassed : str
+                `Optional`; ISO 8601 formatted date. Defaults to todays date
+        Returns:
+            Status code 204
+        """
+
+        response = {"activistCodeId": activist_code_id,
+                    "action": self._action_parse(action),
+                    "type": "activistCode"}
+
+        r = self.apply_response(id, response, id_type, result_code_id=result_code_id,
+                                contact_type_id=contact_type_id, input_type_id=input_type_id,
+                                date_canvassed=date_canvassed)
+
+        logger.info(f'{id_type.upper()} {id} {action.capitalize()} ' +
+                    f'activist code {activist_code_id}')
+
         return r

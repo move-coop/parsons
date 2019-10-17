9074970484f0449376b2b4b5c7514f122b7658b3
Justin Burchard <jburchard@gmail.com>
2019-10-17 15:02:23 -0500

    Move SQ Apply.

--- parsons/ngpvan/people.py
+++ parsons/ngpvan/people.py
@@ -215,54 +215,14 @@ class People(object):
                 `Optional`; Defaults to 11 (API Input)
             date_canvassed : str
                 `Optional`; ISO 8601 formatted date. Defaults to todays date
+        `Returns:`
+            ``None``
         """
 
         logger.info(f'Applying result code {result_code_id} to {id_type} {id}.')
         self.apply_response(id, None, id_type=id_type, contact_type_id=contact_type_id,
                             input_type_id=input_type_id, date_canvassed=date_canvassed,
                             result_code_id=result_code_id)
-
-        return True
-
-    def apply_survey_response(self, id, survey_question_id, survey_response_id,
-                              id_type='vanid', result_code_id=None, contact_type_id=None,
-                              input_type_id=None, date_canvassed=None):
-        """
-        Apply a single survey response to a person.
-
-        `Args:`
-            id: str
-                A valid person id
-            survey_question_id: int
-                A valid survey question id
-            survey_response_id: int
-                A valid survey response id
-            id_type: str
-                A known person identifier type available on this VAN instance
-                such as ``dwid``
-            result_code_id : int
-                `Optional`; Specifies the result code of the response. If
-                not included,responses must be specified. Conversely, if
-                responses are specified, result_code_id must be null. Valid ids
-                can be found by using the :meth:`get_canvass_responses_result_codes`
-            contact_type_id : int
-                `Optional`; A valid contact type id
-            input_type_id : int
-                `Optional`; Defaults to 11 (API Input)
-            date_canvassed : str
-                `Optional`; ISO 8601 formatted date. Defaults to todays date
-        """
-
-        response = {"surveyQuestionId": survey_question_id,
-                    "surveyResponseId": survey_response_id,
-                    "type": "surveyResponse"}
-
-        logger.info(f'Applying survey question {survey_question_id} to {id_type} {id}')
-        self.apply_response(id, response, id_type, result_code_id=result_code_id,
-                            contact_type_id=contact_type_id, input_type_id=input_type_id,
-                            date_canvassed=date_canvassed)
-
-        return True
 
     def toggle_volunteer_action(self, id, volunteer_activity_id, action, id_type='vanid',
                                 result_code_id=None, contact_type_id=None, input_type_id=None,

a5ebe3803d2cd39be4306b615159816145697d01
Eliot Stone <eliot@movementcooperative.org>
2019-10-23 10:24:01 -0400

    fix up exception message from APIConnector
    This commit fixes the message passed to the `HTTPError` exception
    being raised when the
    `parsons.utilities.api_connector.APIConnector` validates an HTTP
    response. The new exception message includes the status code and
    reason (if provided), along with the body of the response if it
    is JSON data.

--- parsons/utilities/api_connector.py
+++ parsons/utilities/api_connector.py
@@ -130,11 +130,16 @@ class APIConnector(object):
 
         if resp.status_code >= 400:
 
+            if resp.reason:
+                message = f'HTTP error occurred ({resp.status_code}): {resp.reason}'
+            else:
+                message = f'HTTP error occurred ({resp.status_code})'
+
             # Some errors return JSONs with useful info about the error. Return it if exists.
             if self.json_check(resp):
-                raise HTTPError(f'HTTP error occurred: {HTTPError}, {resp.json()}')
+                raise HTTPError(f'{message}, json: {resp.json()}')
             else:
-                raise HTTPError(f'HTTP error occurred: {HTTPError}')
+                raise HTTPError(message)
 
     def data_parse(self, resp):
         """

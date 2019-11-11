71c23b4a90c6d7fd50b7d0c1b0b529eb57f2926e
jburchard <jburchard@gmail.com>
2019-11-11 12:42:56 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def main():
 
     setup(
         name="parsons",
-        version='0.7.0',
+        version='0.8.0',
         author="The Movement Cooperative",
         author_email="info@movementcooperative.org",
         url='https://github.com/movementcoop/parsons',

8527bcd9a39046dfd1adb0bd89e1a3d71be660e2
dekedor <the.kevin.osullivan@gmail.com>
2020-01-26 16:40:31 -0800

    Linted code and removed unnecessary test script.

--- parsons/bill_com/bill_com.py
+++ parsons/bill_com/bill_com.py
@@ -1,5 +1,6 @@
 import requests
-import urllib.parse as parse
+import json
+
 
 class BillCom(object):
     """
@@ -26,37 +27,42 @@ class BillCom(object):
             "orgId": orgId,
             "devKey": devKey
         }
-        response = requests.post(url="%sLogin.json" % endPointUrl, data=params, headers=self.headers)
+        response = requests.post(url="%sLogin.json" % endPointUrl,
+                                 data=params,
+                                 headers=self.headers)
         self.devKey = devKey
         self.endPointUrl = endPointUrl
         self.sessionId = response.json()['response_data']['sessionId']
-        
+
     def getPayload(self, data):
         """
         `Args:`
             data: dict
-                A dictionary containing the payload to be sent in the request. The devKey and sessionId
-                should not be included as they are dealt with separately.
-        
+                A dictionary containing the payload to be sent in the request.
+                The devKey and sessionId should not be included as they are
+                dealt with separately.
+
         `Returns:`
-            A dictionary of the payload to be sent in the request with the devKey and sessionId added.
+            A dictionary of the payload to be sent in the request with the
+            devKey and sessionId added.
         """
         return {"devKey": self.devKey,
                 "sessionId": self.sessionId,
                 "data": json.dumps(data)}
-    
+
     def postRequest(self, data, action, objectId):
         """
         `Args:`
             data: dict
-                A dictionary containing the payload to be sent in the request. The devKey and sessionId
-                should not be included as they are dealt with separately.
+                A dictionary containing the payload to be sent in the request.
+                The devKey and sessionId should not be included as they are
+                dealt with separately.
             action: str
                 The action to be taken on the given object.
                 Possible values are "List", "Read", "Create", and "Send".
             objectId: str
                 The id of the object. Possible values are "User", "Customer", and "Invoice".
-        
+
         `Returns:`
             A dictionary containing the JSON response from the post request.
         """
@@ -69,13 +75,14 @@ class BillCom(object):
             url = "%s%s/%s.json" % (self.endPointUrl, action, objectId)
         response = requests.post(url=url, data=payload, headers=self.headers)
         return response.json()
-    
+
     def getRequestResponse(self, data, action, objectId, field="response_data"):
         """
         `Args:`
             data: dict
-                A dictionary containing the payload to be sent in the request. The devKey and sessionId
-                should not be included as they are dealt with separately.
+                A dictionary containing the payload to be sent in the request.
+                The devKey and sessionId should not be included as they are
+                dealt with separately.
             action: str
                 The action to be taken on the given object.
                 Possible values are "List", "Read", "Create", and "Send".
@@ -83,12 +90,12 @@ class BillCom(object):
                 The id of the object. Possible values are "User", "Customer", and "Invoice".
             field: str
                 The JSON field where the response data is stored. Defaults to "response_data".
-        
+
         `Returns:`
             A dictionary containing the choosen field from the JSON response from the post request.
         """
         return self.postRequest(data, action, objectId)[field]
-        
+
     def getUserList(self, startUser=0, maxUser=999):
         """
         `Args:`
@@ -96,16 +103,16 @@ class BillCom(object):
                 The index of first user to return
             maxUser: str
                 The index of the max user to return
-        
+
         `Returns:`
             A list of dictionaries of user information for every user from startUser to maxUser.
         """
         data={
-           "start" : startUser,
-           "max" : maxUser,
+           "start": startUser,
+           "max": maxUser,
         }
         return self.getRequestResponse(data, "List", "User")
-    
+
     def getCustomerList(self, startCustomer=0, maxCustomer=999):
         """
         `Args:`
@@ -113,22 +120,23 @@ class BillCom(object):
                 The index of first customer to return
             maxUser: str
                 The index of the max customer to return
-        
+
         `Returns:`
-            A list of dictionaries of customer information for every user from startCustomer to maxCustomer.
+            A list of dictionaries of customer information for
+            every user from startCustomer to maxCustomer.
         """
         data={
-           "start" : startCustomer,
-           "max" : maxCustomer,
+           "start": startCustomer,
+           "max": maxCustomer,
         }
         return self.getRequestResponse(data, "List", "Customer")
-    
+
     def readCustomer(self, customerId):
         """
         `Args:`
             customerId: str
                 The id of the customer to query
-        
+
         `Returns:`
             A dictionary of the customer's information.
         """
@@ -136,21 +144,21 @@ class BillCom(object):
             'id': customerId
         }
         return self.getRequestResponse(data, "Read", "Customer")
-    
+
     def readInvoice(self, invoiceId):
         """
         `Args:`
             invoiceId: str
                 The id of the invoice to query
-        
+
         `Returns:`
             A dictionary of the invoice information.
         """
         data={
-           "id" : invoiceId
+           "id": invoiceId
         }
         return self.getRequestResponse(data, "Read", "invoice")
-    
+
     def checkCustomer(self, customer1, customer2):
         """
         `Args:`
@@ -158,7 +166,7 @@ class BillCom(object):
                 A dictionary of data on customer1
             customer2: dict
                 A dictionary of data on customer2
-        
+
         `Returns:`
             True if either
                 1. customer1 and customer2 have the same id
@@ -174,16 +182,17 @@ class BillCom(object):
             if customer1[key] != customer2[key]:
                 return False
         return True
-    
+
     def createCustomer(self, customer):
         """
         `Args:`
             customer: dict
                 A dictionary of data on the customer
-        
+
         `Returns:`
             A dictionary of the customer's information including an id.
-            If the customer already exists, this function will not create a new id and instead use the existing id.
+            If the customer already exists, this function will not
+            create a new id and instead use the existing id.
         """
         # check if customer already exists
         customerList = self.getCustomerList()
@@ -192,16 +201,16 @@ class BillCom(object):
                 return existingCustomer
         # customer doesn't exist, create
         data={
-            "obj" : customer
+            "obj": customer
         }
         return self.getRequestResponse(data, "Create", "Customer")
-    
+
     def createInvoice(self, invoice):
         """
         `Args:`
             invoice: dict
                 A dictionary of data on the invoice
-        
+
         `Returns:`
             A dictionary of the invoice's information including an id.
         """
@@ -209,7 +218,7 @@ class BillCom(object):
             "obj": invoice
         }
         return self.getRequestResponse(data, "Create", "Invoice")
-    
+
     def sendInvoice(self, invoiceId, fromUserId, toEmailAddresses, messageSubject, messageBody):
         """
         `Args:`
@@ -223,19 +232,19 @@ class BillCom(object):
                 The subject of the email to send to the customer
             messageBody:
                 The boyd of the email to send to the customer
-        
+
         `Returns:`
             A dictionary of the sent invoice.
         """
         data={
-          "invoiceId" : invoiceId,
-          "headers" : {
-            "fromUserId" : fromUserId,
-            "toEmailAddresses" : toEmailAddresses,
-            "subject" : messageSubject
+          "invoiceId": invoiceId,
+          "headers": {
+            "fromUserId": fromUserId,
+            "toEmailAddresses": toEmailAddresses,
+            "subject": messageSubject
           },
-          "content" : {
-            "body" : messageBody
+          "content": {
+            "body": messageBody
           }
         }
         return self.getRequestResponse(data, "Send", "Invoice")

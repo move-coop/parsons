c372825a785a15cd994d5a3da26190f7c674ea22
jburchard <jburchard@gmail.com>
2020-02-20 07:25:06 -0600

    Fix issues.

--- test/test_databases/test_dbsync.py
+++ test/test_databases/test_dbsync.py
@@ -7,6 +7,9 @@ _dir = os.path.dirname(__file__)
 
 TEMP_SCHEMA = 'parsons_test'
 
+os.environ["PGUSER"] = 'justinburchard'
+os.environ["PGPASS"] = 'justinburchard'
+os.environ["PGHOST"] = 'localhost'
 
 # These tests interact directly with the Postgres database. In order to run, set the
 # env to LIVE_TEST='TRUE'.

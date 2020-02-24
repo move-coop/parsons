20987bb0a640afc1a5c93b502046150693fa4e31
jburchard <jburchard@gmail.com>
2020-02-24 09:22:30 -0600

    Database Sync: Add Redshift Support (#163)
    * Initial push.
    
    * Work on incremental...
    
    * Little bug.
    
    * First tests.
    
    * Finish incremental sync tests.
    
    * Doc edits.
    
    * Test DB.
    
    * Fix init.
    
    * Fix testing name class.
    
    * Formatting.
    
    * Add support for table create in incremental sync.
    
    * Update documentation.
    
    * Lint.
    
    * Add Redshift support for DB Sync.
    
    * Fix tests.
    
    * Fix docs.
    
    * Doc.

--- test/test_databases/test_dbsync.py
+++ test/test_databases/test_dbsync.py
@@ -1,4 +1,4 @@
-from parsons import Postgres, DBSync, Table
+from parsons import Postgres, DBSync, Table, Redshift
 from test.utils import assert_matching_tables
 import unittest
 import os
@@ -16,24 +16,24 @@ class TestPostgresDBSync(unittest.TestCase):
     def setUp(self):
 
         self.temp_schema = TEMP_SCHEMA
-        self.pg = Postgres()
+        self.db = Postgres()
 
         # Create a schema.
         setup_sql = f"""
                      DROP SCHEMA IF EXISTS {self.temp_schema} CASCADE;
                      CREATE SCHEMA {self.temp_schema};
                      """
-        self.pg.query(setup_sql)
+        self.db.query(setup_sql)
 
         # Load dummy data to parsons tables
         self.table1 = Table.from_csv(f'{_dir}/test_data/sample_table_1.csv')
         self.table2 = Table.from_csv(f'{_dir}/test_data/sample_table_2.csv')
 
         # Create source table
-        self.pg.copy(self.table1, f'{self.temp_schema}.source')
+        self.db.copy(self.table1, f'{self.temp_schema}.source')
 
         # Create DB Sync object
-        self.db_sync = DBSync(self.pg, self.pg)
+        self.db_sync = DBSync(self.db, self.db)
 
     def tearDown(self):
         # Drop the view, the table and the schema
@@ -41,33 +41,33 @@ class TestPostgresDBSync(unittest.TestCase):
         teardown_sql = f"""
                        DROP SCHEMA IF EXISTS {self.temp_schema} CASCADE;
                        """
-        self.pg.query(teardown_sql)
+        self.db.query(teardown_sql)
 
     def test_table_sync_full_drop(self):
         # Test a db sync with drop.
 
         self.db_sync.table_sync_full(f'{self.temp_schema}.source',
-        	                         f'{self.temp_schema}.destination')
+                                     f'{self.temp_schema}.destination')
 
-        source = self.pg.query(f"SELECT * FROM {self.temp_schema}.source")
-        destination = self.pg.query(f"SELECT * FROM {self.temp_schema}.destination")
+        source = self.db.query(f"SELECT * FROM {self.temp_schema}.source")
+        destination = self.db.query(f"SELECT * FROM {self.temp_schema}.destination")
         assert_matching_tables(source, destination)
 
     def test_table_sync_full_truncate(self):
         # Test a db sync with truncate.
 
         self.db_sync.table_sync_full(f'{self.temp_schema}.source',
-        	                          f'{self.temp_schema}.destination',
-        	                         if_exists='truncate')
-        source = self.pg.query(f"SELECT * FROM {self.temp_schema}.source")
-        destination = self.pg.query(f"SELECT * FROM {self.temp_schema}.destination")
+                                      f'{self.temp_schema}.destination',
+                                     if_exists='truncate')
+        source = self.db.query(f"SELECT * FROM {self.temp_schema}.source")
+        destination = self.db.query(f"SELECT * FROM {self.temp_schema}.destination")
         assert_matching_tables(source, destination)
 
     def test_table_sync_full_empty_table(self):
         # Test a full sync of a table when the source table is empty.
 
         # Empty the source table
-        self.pg.query(f"TRUNCATE {self.temp_schema}.source")
+        self.db.query(f"TRUNCATE {self.temp_schema}.source")
 
         # Attempt to sync.
         self.db_sync.table_sync_full(f'{self.temp_schema}.source',
@@ -81,55 +81,83 @@ class TestPostgresDBSync(unittest.TestCase):
         self.db_sync.table_sync_full(f'{self.temp_schema}.source',
                                      f'{self.temp_schema}.destination')
 
-        source = self.pg.query(f"SELECT * FROM {self.temp_schema}.source")
-        destination = self.pg.query(f"SELECT * FROM {self.temp_schema}.destination")
+        source = self.db.query(f"SELECT * FROM {self.temp_schema}.source")
+        destination = self.db.query(f"SELECT * FROM {self.temp_schema}.destination")
         assert_matching_tables(source, destination)
 
     def test_table_sync_incremental(self):
         # Test that incremental sync
 
-        self.pg.copy(self.table1, f'{self.temp_schema}.destination')
-        self.pg.copy(self.table2, f'{self.temp_schema}.source', if_exists='append')
+        self.db.copy(self.table1, f'{self.temp_schema}.destination')
+        self.db.copy(self.table2, f'{self.temp_schema}.source', if_exists='append')
         self.db_sync.table_sync_incremental(f'{self.temp_schema}.source',
                                             f'{self.temp_schema}.destination',
                                             'pk')
 
-        count1 = self.pg.query(f"SELECT * FROM {self.temp_schema}.source")
-        count2 = self.pg.query(f"SELECT * FROM {self.temp_schema}.destination")
+        count1 = self.db.query(f"SELECT * FROM {self.temp_schema}.source")
+        count2 = self.db.query(f"SELECT * FROM {self.temp_schema}.destination")
         assert_matching_tables(count1, count2)
 
     def test_table_sync_incremental_chunk(self):
         # Test chunking of incremental sync.
 
         self.db_sync.chunk_size = 10
-        self.pg.copy(self.table1, f'{self.temp_schema}.destination')
-        self.pg.copy(self.table2, f'{self.temp_schema}.source', if_exists='append')
+        self.db.copy(self.table1, f'{self.temp_schema}.destination')
+        self.db.copy(self.table2, f'{self.temp_schema}.source', if_exists='append')
         self.db_sync.table_sync_incremental(f'{self.temp_schema}.source',
                                             f'{self.temp_schema}.destination',
                                             'pk')
 
-        count1 = self.pg.query(f"SELECT * FROM {self.temp_schema}.source")
-        count2 = self.pg.query(f"SELECT * FROM {self.temp_schema}.destination")
+        count1 = self.db.query(f"SELECT * FROM {self.temp_schema}.source")
+        count2 = self.db.query(f"SELECT * FROM {self.temp_schema}.destination")
         assert_matching_tables(count1, count2)
 
     def test_table_sync_incremental_create_destination_table(self):
         # Test that an incremental sync works if the destination table does not exist.
-        
+
         self.db_sync.table_sync_incremental(f'{self.temp_schema}.source',
                                             f'{self.temp_schema}.destination',
                                             'pk')
 
-        count1 = self.pg.query(f"SELECT * FROM {self.temp_schema}.source")
-        count2 = self.pg.query(f"SELECT * FROM {self.temp_schema}.destination")
+        count1 = self.db.query(f"SELECT * FROM {self.temp_schema}.source")
+        count2 = self.db.query(f"SELECT * FROM {self.temp_schema}.destination")
         assert_matching_tables(count1, count2)
 
     def test_table_sync_incremental_empty_table(self):
         # Test an incremental sync of a table when the source table is empty.
 
         # Empty the source table
-        self.pg.query(f"TRUNCATE {self.temp_schema}.source")
+        self.db.query(f"TRUNCATE {self.temp_schema}.source")
 
         # Attempt to sync.
         self.db_sync.table_sync_incremental(f'{self.temp_schema}.source',
                                             f'{self.temp_schema}.destination',
                                             'pk')
+
+# These tests interact directly with the Postgres database. In order to run, set the
+# env to LIVE_TEST='TRUE'.
+@unittest.skipIf(not os.environ.get('LIVE_TEST'), 'Skipping because not running live test')
+class TestRedshiftDBSync(TestPostgresDBSync):
+    """ This test inherits all of the tests from the Postgres test. """
+
+    def setUp(self):
+
+        self.temp_schema = TEMP_SCHEMA
+        self.db = Redshift()
+
+        # Create a schema.
+        setup_sql = f"""
+                     DROP SCHEMA IF EXISTS {self.temp_schema} CASCADE;
+                     CREATE SCHEMA {self.temp_schema};
+                     """
+        self.db.query(setup_sql)
+
+        # Load dummy data to parsons tables
+        self.table1 = Table.from_csv(f'{_dir}/test_data/sample_table_1.csv')
+        self.table2 = Table.from_csv(f'{_dir}/test_data/sample_table_2.csv')
+
+        # Create source table
+        self.db.copy(self.table1, f'{self.temp_schema}.source')
+
+        # Create DB Sync object
+        self.db_sync = DBSync(self.db, self.db)

d44a47f74f8894035d816e73febf06e07d2c3428
Schuyler Duveen <sky@skyb.us>
2020-03-23 00:06:30 -0400

    actionkit bulk_user_upload with user_fields_only optimization (#188)
    * actionkit bulk_user_upload with user_fields_only optimization
    
    * review feedback: set defaults to actual bools and doc the fast-processing

--- test/test_action_kit.py
+++ test/test_action_kit.py
@@ -279,11 +279,26 @@ class TestActionKit(unittest.TestCase):
         type(resp_mock.post()).status_code = mock.PropertyMock(return_value=201)
         self.actionkit._conn = lambda self: resp_mock
         self.actionkit.bulk_upload_table(
+            Table([('user_id', 'user_customfield1', 'action_foo'), (5, 'yes', '123 Main St')]),
+            'fake_page')
+        self.assertEqual(resp_mock.post.call_count, 2)
+        name, args, kwargs = resp_mock.method_calls[1]
+        self.assertEqual(kwargs['data'],
+                         {'page': 'fake_page', 'autocreate_user_fields': 0, 'user_fields_only': 0})
+        upload_data = gzip.open(kwargs['files']['upload']).read()
+        self.assertEqual(upload_data.decode(),
+                         'user_id,user_customfield1,action_foo\r\n5,yes,123 Main St\r\n')
+
+    def test_bulk_upload_table_userfields(self):
+        resp_mock = mock.MagicMock()
+        type(resp_mock.post()).status_code = mock.PropertyMock(return_value=201)
+        self.actionkit._conn = lambda self: resp_mock
+        self.actionkit.bulk_upload_table(
             Table([('user_id', 'user_customfield1'), (5, 'yes')]),
             'fake_page')
         self.assertEqual(resp_mock.post.call_count, 2)
         name, args, kwargs = resp_mock.method_calls[1]
-        self.assertEqual(kwargs['data'], {'page': 'fake_page', 'autocreate_user_fields': 0})
-        upload_data = gzip.open(kwargs['files']['upload']).read()
-        self.assertEqual(upload_data.decode(),
+        self.assertEqual(kwargs['data'],
+                         {'page': 'fake_page', 'autocreate_user_fields': 0, 'user_fields_only': 1})
+        self.assertEqual(kwargs['files']['upload'].read().decode(),
                          'user_id,user_customfield1\r\n5,yes\r\n')

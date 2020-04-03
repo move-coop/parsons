affe7bbf4367f70dd68aaa7daf86d48453e48cf9
Schuyler Duveen <sky@skyb.us>
2020-04-03 14:26:02 -0400

    aws.distribute_task: better filename

--- parsons/aws/__init__.py
+++ parsons/aws/__init__.py
@@ -1,5 +1,5 @@
 from parsons.aws.s3 import S3
-from parsons.aws.distribute_s3_lambda import distribute_task
+from parsons.aws.lambda_distribute import distribute_task
 from parsons.aws.aws_async import event_command
 
 __all__ = [
--- test/test_aws_async.py
+++ test/test_aws_async.py
@@ -3,7 +3,7 @@ from test.utils import assert_matching_tables
 
 from parsons.etl.table import Table
 from parsons.aws.aws_async import import_and_get_task, get_func_task_path
-from parsons.aws.distribute_s3_lambda import distribute_task
+from parsons.aws.lambda_distribute import distribute_task
 
 
 def fake_func(arg1, arg2, kwarg1=None, kwarg2=56):

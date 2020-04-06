676df74f27ec5166a99660572d980dc75626dcaa
Kevin Porras <kporras07@gmail.com>
2020-04-06 15:43:30 -0600

    Fix assert called_with calls and other minor fixes.

--- test/test_newmode/test_newmode.py
+++ test/test_newmode/test_newmode.py
@@ -30,43 +30,57 @@ class TestNewmode(unittest.TestCase):
         }
 
         self.nm.client.getAction.return_value = {
-            "required_fields": [
+            'required_fields': [
                 {
-                    "key": "first_name",
-                    "name": "First Name",
-                    "type": "textfield",
-                    "value": ""
+                    'key': 'first_name',
+                    'name': 'First Name',
+                    'type': 'textfield',
+                    'value': ''
                 }
             ]
         }
 
         self.nm.client.lookupTargets.return_value = {
-            "0": {
-                "unique_id": "TESTMODE-uniqueid",
-                "full_name": "John Doe"
+            '0': {
+                'unique_id': 'TESTMODE-uniqueid',
+                'full_name': 'John Doe'
             }
+        }
+
+        self.nm.client.runAction.return_value = {
+            'sid': 1
         }
 
     def test_get_tools(self):
         args = {}
         response = self.nm.get_tools(args)
-        assert self.nm.client.getTools.called_with(args)
+        self.nm.client.getTools.assert_called_with(params=args)
         self.assertEqual(response[0]['title'], 'Tool 1')
 
     def test_get_tool(self):
         id = 1
         response = self.nm.get_tool(id)
-        assert self.nm.client.getTool.called_with(id)
+        self.nm.client.getTool.assert_called_with(id, params={})
         self.assertEqual(response[0]['name'], 'Tool 1')
 
     def test_lookup_targets(self):
         id = 1
         response = self.nm.lookup_targets(id)
-        assert self.nm.client.lookupTargets.called_with(id)
+        self.nm.client.lookupTargets.assert_called_with(id, None, params={})
         self.assertEqual(response[0]['full_name'], 'John Doe')
 
     def test_get_action(self):
         id = 1
         response = self.nm.get_action(id)
-        assert self.nm.client.getAction.called_with(id)
+        self.nm.client.getAction.assert_called_with(id, params={})
         self.assertEqual(response[0]['required_fields'][0]['key'], 'first_name')
+
+    def test_run_action(self):
+        id = 1
+        payload = {
+            'email': 'john.doe@example.com',
+            'first_name': 'John',
+        }
+        response = self.nm.run_action(id, payload)
+        self.nm.client.runAction.assert_called_with(id, payload, params={})
+        self.assertEqual(response, 1)

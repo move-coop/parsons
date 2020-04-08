1849bad03657f5182be831d757f37e59d5616f81
Soren Spicknall <soren@spicknall.us>
2020-04-08 07:53:52 -0500

    Improve template table schema use, add testing for Redshift upsert (#206)
    * Improve idiomaticity of template table schema use, add testing

--- test/test_redshift.py
+++ test/test_redshift.py
@@ -339,6 +339,20 @@ class TestRedshiftDB(unittest.TestCase):
         self.assertRaises(ValueError, self.rs.upsert, upsert_tbl, f'{self.temp_schema}.test_copy',
                           ['ID', 'name'])
 
+        self.rs.query(f'truncate table {self.temp_schema}.test_copy')
+
+        # Run upsert with nonmatching datatypes
+        upsert_tbl = Table([['id', 'name'], [3, 600],
+                            [6, 9999]])
+        self.rs.upsert(upsert_tbl, f'{self.temp_schema}.test_copy', 'ID')
+
+        # Make sure our table looks like we expect
+        expected_tbl = Table([['id', 'name'],
+                              [3, '600'],
+                              [6, '9999']])
+        updated_tbl = self.rs.query(f'select * from {self.temp_schema}.test_copy order by id;')
+        assert_matching_tables(expected_tbl, updated_tbl)
+
     def test_unload(self):
 
         # Copy a table to Redshift

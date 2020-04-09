78ecd0c2e349c50387584c5c6e618879bef37b34
Kevin Porras <kporras07@gmail.com>
2020-04-09 13:33:49 -0600

    Move docblocks after method signature.

--- parsons/newmode/newmode.py
+++ parsons/newmode/newmode.py
@@ -7,22 +7,22 @@ logger = logging.getLogger(__name__)
 
 
 class Newmode:
-    """
-    Args:
-        api_user: str
-            The Newmode api user. Not required if ``NEWMODE_API_USER`` env variable is
-            passed.
-        api_password: str
-            The Newmode api password. Not required if ``NEWMODE_API_PASSWORD`` env variable is
-            passed.
-        api_version: str
-            The Newmode api version. Defaults to "v1.0" or the value of ``NEWMODE_API_VERSION``
-            env variable.
-    Returns:
-        Newmode class
-    """
-    def __init__(self, api_user=None, api_password=None, api_version=None):
 
+    def __init__(self, api_user=None, api_password=None, api_version=None):
+        """
+        Args:
+            api_user: str
+                The Newmode api user. Not required if ``NEWMODE_API_USER`` env variable is
+                passed.
+            api_password: str
+                The Newmode api password. Not required if ``NEWMODE_API_PASSWORD`` env variable is
+                passed.
+            api_version: str
+                The Newmode api version. Defaults to "v1.0" or the value of ``NEWMODE_API_VERSION``
+                env variable.
+        Returns:
+            Newmode class
+        """
         self.api_user = check_env.check('NEWMODE_API_USER', api_user)
         self.api_password = check_env.check('NEWMODE_API_PASSWORD', api_password)
 
@@ -43,15 +43,15 @@ class Newmode:
 
         return table
 
-    """
-    Get existing tools.
-    Args:
-        params:
-            Extra parameters sent to New/Mode library.
-    Returns:
-        Tools information as table.
-    """
     def get_tools(self, params={}):
+        """
+        Get existing tools.
+        Args:
+            params:
+                Extra parameters sent to New/Mode library.
+        Returns:
+            Tools information as table.
+        """
         tools = self.client.getTools(params=params)
         if tools:
             return self.convert_to_table(tools)
@@ -59,17 +59,17 @@ class Newmode:
             logging.warning("Empty tools returned")
             return self.convert_to_table([])
 
-    """
-    Get specific tool.
-    Args:
-        tool_id:
-            The id of the tool to return.
-        params:
-            Extra parameters sent to New/Mode library.
-    Returns:
-        Tool information.
-    """
     def get_tool(self, tool_id, params={}):
+        """
+        Get specific tool.
+        Args:
+            tool_id:
+                The id of the tool to return.
+            params:
+                Extra parameters sent to New/Mode library.
+        Returns:
+            Tool information.
+        """
         tool = self.client.getTool(tool_id, params=params)
         if tool:
             return tool
@@ -77,24 +77,24 @@ class Newmode:
             logging.warning("Empty tool returned")
             return None
 
-    """
-    Lookup targets for a given tool
-    Args:
-        tool_id:
-            The tool to lookup targets.
-        search:
-            The search criteria. It could be:
-            - Empty: If empty, return custom targets associated to the tool.
-            - Postal code: Return targets matched by postal code.
-            - Lat/Long: Latitude and Longitude pair separated by '::'.
-              Ex. 45.451596::-73.59912099999997. It will return targets
-              matched for those coordinates.
-            - Search term: For your csv tools, this will return targets
-              matched by given valid search term.
-    Returns:
-        Targets information as table.
-    """
     def lookup_targets(self, tool_id, search=None, params={}):
+        """
+        Lookup targets for a given tool
+        Args:
+            tool_id:
+                The tool to lookup targets.
+            search:
+                The search criteria. It could be:
+                - Empty: If empty, return custom targets associated to the tool.
+                - Postal code: Return targets matched by postal code.
+                - Lat/Long: Latitude and Longitude pair separated by '::'.
+                Ex. 45.451596::-73.59912099999997. It will return targets
+                matched for those coordinates.
+                - Search term: For your csv tools, this will return targets
+                matched by given valid search term.
+        Returns:
+            Targets information as table.
+        """
         targets = self.client.lookupTargets(tool_id, search, params=params)
         if targets:
             data = []
@@ -106,17 +106,17 @@ class Newmode:
             logging.warning("Empty targets returned")
             return self.convert_to_table([])
 
-    """
-    Get the action information for a given tool.
-    Args:
-        tool_id:
-            The id of the tool to return.
-        params:
-            Extra parameters sent to New/Mode library.
-    Returns:
-        Tool action information.
-    """
     def get_action(self, tool_id, params={}):
+        """
+        Get the action information for a given tool.
+        Args:
+            tool_id:
+                The id of the tool to return.
+            params:
+                Extra parameters sent to New/Mode library.
+        Returns:
+            Tool action information.
+        """
         action = self.client.getAction(tool_id, params=params)
         if action:
             return action
@@ -124,20 +124,20 @@ class Newmode:
             logging.warning("Empty action returned")
             return None
 
-    """
-    Run specific action with given payload.
-    Args:
-        tool_id:
-            The id of the tool to run.
-        payload:
-            Payload data used to run the action. Structure will depend
-            on the stuff returned by get_action.
-        params:
-            Extra parameters sent to New/Mode library.
-    Returns:
-        Action link (if otl) or sid.
-    """
     def run_action(self, tool_id, payload, params={}):
+        """
+        Run specific action with given payload.
+        Args:
+            tool_id:
+                The id of the tool to run.
+            payload:
+                Payload data used to run the action. Structure will depend
+                on the stuff returned by get_action.
+            params:
+                Extra parameters sent to New/Mode library.
+        Returns:
+            Action link (if otl) or sid.
+        """
         action = self.client.runAction(tool_id, payload, params=params)
         if action:
             if 'link' in action:
@@ -148,17 +148,17 @@ class Newmode:
             logging.warning("Error in response")
             return None
 
-    """
-    Get specific target.
-    Args:
-        target_id:
-            The id of the target to return.
-        params:
-            Extra parameters sent to New/Mode library.
-    Returns:
-        Target information.
-    """
     def get_target(self, target_id, params={}):
+        """
+        Get specific target.
+        Args:
+            target_id:
+                The id of the target to return.
+            params:
+                Extra parameters sent to New/Mode library.
+        Returns:
+            Target information.
+        """
         target = self.client.getTarget(target_id, params=params)
         if target:
             return target
@@ -166,15 +166,15 @@ class Newmode:
             logging.warning("Empty target returned")
             return None
 
-    """
-    Get existing campaigns.
-    Args:
-        params:
-            Extra parameters sent to New/Mode library.
-    Returns:
-        Campaigns information as table.
-    """
     def get_campaigns(self, params={}):
+        """
+        Get existing campaigns.
+        Args:
+            params:
+                Extra parameters sent to New/Mode library.
+        Returns:
+            Campaigns information as table.
+        """
         campaigns = self.client.getCampaigns(params=params)
         if campaigns:
             return self.convert_to_table(campaigns)
@@ -182,17 +182,17 @@ class Newmode:
             logging.warning("Empty campaigns returned")
             return self.convert_to_table([])
 
-    """
-    Get specific campaign.
-    Args:
-        campaign_id:
-            The id of the campaign to return.
-        params:
-            Extra parameters sent to New/Mode library.
-    Returns:
-        Campaign information.
-    """
     def get_campaign(self, campaign_id, params={}):
+        """
+        Get specific campaign.
+        Args:
+            campaign_id:
+                The id of the campaign to return.
+            params:
+                Extra parameters sent to New/Mode library.
+        Returns:
+            Campaign information.
+        """
         campaign = self.client.getCampaign(campaign_id, params=params)
         if campaign:
             return campaign
@@ -200,15 +200,15 @@ class Newmode:
             logging.warning("Empty campaign returned")
             return None
 
-    """
-    Get existing organizations.
-    Args:
-        params:
-            Extra parameters sent to New/Mode library.
-    Returns:
-        Organizations information as table.
-    """
     def get_organizations(self, params={}):
+        """
+        Get existing organizations.
+        Args:
+            params:
+                Extra parameters sent to New/Mode library.
+        Returns:
+            Organizations information as table.
+        """
         organizations = self.client.getOrganizations(params=params)
         if organizations:
             return self.convert_to_table(organizations)
@@ -216,17 +216,17 @@ class Newmode:
             logging.warning("Empty organizations returned")
             return self.convert_to_table([])
 
-    """
-    Get specific organization.
-    Args:
-        organization_id:
-            The id of the organization to return.
-        params:
-            Extra parameters sent to New/Mode library.
-    Returns:
-        Organization information.
-    """
     def get_organization(self, organization_id, params={}):
+        """
+        Get specific organization.
+        Args:
+            organization_id:
+                The id of the organization to return.
+            params:
+                Extra parameters sent to New/Mode library.
+        Returns:
+            Organization information.
+        """
         organization = self.client.getOrganization(organization_id, params=params)
         if organization:
             return organization
@@ -234,15 +234,15 @@ class Newmode:
             logging.warning("Empty organization returned")
             return None
 
-    """
-    Get existing services.
-    Args:
-        params:
-            Extra parameters sent to New/Mode library.
-    Returns:
-        Services information as table.
-    """
     def get_services(self, params={}):
+        """
+        Get existing services.
+        Args:
+            params:
+                Extra parameters sent to New/Mode library.
+        Returns:
+            Services information as table.
+        """
         services = self.client.getServices(params=params)
         if services:
             return self.convert_to_table(services)
@@ -250,17 +250,17 @@ class Newmode:
             logging.warning("Empty services returned")
             return self.convert_to_table([])
 
-    """
-    Get specific service.
-    Args:
-        service_id:
-            The id of the service to return.
-        params:
-            Extra parameters sent to New/Mode library.
-    Returns:
-        Service information.
-    """
     def get_service(self, service_id, params={}):
+        """
+        Get specific service.
+        Args:
+            service_id:
+                The id of the service to return.
+            params:
+                Extra parameters sent to New/Mode library.
+        Returns:
+            Service information.
+        """
         service = self.client.getService(service_id, params=params)
         if service:
             return service
@@ -268,17 +268,17 @@ class Newmode:
             logging.warning("Empty service returned")
             return None
 
-    """
-    Get existing outreaches for a given tool.
-    Args:
-        tool_id:
-            Tool to return outreaches.
-        params:
-            Extra parameters sent to New/Mode library.
-    Returns:
-        Outreaches information as table.
-    """
     def get_outreaches(self, tool_id, params={}):
+        """
+        Get existing outreaches for a given tool.
+        Args:
+            tool_id:
+                Tool to return outreaches.
+            params:
+                Extra parameters sent to New/Mode library.
+        Returns:
+            Outreaches information as table.
+        """
         outreaches = self.client.getOutreaches(tool_id, params=params)
         if outreaches:
             return self.convert_to_table(outreaches)
@@ -286,17 +286,17 @@ class Newmode:
             logging.warning("Empty outreaches returned")
             return self.convert_to_table([])
 
-    """
-    Get specific outreach.
-    Args:
-        outreach_id:
-            The id of the outreach to return.
-        params:
-            Extra parameters sent to New/Mode library.
-    Returns:
-        Outreach information.
-    """
     def get_outreach(self, outreach_id, params={}):
+        """
+        Get specific outreach.
+        Args:
+            outreach_id:
+                The id of the outreach to return.
+            params:
+                Extra parameters sent to New/Mode library.
+        Returns:
+            Outreach information.
+        """
         outreach = self.client.getOutreach(outreach_id, params=params)
         if outreach:
             return outreach

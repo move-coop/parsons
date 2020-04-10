eb3890ccbd27172e7890dbbbf106ea8a3d8f5be6
Eliot Stone <eliot@movementcooperative.org>
2020-04-10 12:58:50 -0400

    don't import newmode

--- parsons/__init__.py
+++ parsons/__init__.py
@@ -38,7 +38,7 @@ if not os.environ.get('PARSONS_SKIP_IMPORT_ALL'):
     from parsons.databases.postgres.postgres import Postgres
     from parsons.freshdesk.freshdesk import Freshdesk
     from parsons.bill_com.bill_com import BillCom
-    from parsons.newmode.newmode import Newmode
+    #from parsons.newmode.newmode import Newmode
     from parsons.databases.mysql.mysql import MySQL
 
     __all__ = [

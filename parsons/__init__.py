189fd501a9b5f7099ab96b724510fa2d6dc2bd8d
Soren Spicknall <soren@spicknall.us>
2020-05-14 08:14:20 -0500

    Create Parsons Mailchimp connector (#239)
    * Create Parsons Mailchimp connector
    
    * pep8 compliance
    
    * pep8 compliance
    
    * Added RST documentation for Mailchimp class
    
    * Used Parsons check_env for API key in __init__
    
    * Substituted f-strings where appropriate
    
    * pep8 compliance
    
    * Completed docstrings for three functions
    
    * Completed docstrings
    
    * pep8 compliance
    
    * Removed transform_table function (not directly in use within Parsons)
    
    * Initial testing structure (functionality incomplete)
    
    * Fixed syntax and boolean references
    
    * Fixed API key structure
    
    * Continued work on test JSON
    
    * Fixed incorrectly formatted booleans
    
    * Added args for function calls
    
    * Fixed references to copy/pasted functions
    
    * Testing URI fixes
    
    * Added members JSON to tests
    
    * Added unsubscribe data to expected JSON
    
    * Fixed stray punctuation
    
    * Fixed punctuation
    
    * Added some basic asserts
    
    * Added Mailchimp to __init__.py
    
    * Return empty tables instead of None
    
    * Fixed Mailchimp references in __init__.py
    
    Co-authored-by: Eliot Stone <eliot@movementcooperative.org>

--- parsons/__init__.py
+++ parsons/__init__.py
@@ -41,6 +41,7 @@ if not os.environ.get('PARSONS_SKIP_IMPORT_ALL'):
     from parsons.newmode.newmode import Newmode
     from parsons.databases.mysql.mysql import MySQL
     from parsons.rockthevote.rtv import RockTheVote
+    from parsons.mailchimp.mailchimp import Mailchimp
     from parsons.zoom.zoom import Zoom
     from parsons.action_network.action_network import ActionNetwork
 
@@ -78,9 +79,10 @@ if not os.environ.get('PARSONS_SKIP_IMPORT_ALL'):
         'Newmode',
         'MySQL',
         'RockTheVote',
+        'Mailchimp',
         'Zoom',
-        'ActionNetwork'
-        ]
+        'ActionNetwork',
+    ]
 
 # Define the default logging config for Parsons and its submodules. For now the
 # logger gets a StreamHandler by default. At some point a NullHandler may be more

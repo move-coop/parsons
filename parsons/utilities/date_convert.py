4284d21b0400859b6915f9e559d4d1cd3da5267d
Daniel Bravo <bravod60@gmail.com>
2020-05-19 14:50:26 -0700

    Fix iso_to_unix function

--- parsons/utilities/date_convert.py
+++ parsons/utilities/date_convert.py
@@ -1,11 +1,20 @@
-import dateutil.parser as dp
+from dateutil.parser import parse
+from datetime import timezone
 
 
-def iso_to_unix(iso_date):
+def iso_to_unix(iso_date, tzinfo=None):
     # Convert an ISO date to a Unix time
 
     if iso_date:
-        parsed_date = dp.parse(iso_date)
-        return int(parsed_date.strftime('%s'))
+        parsed_date = parse(iso_date)
+
+        # if no timezone info is parsed or provided, use UTC
+        if not parsed_date.tzinfo:
+            if tzinfo:
+                parsed_date = parsed_date.replace(tzinfo=tzinfo)
+            else:
+                parsed_date = parsed_date.replace(tzinfo=timezone.utc)
+
+        return int(parsed_date.timestamp())
     else:
         return None

6a7a32100ed72d5171e911db318abd46d7ffc121
Shilpa Dutta <dutta.shilpa6@gmail.com>
2020-07-02 17:38:26 -0700

    list indices must be integers or slices, not str  error pt 2

--- parsons/ngpvan/targets.py
+++ parsons/ngpvan/targets.py
@@ -65,9 +65,10 @@ class Targets(object):
         for i in json_obj:
             job_status = i['jobStatus']
         if job_status == 'Complete':
-            csv = json_obj['file']['downloadUrl']
-            response_csv = requests.get(csv)
-            return Table.from_csv_string(response_csv.text)
+            for j in json_obj:
+                csv = j['file']['downloadUrl']
+                response_csv = requests.get(csv)
+                return Table.from_csv_string(response_csv.text)
         elif job_status == 'Pending' or job_status == 'InProcess':
             logger.info(f'Target export job is pending or in process for {export_job_id}.')
         else:

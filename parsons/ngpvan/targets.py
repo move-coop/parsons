bb948157bae07b61f89d75a519909730b5bd88e3
Shilpa Dutta <dutta.shilpa6@gmail.com>
2020-07-06 15:56:19 -0700

    fix for list obj no items 2

--- parsons/ngpvan/targets.py
+++ parsons/ngpvan/targets.py
@@ -60,8 +60,9 @@ class Targets(object):
         """
 
         response = self.connection.get_request(f'targetExportJobs/{export_job_id}')
-        json_string = json.dumps(response)
-        json_obj = json.loads(json_string)
+        json_obj = json.loads(response)
+        # json_string = json.dumps(response)
+        # json_obj = json.loads(json_string)
         for i,j in json_obj.items():
             if i =='jobStatus':
                 job_status=j

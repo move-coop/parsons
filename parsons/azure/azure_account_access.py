6090645ec219bb3536ed4d8a85805972e0b3791f
Soren Spicknall <soren@spicknall.us>
2020-07-13 10:12:36 -0500

    Switch to environment variable import for authority URL

--- parsons/azure/azure_account_access.py
+++ parsons/azure/azure_account_access.py
@@ -1,3 +1,5 @@
+import os
+
 import adal
 
 class AzureAccountAccess(object):
@@ -12,7 +14,7 @@ class AzureAccountAccess(object):
 
     def get_refresh_azure_token(self, client_id, client_secret):
         RESOURCE = "https://storage.azure.com/"
-        authority_url = "https://login.microsoftonline.com/openprogress.com"
+        authority_url = os.environ('AUTHORITY_URL')
         context = adal.AuthenticationContext(authority_url)
         token = context.acquire_token_with_client_credentials(RESOURCE, client_id, client_secret)
 

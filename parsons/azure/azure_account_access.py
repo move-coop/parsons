10b34e6862ba95ba52fcf27fd5896178cdb3781e
Soren Spicknall <soren@spicknall.us>
2020-07-13 10:07:46 -0500

    Added initial description for class

--- parsons/azure/azure_account_access.py
+++ parsons/azure/azure_account_access.py
@@ -1,6 +1,15 @@
 import adal
 
 class AzureAccountAccess(object):
+    """
+    Instantiate AzureAccountAccess Class for a given Azure storage account.
+    This class is not yet fully built out, and will later have an __init__
+    function added.
+        
+    `Returns:`
+        `AzureAccountAccess`
+    """
+
     def get_refresh_azure_token(self, client_id, client_secret):
         RESOURCE = "https://storage.azure.com/"
         authority_url = "https://login.microsoftonline.com/openprogress.com"

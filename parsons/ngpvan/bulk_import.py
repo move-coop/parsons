1a567d2508ef55feb35aa38605596f14cbba60ff
jburchard <jburchard@gmail.com>
2020-08-14 12:38:06 -0500

    Add methods and some tests.

--- parsons/ngpvan/bulk_import.py
+++ parsons/ngpvan/bulk_import.py
@@ -1,6 +1,10 @@
 """NGPVAN Bulk Import Endpoints"""
+from parsons.etl.table import Table
+from parsons.utilities import cloud_storage
 
 import logging
+import uuid
+
 logger = logging.getLogger(__name__)
 
 
@@ -24,3 +28,109 @@ class BulkImport(object):
         r = self.connection.get_request(f'bulkImportJobs/resources')
         logger.info(f'Found {len(r)} bulk import resources.')
         return r
+
+    def get_bulk_import_job(self, job_id):
+        """
+        Get a bulk import job status.
+
+        `Args:`
+            job_id : int
+                The bulk import job id.
+        `Returns:`
+            dict
+                The bulk import job
+        """
+
+        r = self.connection.get_request(f'bulkImportJobs/{job_id}')
+        logger.info(f'Found bulk import job {job_id}.')
+        return r
+
+    def get_bulk_import_mapping_types(self):
+        """
+        Get bulk import mapping types.
+
+        `Returns:`
+            Parsons Table
+                See :ref:`parsons-table` for output options.
+        """
+
+        tbl = Table(self.connection.get_request('bulkImportMappingTypes'))
+        logger.info(f'Found {tbl.num_rows} bulk import mapping types.')
+        return tbl
+
+    def get_bulk_import_mapping_type(self, type_name):
+        """
+        Get a single bulk import mapping type.
+
+        `Args:`
+            type_name: str
+        `Returns`:
+            dict
+                A mapping type json
+        """
+
+        r = self.connection.get_request(f'bulkImportMappingTypes/{type_name}')
+        logger.info(f'Found {type_name} bulk import mapping type.')
+        return r
+
+    def post_bulk_import(self, tbl, url_type, resource_type, mapping_types,
+                         description, **url_kwargs):
+        # Internal method to post bulk imports. Specific types of imports call this
+        # internal method.
+
+        # Move to cloud storage
+        file_name = str(uuid.uuid1())
+        url = cloud_storage.post_file(tbl, url_type, file_path=file_name + '.zip', **url_kwargs)
+        logger.info(f'Table uploaded to {url_type}.')
+
+        # Generate request json
+        json = {"description": description,
+                "file": {
+                    "columnDelimiter": 'csv',
+                    "columns": [{'name': c} for c in tbl.columns],
+                    "fileName": file_name + '.csv',
+                    "hasHeader": "True",
+                    "hasQuotes": "False",
+                    "sourceUrl": url},
+                "actions": [{"resultFileSizeKbLimit": 5000,
+                             "resourceType": resource_type,
+                             "actionType": "loadMappedFile",
+                             "mappingTypes": mapping_types}]
+                }
+
+        r = self.connection.post_request('bulkImportJobs', json=json)
+        logger.info(f"Bulk upload {r['jobId']} created.")
+        return r['jobId']
+
+    def bulk_apply_activist_codes(self, tbl, url_type, **url_kwargs):
+        """
+        Apply activist codes
+
+        `Args:`
+            table: Parsons table
+                A Parsons table.
+            url_type: str
+                The cloud file storage to use to post the file. Currently only ``S3``.
+            **url_kwargs: kwargs
+                Arguments to configure your cloud storage url type.
+                    * S3 requires ``bucket`` argument and, if not stored as env variables
+                      ``aws_access_key`` and ``aws_secret_access_key``.
+        `Returns:`
+            int
+                The bulk import job id
+        """
+
+        self.validate_first_column(tbl, "vanid")
+        return self.post_bulk_import(tbl,
+                                     url_type,
+                                     'ContactsActivistCodes',
+                                     [{"name": "ActivistCode"}],
+                                     'Activist Code Upload',
+                                     **url_kwargs)
+
+    def validate_first_column(self, tbl, required_column):
+        # The first column of a file is restricted based on the ResourceType and MappingType
+        # and must match or be mapped to a specific column name. This method validates it.
+
+        if tbl.columns[0].lower() != required_column:
+            raise ValueError(f"First column name must be {required_column}")
--- test/test_van/test_bulkimport.py
+++ test/test_van/test_bulkimport.py
@@ -2,6 +2,8 @@ import unittest
 import os
 import requests_mock
 from parsons.ngpvan.van import VAN
+from parsons.etl.table import Table
+from test.utils import assert_matching_tables
 
 os.environ['VAN_API_KEY'] = 'SOME_KEY'
 
@@ -10,7 +12,8 @@ class TestBulkImport(unittest.TestCase):
 
     def setUp(self):
 
-        self.van = VAN(os.environ['VAN_API_KEY'], db="MyVoters", raise_for_status=False)
+        self.van = VAN(os.environ['VAN_API_KEY'],
+                       db="MyVoters", raise_for_status=False)
 
     def tearDown(self):
 
@@ -19,8 +22,74 @@ class TestBulkImport(unittest.TestCase):
     @requests_mock.Mocker()
     def test_get_bulk_import_resources(self, m):
 
-        json = ['Contacts', 'Contributions', 'ActivistCodes', 'ContactsActivistCodes']
+        json = ['Contacts', 'Contributions',
+                'ActivistCodes', 'ContactsActivistCodes']
 
         m.get(self.van.connection.uri + 'bulkImportJobs/resources', json=json)
 
         self.assertEqual(self.van.get_bulk_import_resources(), json)
+
+    @requests_mock.Mocker()
+    def test_get_bulk_import_job(self, m):
+
+        json = {'id': 59,
+                'status': 'InProgress',
+                'resourceType': 'ContactsActivistCodes',
+                'webhookUrl': None,
+                'resultFileSizeLimitKb': 5000,
+                'errors': [],
+                'resultFiles': []}
+
+        m.get(self.van.connection.uri + 'bulkImportJobs/53407', json=json)
+
+        self.assertEqual(self.van.get_bulk_import_job(53407), json)
+
+    @requests_mock.Mocker()
+    def test_get_bulk_import_mapping_types(self, m):
+
+        json = {'name': 'ActivistCode',
+                'displayName': 'Apply Activist Code',
+                'allowMultipleMode': 'Multiple',
+                'resourceTypes': ['Contacts', 'ContactsActivistCodes'],
+                'fields': [{
+                    'name': 'ActivistCodeID',
+                    'description': 'Activist Code ID',
+                    'hasPredefinedValues': True,
+                    'isRequired': True,
+                    'canBeMappedToColumn': True,
+                    'canBeMappedByName': True,
+                    'parents': None},
+                    {'name': 'CanvassedBy',
+                     'description': 'Recruited By, Must be a Valid User ID',
+                     'hasPredefinedValues': False,
+                     'isRequired': False,
+                     'canBeMappedToColumn': True,
+                     'canBeMappedByName': True,
+                     'parents': None},
+                    {'name': 'DateCanvassed',
+                     'description': 'Contacted When',
+                     'hasPredefinedValues': False,
+                     'isRequired': False,
+                     'canBeMappedToColumn': True,
+                     'canBeMappedByName': True,
+                     'parents': [{
+                         'parentFieldName': 'CanvassedBy',
+                         'limitedToParentValues': None
+                     }]
+                     }, {
+                    'name': 'ContactTypeID',
+                    'description': 'Contacted How',
+                    'hasPredefinedValues': True,
+                    'isRequired': False,
+                    'canBeMappedToColumn': True,
+                    'canBeMappedByName': True,
+                    'parents': [{
+                        'parentFieldName': 'CanvassedBy',
+                        'limitedToParentValues': None
+                    }]
+                }]
+                }
+
+        m.get(self.van.connection.uri + 'bulkImportMappingTypes', json=json)
+
+        assert_matching_tables(self.van.get_bulk_import_mapping_types(), Table(json))

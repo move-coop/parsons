01fb1899220f86244c0512b8d28173d03313c690
Daniel Bravo <bravod60@gmail.com>
2020-09-04 22:13:32 -0700

    Fix lint errors in fitures

--- test/fixtures.py
+++ test/fixtures.py
@@ -8,15 +8,18 @@ Simple Table
 The bare minimum Parsons table, and matching files representing that table.
 '''
 
+
 @pytest.fixture
 def simple_table():
     # Note - If you modify this table, you must also update the related "simple" files.
     # Fortunately Parsons should make that easy to do :)
     return Table([{'first': 'Bob', 'last': 'Smith'}])
 
+
 @pytest.fixture
 def simple_csv_path(shared_datadir):
     return str(shared_datadir / 'test-simple-table.csv')
+
 
 @pytest.fixture
 def simple_compressed_csv_path(shared_datadir):

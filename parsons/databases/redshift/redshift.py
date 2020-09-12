48d83436797ca50933a3a8b5295e0f56a385ddf6
jburchard <jburchard@gmail.com>
2020-09-12 14:28:40 -0500

    Redshift: Unload Bug Second Attempt (#386)
    * Add hard return...
    
    * One more issue.

--- parsons/databases/redshift/redshift.py
+++ parsons/databases/redshift/redshift.py
@@ -608,7 +608,7 @@ class Redshift(RedshiftCreateTable, RedshiftCopyTable, RedshiftTableUtilities, R
         if header:
             statement += "HEADER \n"
         if delimiter:
-            statement += f"DELIMITER as {delimiter} \n"
+            statement += f"DELIMITER as '{delimiter}' \n"
         if compression:
             statement += f"{compression.upper()} \n"
         if add_quotes:

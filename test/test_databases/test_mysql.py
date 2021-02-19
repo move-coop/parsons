772372749e186644991b79716eb3613835976353
Yotam <paxarchy@gmail.com>
2021-02-19 10:29:05 -0500

    removing MySQLTable class so as not to override BaseTable query methods

--- test/test_databases/test_mysql.py
+++ test/test_databases/test_mysql.py
@@ -1,4 +1,4 @@
-from parsons.databases.mysql.mysql import MySQL, MySQLTable
+from parsons.databases.mysql.mysql import MySQL
 from parsons.etl.table import Table
 from test.utils import assert_matching_tables
 import unittest
@@ -63,7 +63,7 @@ class TestMySQL(unittest.TestCase):
                                 ('you', 'hey', '3')
                          """)
 
-        self.tbl = MySQLTable(self.mysql, 'test')
+        self.tbl = BaseTable(self.mysql, 'test')
 
     def tearDown(self):
 
@@ -90,7 +90,7 @@ class TestMySQL(unittest.TestCase):
 
         self.assertTrue(self.tbl.exists)
 
-        tbl_bad = MySQLTable(self.mysql, 'bad_test')
+        tbl_bad = BaseTable(self.mysql, 'bad_test')
         self.assertFalse(tbl_bad.exists)
 
     def test_drop(self):

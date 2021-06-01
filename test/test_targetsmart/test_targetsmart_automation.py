d6bf833963d523f001b226fef0eb0b7269eb7044
Chris Cuellar <58723+ChrisC@users.noreply.github.com>
2021-06-01 10:28:55 -0700

    Updates targetsmart sftp config (#528)
    * Updates targetsmart sftp config
    
    * Update folder references for targetsmart test XML files

--- test/test_targetsmart/test_targetsmart_automation.py
+++ test/test_targetsmart/test_targetsmart_automation.py
@@ -15,7 +15,7 @@ class TestTargetSmartAutomation(unittest.TestCase):
                          os.environ['TS_SFTP_USERNAME'],
                          os.environ['TS_SFTP_PASSWORD'],
                          self.ts.sftp_port)
-        self.test_xml = 'test/test_ts/job_config.xml'
+        self.test_xml = 'test/test_targetsmart/job_config.xml'
 
     def tearDown(self):
 
@@ -51,13 +51,13 @@ class TestTargetSmartAutomation(unittest.TestCase):
     def test_match_status(self):
 
         # Find good configuration
-        good_match = 'test/test_ts/match_good.xml'
+        good_match = 'test/test_targetsmart/match_good.xml'
         self.sftp.put_file(good_match, f'{self.ts.sftp_dir}/{self.job_name}.finish.xml')
         self.assertTrue(self.ts.match_status(self.job_name))
         self.ts.remove_files(self.job_name)
 
         # Find bad configuration
-        bad_match = 'test/test_ts/match_bad.xml'
+        bad_match = 'test/test_targetsmart/match_bad.xml'
         self.sftp.put_file(bad_match, f'{self.ts.sftp_dir}/{self.job_name}.finish.xml')
         self.assertRaises(ValueError, self.ts.match_status, self.job_name)
 

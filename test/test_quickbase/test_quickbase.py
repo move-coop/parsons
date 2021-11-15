683c94b452e7072268ed6642d72abeab438ee62e
Soren Spicknall <soren@movementcooperative.org>
2021-11-15 16:29:48 -0600

    Quickbase connector - some initial endpoint implementations (#586)
    Airst pass at essential Quickbase connector features, including grabbing table IDs from the Quickbase API and querying those tables using Quickbase's query language.
    
    Co-authored-by: Chris Cuellar <58723+ChrisC@users.noreply.github.com>

---
+++ test/test_quickbase/test_quickbase.py
@@ -0,0 +1,26 @@
+from parsons.quickbase.quickbase import Quickbase
+from test.test_quickbase import test_data
+import unittest
+import requests_mock
+
+
+class TestQuickbase(unittest.TestCase):
+
+    @requests_mock.Mocker()
+    def test_get_app_tables(self, m):
+
+        qb = Quickbase(hostname='test.example.com', user_token='12345')
+        m.get(f'{qb.api_hostname}/tables?appId=test',
+              json=test_data.test_get_app_tables)
+        tbl = qb.get_app_tables(app_id='test')
+
+        self.assertEqual(tbl.num_rows, 2)
+
+    @requests_mock.Mocker()
+    def test_query_records(self, m):
+        qb = Quickbase(hostname='test.example.com', user_token='12345')
+        m.post(f'{qb.api_hostname}/records/query',
+               json=test_data.test_query_records)
+        tbl = qb.query_records(table_from='test_table')
+
+        self.assertEqual(tbl.num_rows, 1)

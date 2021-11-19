450b25c8410ae72d586b7b0cbb45a3269dabdaa9
Chris Cuellar <58723+ChrisC@users.noreply.github.com>
2021-11-19 09:14:44 -0800

    adds documentation
    + placeholders for additional connector methods

---
+++ docs/controlshift.rst
@@ -0,0 +1,42 @@
+Controlshift
+=========
+
+********
+Overview
+********
+
+Controlshift is a platform for creating campaigns with distributed events, local groups, and member-led petitions. This class allows you to interact with select functions of the the `Controlshift Authenticated REST API <https://developers.controlshiftlabs.com/#authenticated-rest-api/>`_ .
+
+.. note::
+  Authentication
+    An API Application Integration is required to instantiate the ``Controlshift`` class.
+    Details on how to create the integration and acquire credentials can be found `on the Controlshift website
+    <https://developers.controlshiftlabs.com/#authenticated-rest-api-quickstart-guide>`_.
+
+**********
+Quickstart
+**********
+
+To instantiate the Controlshift class, you can either store your credentials as environment
+variables (``CONTROLSHIFT_HOSTNAME``, ``CONTROLSHIFT_CLIENT_ID``, and ``CONTROLSHIFT_CLIENT_SECRET``) or pass them in as arguments:
+
+.. code-block:: python
+
+   from parsons import Controlshift
+
+   # First approach: Use API credentials via environmental variables
+   cs = Controlshift()
+
+   # Second approach: Pass API credentials as arguments
+   cs = Controlshift(hostname='my_hostname', client_id='my_client_id', client_secret='my_client_secret')
+
+   # Get all petitions
+   cs.get_petitions()
+
+
+**************
+Controlshift Class
+**************
+
+.. autoclass :: parsons.Controlshift
+    :inherited-members:
--- parsons/controlshift/controlshift.py
+++ parsons/controlshift/controlshift.py
@@ -1,13 +1,34 @@
 from parsons.etl.table import Table
 from parsons.utilities import check_env
-from parsons.utilities.oauth_api_connector import OAuthAPIConnector
+from parsons.utilities.oauth_api_connector import OAuth2APIConnector
 
 
 class Controlshift(object):
-    def __init__(self, hostname=None, client_id=None, client_secret=None) -> None:
+    """
+    Instantiate the Controlshift class. Requires an API Application integration.
+    For more info on setup, see:
+    https://developers.controlshiftlabs.com/#authenticated-rest-api-quickstart-guide
+
+    `Args:`
+        hostname: str
+            The URL for the homepage/login page of the organization's Controlshift
+            instance (e.g. demo.controlshift.app). Not required if
+            ``CONTROLSHIFT_HOSTNAME`` env variable is set.
+        client_id: str
+            The Client ID for your REST API Application. Not required if
+            ``CONTROLSHIFT_CLIENT_ID`` env variable is set.
+        client_secret: str
+            The Client Secret for your REST API Application. Not required if
+            ``CONTROLSHIFT_CLIENT_SECRET`` env variable is set.
+    `Returns:`
+        Controlshift Class
+    """
+
+    def __init__(self, hostname=None, client_id=None, client_secret=None):
+
         self.hostname = check_env.check('CONTROLSHIFT_HOSTNAME', hostname)
         token_url = f'{self.hostname}/oauth/token'
-        self.client = OAuthAPIConnector(
+        self.client = OAuth2APIConnector(
             self.hostname,
             client_id=check_env.check('CONTROLSHIFT_CLIENT_ID', client_id),
             client_secret=check_env.check('CONTROLSHIFT_CLIENT_SECRET', client_secret),
@@ -15,7 +36,14 @@ class Controlshift(object):
             auto_refresh_url=token_url
         )
 
-    def get_petitions(self):
+    def get_petitions(self) -> Table:
+        """
+        Get a full list of all petitions, including ones that are unlaunched or otherwise not
+        visible to the public.
+
+        `Return:`
+            Table Class
+        """
         next_page = 1
         petitions = []
         while next_page:
@@ -25,3 +53,24 @@ class Controlshift(object):
             petitions.extend(response['petitions'])
 
         return Table(petitions)
+
+        def get_signatures(self):
+            pass
+
+        def get_members(self):
+            pass
+
+        def get_partnerships(self):
+            pass
+
+        def get_events(self):
+            pass
+
+        def get_attendees(self):
+            pass
+
+        def get_calendars(self):
+            pass
+
+        def get_local_groups(self):
+            pass

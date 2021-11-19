5c34d3cba38a4dff951c880ed7c6c2dde8713e48
Chris Cuellar <58723+ChrisC@users.noreply.github.com>
2021-11-19 06:03:03 -0800

    add mock tests and test data

--- test/test_controlshift/test_controlshift.py
+++ test/test_controlshift/test_controlshift.py
@@ -1,22 +1,33 @@
-from build.lib.test.utils import validate_list
+import os
+import requests_mock
+from unittest import TestCase
+from test.utils import mark_live_test, validate_list
 from parsons.controlshift.controlshift import Controlshift
-from test.utils import mark_live_test
+from test.test_controlshift import test_data  # type: ignore
 
 
 @mark_live_test
-def test_get_petitions():
-    cs = Controlshift()
-    tbl = cs.get_petitions()
-    expected_columns = [
-        'admin_events_status', 'admin_status', 'alias', 'campaigner_contactable',
-        'can_download_signers', 'created_at', 'custom_goal', 'delivery_details',
-        'external_facebook_page', 'external_site', 'goal', 'hide_petition_creator',
-        'hide_recent_signers', 'hide_signature_form', 'id', 'launched', 'locale',
-        'petition_creator_name_override', 'redirect_to', 'show_progress_bar',
-        'signature_count_add_amount', 'slug',
-        'source', 'title', 'updated_at', 'what', 'who', 'why', 'title_locked', 'who_locked',
-        'what_locked', 'why_locked', 'delivery_details_locked', 'external_facebook_page_locked',
-        'external_site_locked', 'categories_locked', 'url', 'public_who', 'ended', 'successful',
-        'image', 'public_signature_count', 'admin_notes', 'creator', 'mentor', 'reviewer',
-        'location', 'decision_makers', 'effort', 'partnership', 'labels', 'categories']
-    assert validate_list(expected_columns, tbl)
+class TestControlshiftLive(TestCase):
+
+    def test_get_live_petitions(self):
+        cs = Controlshift()
+        tbl = cs.get_petitions()
+        self.assertTrue(validate_list(test_data.expected_petition_columns, tbl))
+
+
+class TestControlshiftMock(TestCase):
+
+    def setUp(self):
+        self.hostname = os.environ["CONTROLSHIFT_HOSTNAME"]
+
+    @requests_mock.Mocker()
+    def test_get_petitions(self, m):
+        m.post(
+            f'{self.hostname}/oauth/token',
+            json={'access_token': '123'})
+        cs = Controlshift()
+
+        m.get(f'{self.hostname}/api/v1/petitions', json=test_data.petition_test_data)
+        tbl = cs.get_petitions()
+
+        self.assertTrue(validate_list(test_data.expected_petition_columns, tbl))

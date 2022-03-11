3144fb52d012f44e75bf802d2b919bfb591a1daf
Melissa Woods <mkwoods927@gmail.com>
2022-03-11 10:46:02 -0500

    formatting cleanup

--- useful_resources/sample_code/civis_job_status_slack_alert.py
+++ useful_resources/sample_code/civis_job_status_slack_alert.py
@@ -1,21 +1,21 @@
-# This script checks the status of all jobs and workflows in a given Civis Project and posts them to a Slack channel.
+# This script checks the status of all jobs and workflows in a given Civis Project
+# and posts them to a Slack channel.
 
 import os
 import civis
 import datetime
-import json
 import logging
 from parsons import Slack, Table
 
 # To use the Civis connector, set the environment variables CIVIS_DATABASE and CIVIS_API_KEY.
-# These environment variables are not necessary if you are running this code in a Civis container script.
+# These environment variables are not necessary if you run this code in a Civis container script.
 client = civis.APIClient()
 # To use the Slack connector, set the environment variable SLACK_API_TOKEN
 slack = Slack()
 
 # Configuration variables
-SLACK_CHANNEL = '' # Slack channel where the alert will post.
-CIVIS_PROJECT = '' # ID of the Civis project with jobs and workflows you want to see the status of.
+SLACK_CHANNEL = ''  # Slack channel where the alert will post.
+CIVIS_PROJECT = ''  # ID of the Civis project with jobs and workflows you want to see the status of.
 
 logger = logging.getLogger(__name__)
 _handler = logging.StreamHandler()
@@ -24,14 +24,18 @@ _handler.setFormatter(_formatter)
 logger.addHandler(_handler)
 logger.setLevel('INFO')
 
+
 # Cleans up datetime format for posting to Slack.
 def format_datetime(text):
+    
     formatted_text = text.replace('Z', '')
     dt = datetime.datetime.fromisoformat(formatted_text)
     return dt.strftime('%Y-%m-%d')
 
+
 # Assigns an emoji for each potential run status a Civis job or workflow might have.
 def get_run_state_emoji(run_state):
+    
     if run_state == 'succeeded':
         return ':white_check_mark:'
     elif run_state == 'failed':
@@ -41,32 +45,38 @@ def get_run_state_emoji(run_state):
     else:
         return ':shrug:'
 
-# Returns a Parsons table with workflow and job data from the specified Civis project.     
+
+# Returns a Parsons table with workflow and job data from the specified Civis project.
 def get_workflows_and_jobs(project_id):
+
     project = client.projects.get(project_id)
-    
+
     # Get workflow and the job data from the project
     workflows = [dict(workflow) for workflow in project['workflows']]
     table = Table(workflows)
-    # We need to distinguish between jobs and workflows later on, so adding a column noting these are workflows.
+    # We need to distinguish between jobs and workflows later on,
+    # so adding a column noting these are workflows.
     table.add_column('object_type', 'workflow')
-    
+
     # Imports and other scripts are separated out in the response but they are all treated as jobs
     # so we pull and combine
     jobs = [dict(job) for job in project['scripts']]
     imports = [dict(import_job) for import_job in project['imports']]
     full_list = jobs + imports
     jobs_table = Table(full_list)
-    jobs_table.add_column('object_type', 'job') 
-    
+    jobs_table.add_column('object_type', 'job')
+
     # Here we combine the table of jobs and imports into the table of workflows.
-    # The object_type column lets us distinguish between the types, which is necessary for the get_last_success function.
+    # The object_type column lets us distinguish between the types,
+    # which is necessary for the get_last_success function.
     table.concat(jobs_table)
-    
+
     return table
-  
+
+
 # Returns the date and time of the last successful run for a Civis job or workflow.
 def get_last_success(object_id, object_type):
+    
     last_success = '-'
 
     if object_type == 'workflow':
@@ -77,10 +87,11 @@ def get_last_success(object_id, object_type):
             else:
                 last_success = format_datetime(execution['finished_at'])
                 break
-    
+
     elif object_type == 'job':
         job_runs = client.jobs.list_runs(object_id)
-        job_runs_tbl = Table([dict(job_run) for job_run in job_runs]).sort(columns='finished_at', reverse=True)
+        job_runs_tbl = Table([dict(run) for run in job_runs]) \
+                       .sort(columns='finished_at', reverse=True)
         for run in job_runs_tbl:
             if run['state'] != 'succeeded':
                 continue
@@ -92,20 +103,22 @@ def get_last_success(object_id, object_type):
         logger.info(f'{object_type} is not a valid object type.')
 
     return last_success
-  
+
+
 def main():
+
     project_name = client.projects.get(CIVIS_PROJECT)['name']
-    
+
     scripts_table = get_workflows_and_jobs(CIVIS_PROJECT).sort(columns=['state', 'name'])
-    
+
     logger.info(f'Found {scripts_table.num_rows} jobs and workflows in {project_name} project.')
-    
+
     # This is a list of strings we will build with each job's status
     output_lines = []
-    
+
     for run in scripts_table:
         last_success = get_last_success(run['id'], run['object_type'])
-        
+
         output_line = f"{get_run_state_emoji(run['state'])} {run['name']} (last success: {last_success})"
         output_lines.append(output_line)
 
@@ -117,6 +130,6 @@ def main():
     # Post message
     slack.message_channel(SLACK_CHANNEL, message, as_user=True)
     logger.info('Slack message posted')
-    
+
 if __name__ == '__main__':
     main()

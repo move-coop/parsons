5e96f8fa21353476a33f9ee0f1592424fdb7ba4f
Shauna Gordon-McKeon <shaunagm@gmail.com>
2022-03-18 14:52:25 -0400

    Update CI to use new docs deploy script

---
+++ docs/deploy_docs.sh
@@ -0,0 +1,7 @@
+git checkout -b latest
+git checkout $(git tag -l --sort=creatordate |tail -1)
+git checkout -b stable
+git checkout main
+
+sphinx-multiversion . html
+cp ./index-redirect.html html/index.html

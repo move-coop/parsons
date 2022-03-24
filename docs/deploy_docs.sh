3539bd116074037b7f6f32e8076cb9aecc39b783
Shauna Gordon-McKeon <shaunagm@gmail.com>
2022-03-24 11:50:41 -0400

    Fix errors in deploy

--- docs/deploy_docs.sh
+++ docs/deploy_docs.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 git checkout -b latest
+git stash
 git checkout $(git tag -l --sort=creatordate |tail -1)
 git checkout -b stable
-git checkout main
+git checkout master
 
 sphinx-multiversion . html
 cp ./index-redirect.html html/index.html

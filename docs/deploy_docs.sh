afdf82b98b6e5b164534673cd089613cfacfbc7a
Shauna Gordon-McKeon <shaunagm@gmail.com>
2022-03-24 12:13:15 -0400

    Fix stashing so we can use conf.py even though its not in master

--- docs/deploy_docs.sh
+++ docs/deploy_docs.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-git checkout -b latest
 git stash
+git checkout -b latest
 git checkout $(git tag -l --sort=creatordate |tail -1)
 git checkout -b stable
-git checkout master
+git stash apply
 
 sphinx-multiversion . html
 cp ./index-redirect.html html/index.html

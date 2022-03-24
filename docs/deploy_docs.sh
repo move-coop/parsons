6a864db5301ab6de0004034436b838bb45c699f8
Shauna Gordon-McKeon <shaunagm@gmail.com>
2022-03-24 12:59:31 -0400

    New approach to creating git branches

--- docs/deploy_docs.sh
+++ docs/deploy_docs.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-git stash
-git checkout -b latest
-git checkout $(git tag -l --sort=creatordate |tail -1)
-git checkout -b stable
-git stash apply
+git branch latest
+git branch stable $(git tag -l --sort=creatordate |tail -1)
 
 sphinx-multiversion . html
 cp ./index-redirect.html html/index.html

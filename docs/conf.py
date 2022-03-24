6fccb0c564f078cc012d4d2fdf9e88b0caa70a9b
Shauna Gordon-McKeon <shaunagm@gmail.com>
2022-03-24 11:54:31 -0400

    Minor change to force tests to re-run

--- docs/conf.py
+++ docs/conf.py
@@ -178,7 +178,7 @@ texinfo_documents = [
 
 # -- Extension configuration -------------------------------------------------
 
-# overrides for sphinx-multiversion extension
+# sphinx-multiversion
 
 DOCUMENTED_VERSIONS = ["v0.18.1", "v0.18.0", "v0.17.0", "v0.16.0", "v0.15.0", "v0.14.0"]
 

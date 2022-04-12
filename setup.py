be081eafa67f1c747a19ddf0fe7a46d4776b9f0f
CM Lubinski <lubinskic@dnc.org>
2022-04-12 14:26:56 -0400

    Tag 3.9, 3.10 and limit Python versions.
    Pip'll pay attention to the Python version when installing and prevent
    users from installing Parsons if the versions don't match.

--- setup.py
+++ setup.py
@@ -24,7 +24,10 @@ def main():
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8'
-        ]
+            'Programming Language :: Python :: 3.9'
+            'Programming Language :: Python :: 3.10'
+        ],
+        python_requires=">=3.6.0,<3.11.0",
     )
 
 

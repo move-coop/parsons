3f652449c2d0563558c0bdc6abb7e671706747ec
Soren Spicknall <soren@spicknall.us>
2022-05-11 13:43:24 -0500

    HOTFIX language classifiers in setup.py for PyPI release (#686)

--- setup.py
+++ setup.py
@@ -74,8 +74,8 @@ def main():
             'Development Status :: 3 - Alpha',
             'Intended Audience :: Developers',
             'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8'
-            'Programming Language :: Python :: 3.9'
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10'
         ],
         python_requires=">=3.7.0,<3.11.0",

f92a1c6fb049043c3f06270eb2646d2cacc91ef3
Corma Martinez del Rio <66973815+cmdelrio@users.noreply.github.com>
2022-09-16 16:38:45 -0600

    A few more methods
    Need to figure out page_count issue

--- parsons/mobilecommons/mobilecommons.py
+++ parsons/mobilecommons/mobilecommons.py
@@ -1,3 +1,8 @@
+"""
+Figure out what to do about campaign subscribers, which has page_count key. Also find out
+if limit is a page count limit, or a limit on the total number of rows returned.
+"""
+
 from parsons.utilities import check_env
 from parsons.utilities.api_connector import APIConnector
 from parsons.utilities.datetime import parse_date
@@ -12,7 +17,7 @@ logger = logging.getLogger(__name__)
 
 MC_URI = 'https://secure.mcommons.com/api/'
 DATE_FMT = '%Y-%m-%d'
-format_date = lambda x: parse_date(x).strftime(DATE_FMT)
+format_date = lambda x: parse_date(x).strftime(DATE_FMT) if x is not None else None
 
 class MobileCommons:
     """
@@ -36,16 +41,20 @@ class MobileCommons:
         self.default_params = {'company': company_id} if company_id else {}
         self.client = APIConnector(uri=MC_URI, auth=(self.username,self.password))
 
-    def mc_get_request(self, endpoint, data_key, params, elements_to_unpack, limit):
+    def mc_get_request(self, endpoint, first_data_key, second_data_key, params,
+                       elements_to_unpack=None, limit=None):
         """
         A function for GET requests that handles MobileCommons xml responses and pagination
 
         `Args:`
             endpoint: str
                 The endpoint, which will be appended to the base URL for each request
-            data_key: str
-                The key used to extract the desired data from the response dictionary derived from
-                the xml response
+            first_data_key: str
+                The first key used to extract the desired data from the response dictionary derived
+                from the xml response
+            first_data_key: str
+                The second key used to extract the desired data from the response dictionary derived
+                from the xml response. The value of this key is a list of values
             params: str
                 Parameters to be passed into GET request
             elements_to_unpack: list
@@ -81,17 +90,18 @@ class MobileCommons:
             response_dict = xmltodict.parse(response.text, attr_prefix='', cdata_key='',
                                             dict_constructor=dict)
             # if there's only one row, then it is returned as a dict, otherwise is a list
-            data = response_dict['response'][endpoint][data_key]
+            data = response_dict['response'][first_data_key][second_data_key]
             if isinstance(data, dict):
                 data = [data]
             response_table = Table(data)
             # Unpack any specified elements
-            for col in elements_to_unpack:
-                response_table.unpack_dict(col)
+            if elements_to_unpack:
+                for col in elements_to_unpack:
+                    response_table.unpack_dict(col)
             # Append to final table
             final_table.concat(response_table)
             # Check to see if there are more pages and determine how many to retrieve
-            avail_pages = int(response_dict['response'][endpoint]['page_count'])
+            avail_pages = int(response_dict['response'][first_data_key]['page_count'])
             req_pages = math.ceil(limit/1000)
             pages_to_get = min(avail_pages, req_pages)
             # Go fetch other pages of data
@@ -102,38 +112,121 @@ class MobileCommons:
                 response = self.client.request(endpoint, 'GET', params=page_params)
                 response_dict = xmltodict.parse(response.text, attr_prefix='', cdata_key='',
                                                 dict_constructor=dict)
-                response_table = Table(response_dict['response'][endpoint][data_key])
+                response_table = Table(response_dict['response'][first_data_key][second_data_key])
                 final_table.concat(response_table)
 
         return final_table
 
-    def get_broadcasts(self, start=None, end=None, status=None, campaign_id=None, limit=None):
+    def get_broadcasts(self, first_date=None, last_date=None, status=None, campaign_id=None,
+                       limit=None):
         """
         A function for get broadcasts
 
         `Args:`
-            start: str
+            first_date: str
                 The date of the earliest possible broadcast you'd like returned.
-            end: str
+            last_date: str
                 The date of the latest possible broadcast you'd like returned.
-            params: str
-                Parameters to be passed into GET request
             status: str
                 'draft', 'scheduled', or 'generated'
             campaign_id: int
                 Specify to return broadcasts from a specific campaign
+            limit: int
+                Max rows you want returned
 
         `Returns:`
             Parsons table with requested broadcasts
         """
 
         params = {
-            'start_time': format_date(start),
-            'end_time': format_date(end),
+            'start_time': format_date(first_date),
+            'end_time': format_date(last_date),
             'campaign_id': campaign_id,
             'status': status,
             **self.default_params
         }
 
-        return self.mc_get_request(endpoint='broadcasts', data_key='broadcast',
-                                   params=params, elements_to_unpack=['campaign'], limit=limit)
+        return self.mc_get_request(endpoint='broadcasts', first_data_key='broadcasts',
+                                   second_data_key='broadcast', params=params,
+                                   elements_to_unpack=['campaign'], limit=limit)
+
+
+    def get_campaign_subscribers(self, campaign_id: int, first_date: str=None, last_date: str=None,
+                                 opt_in_path_id: int=None, limit: int=None):
+        """
+        A function for getting subscribers of a specified campaign
+
+        `Args:`
+            campaign_id: int
+                The campaign for which you'd like to get subscribers. You can get this from the url
+                of the campaign's page after select a campaign at
+                https://secure.mcommons.com/campaigns
+            first_date: str
+                The date of the earliest possible subscription returned.
+            last_date: str
+                The date of the latest possible subscription you'd like returned.
+            opt_in_path_id: int
+                Optional parameter to narrow results to on particular opt-in path. You can get this
+                from the url of the opt in paths page https://secure.mcommons.com/opt_in_paths
+            limit: int
+                Max rows you want returned
+
+        `Returns:`
+            Parsons table with requested broadcasts
+        """
+
+        params = {
+            'campaign_id': campaign_id,
+            'from': format_date(first_date),
+            'to': format_date(last_date),
+            'opt_in_path_id': opt_in_path_id,
+            **self.default_params
+        }
+
+        return self.mc_get_request(endpoint='campaign_subscribers', first_data_key='subscriptions',
+                                   second_data_key='sub', params=params, limit=limit)
+
+
+    def get_profiles(self, phones: list=None, first_date: str = None, last_date: str = None,
+                     include_custom_columns: bool=True, include_subscriptions: bool=False,
+                     limit: int = None):
+        """
+        A function for getting profiles, which are MobileCommons people records
+
+        `Args:`
+            phones: list
+                A list of phone numbers including country codes for which you want profiles returned
+                MobileCommons claims to recognize most formats.
+            first_date: str
+                The date of the earliest possible subscription returned.
+            last_date: str
+                The date of the latest possible subscription you'd like returned.
+            include_custom_columns: bool
+                Optional parameter to that, if set to True, will return custom column values for
+                profiles as a list of dictionaries contained within a column.
+            include_subscriptions: bool
+                Optional parameter to that, if set to True, will return a list of campaigns a
+                given profile is subscribed to in a single column
+            limit: int
+                Max rows you want returned
+
+        `Returns:`
+            Parsons table with requested broadcasts
+        """
+
+        custom_cols = 'true' if include_custom_columns == True else 'false'
+        subscriptions='true' if include_subscriptions == True else 'false'
+
+        params = {
+            'phones': phones,
+            'from': format_date(first_date),
+            'to': format_date(last_date),
+            'include_custom_columns': custom_cols,
+            'include_subscriptions': subscriptions,
+            **self.default_params
+        }
+
+        return self.mc_get_request(endpoint='profiles', first_data_key='profiles',
+                                   second_data_key='profile',
+                                   elements_to_unpack=['source', 'address'], params=params,
+                                   limit=limit)

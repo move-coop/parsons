00d171d3528c7fd4550470474cdedf4803d0362c
Adam Greenspan <agreenspan24@gmail.com>
2022-10-21 08:39:39 -0400

    flake linting'

--- parsons/scytl/scytl.py
+++ parsons/scytl/scytl.py
@@ -25,7 +25,10 @@ COUNTY_DETAIL_XML_ZIP_URL_TEMPLATE = CLARITY_URL + \
 ELECTION_SETTINGS_JSON_URL_TEMPLATE = \
     CLARITY_URL + '{state}/{election_id}/{version_num}/json/en/electionsettings.json'
 
-BROWSER_HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+BROWSER_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) ' +
+                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+}
 
 TZ_INFO = {
     "EST": "UTC-5",

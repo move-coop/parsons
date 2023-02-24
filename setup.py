baab3e999765661a5799a59a8aaf21089443c075
Jason Walker <jason@indivisible.org>
2023-02-24 12:35:40 -0500

    Change release version in setup.py

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ def main():
 
     setup(
         name="parsons",
-        version='0.21.0',
+        version='1.0.0',
         author="The Movement Cooperative",
         author_email="info@movementcooperative.org",
         url='https://github.com/movementcoop/parsons',

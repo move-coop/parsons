e209ed8d21a3336cf563add0f9ed939a3db2c896
Yotam <paxarchy@gmail.com>
2023-05-26 09:07:05 -0400

    lint using black

--- parsons/action_builder/__init__.py
+++ parsons/action_builder/__init__.py
@@ -1,5 +1,3 @@
 from parsons.action_builder.action_builder import ActionBuilder
 
-__all__ = [
-    'ActionBuilder'
-]
+__all__ = ["ActionBuilder"]
--- parsons/action_builder/action_builder.py
+++ parsons/action_builder/action_builder.py
@@ -4,7 +4,8 @@ from parsons.utilities import check_env
 from parsons.utilities.api_connector import APIConnector
 import logging
 
-API_URL = 'https://{subdomain}.actionbuilder.org/api/rest/v1'
+API_URL = "https://{subdomain}.actionbuilder.org/api/rest/v1"
+
 
 class ActionBuilder(object):
     """
@@ -18,46 +19,46 @@ class ActionBuilder(object):
             or edited. Can also be supplied in individual methods in case multiple campaigns need
             to be referenced.
     """
-    
+
     def __init__(self, api_token=None, subdomain=None, campaign=None):
-        self.api_token = check_env.check('ACTION_BUILDER_API_TOKEN', api_token)
+        self.api_token = check_env.check("ACTION_BUILDER_API_TOKEN", api_token)
         self.headers = {
             "Content-Type": "application/json",
-            "OSDI-API-Token": self.api_token
+            "OSDI-API-Token": self.api_token,
         }
         self.api_url = API_URL.format(subdomain=subdomain)
         self.api = APIConnector(self.api_url, headers=self.headers)
         self.campaign = campaign
-        
+
     def _campaign_check(self, campaign):
         # Raise an error if campaign is not provided via instatiation nor method argument
 
-        final_campaign = campaign or self.campaign            
+        final_campaign = campaign or self.campaign
         if not final_campaign:
-            raise ValueError('No campaign provided!')
-            
+            raise ValueError("No campaign provided!")
+
         return final_campaign
-    
+
     def _get_page(self, campaign, object_name, page, per_page=25, filter=None):
         # Returns data from one page of results
 
         if per_page > 25:
             per_page = 25
-            logger.info("Action Builder's API will not return more than 25 entries per page. \
-            Changing per_page parameter to 25.")
+            logger.info(
+                "Action Builder's API will not return more than 25 entries per page. \
+            Changing per_page parameter to 25."
+            )
 
-        params = {
-            "page": page,
-            "per_page": per_page,
-            "filter": filter
-        }
-        
+        params = {"page": page, "per_page": per_page, "filter": filter}
+
         campaign = self._campaign_check(campaign)
-        url = f'campaigns/{campaign}/{object_name}'
-        
+        url = f"campaigns/{campaign}/{object_name}"
+
         return self.api.get_request(url=url, params=params)
-    
-    def _get_entry_list(self, campaign, object_name, limit=None, per_page=25, filter=None):
+
+    def _get_entry_list(
+        self, campaign, object_name, limit=None, per_page=25, filter=None
+    ):
         # Returns a list of entries for a given object, such as people, tags, or connections.
         # See Action Builder API docs for more: https://www.actionbuilder.org/docs/v1/index.html
 
@@ -67,13 +68,14 @@ class ActionBuilder(object):
 
         # Keep getting the next page until record limit is exceeded or an empty result returns
         while True:
-
             # Get this page and increase page number to the next one
-            response = self._get_page(campaign, object_name, page, per_page, filter=filter)
+            response = self._get_page(
+                campaign, object_name, page, per_page, filter=filter
+            )
             page = page + 1
 
             # Check that there's actually data
-            response_list = response.get('_embedded', {}).get(f"osdi:{object_name}")
+            response_list = response.get("_embedded", {}).get(f"osdi:{object_name}")
 
             if not response_list:
                 # This page has no data, so we're done
@@ -86,7 +88,7 @@ class ActionBuilder(object):
                 if count >= limit:
                     # Limit reached or exceeded, so return just the requested limit amount
                     return Table(return_list[0:limit])
-    
+
     def get_campaign_tags(self, campaign=None, limit=None, per_page=25, filter=None):
         """
         Retrieve all tags (i.e. custom field values) within provided limit and filters
@@ -104,9 +106,11 @@ class ActionBuilder(object):
         `Returns:`
             Parsons Table of full set of tags available in Action Builder.
         """
-        
-        return self._get_entry_list(campaign, 'tags', limit=limit, per_page=per_page, filter=filter)
-    
+
+        return self._get_entry_list(
+            campaign, "tags", limit=limit, per_page=per_page, filter=filter
+        )
+
     def get_tag_by_name(self, tag_name, campaign=None):
         """
         Convenience method to retrieve data on a single tag by its name/value
@@ -119,11 +123,11 @@ class ActionBuilder(object):
         `Returns:`
             Parsons Table of data found on tag in Action Builder from searching by name.
         """
-        
+
         filter = f"name eq '{tag_name}'"
-        
+
         return self.get_campaign_tags(campaign=campaign, filter=filter)
-    
+
     def insert_new_tag(self, tag_name, tag_field, tag_section, campaign=None):
         """
         Load a new tag value into Action Builder. Required before applying the value to any entity
@@ -141,19 +145,21 @@ class ActionBuilder(object):
         `Returns:`
             Dict containing Action Builder tag data.
         """
-        
+
         campaign = self._campaign_check(campaign)
-        url = f'campaigns/{campaign}/tags'
-        
+        url = f"campaigns/{campaign}/tags"
+
         data = {
             "name": tag_name,
             "action_builder:field": tag_field,
-            "action_builder:section": tag_section
+            "action_builder:section": tag_section,
         }
-        
+
         return self.api.post_request(url=url, data=json.dumps(data))
-    
-    def upsert_entity(self, entity_type=None, identifiers=None, data=None, campaign=None):
+
+    def upsert_entity(
+        self, entity_type=None, identifiers=None, data=None, campaign=None
+    ):
         """
         Load or update an entity record in Action Builder based on whether any identifiers are
         passed.
@@ -177,29 +183,31 @@ class ActionBuilder(object):
                 retrieved or edited. Not necessary if supplied when instantiating the class.
         `Returns:`
             Dict containing Action Builder entity data.
-        """ # noqa: E501
-        
+        """
+
         # Check that we have appropriate entity type/identifier, name, and campaign first
         if entity_type == identifiers == None:
-            error_msg = 'Must provide either entity_type (to insert a new record) '
-            error_msg += 'or identifiers (to update an existing record)'
+            error_msg = "Must provide either entity_type (to insert a new record) "
+            error_msg += "or identifiers (to update an existing record)"
             raise ValueError(error_msg)
-            
+
         if not isinstance(data, dict):
             data = {}
-            
-        name_check = [key for key in data.get('person', {}) if key in ('name', 'given_name')]
+
+        name_check = [
+            key for key in data.get("person", {}) if key in ("name", "given_name")
+        ]
         if identifiers is None and not name_check:
-            raise ValueError('Must provide name or given name if inserting new record')
-            
+            raise ValueError("Must provide name or given name if inserting new record")
+
         campaign = self._campaign_check(campaign)
-            
-        url = f'campaigns/{campaign}/people'
-            
-        if 'person' not in data:
+
+        url = f"campaigns/{campaign}/people"
+
+        if "person" not in data:
             # The POST data must live inside of person key
-            data['person'] = {}
-            
+            data["person"] = {}
+
         if identifiers:
             # Updating an existing record
 
@@ -208,16 +216,20 @@ class ActionBuilder(object):
                 identifiers = [identifiers]
 
             # Default to assuming identifier comes from Action Builder and add prefix if missing
-            identifiers = [f'action_builder:{x}' if ':' not in x else x for x in identifiers]
-            data['person']['identifiers'] = identifiers
-        
+            identifiers = [
+                f"action_builder:{x}" if ":" not in x else x for x in identifiers
+            ]
+            data["person"]["identifiers"] = identifiers
+
         if entity_type:
             # Inserting a new record
-            data['person']['action_builder:entity_type'] = entity_type
+            data["person"]["action_builder:entity_type"] = entity_type
 
         return self.api.post_request(url=url, data=json.dumps(data))
-    
-    def add_tags_to_record(self, identifiers, tag_name, tag_field, tag_section, campaign=None):
+
+    def add_tags_to_record(
+        self, identifiers, tag_name, tag_field, tag_section, campaign=None
+    ):
         """
         Add a tag (i.e. custom field value) to an existing entity record in Action Builder. The
         tags, along with their category/field and section/group, must already exist (except for
@@ -243,40 +255,49 @@ class ActionBuilder(object):
         `Returns:`
             Dict containing Action Builder entity data of the entity being tagged.
         """
-        
+
         # Ensure all tag args are lists
         tag_name = tag_name if isinstance(tag_name, list) else [tag_name]
         tag_field = tag_field if isinstance(tag_field, list) else [tag_field]
         tag_section = tag_section if isinstance(tag_section, list) else [tag_section]
-        
+
         # Use lists of tuples to identify length ordering in case amounts differ
         lengths = []
-        lengths.append(('name', len(tag_name))) # Most specific - should be >= other lengths
-        lengths.append(('field', len(tag_field)))
-        lengths.append(('section', len(tag_section))) # Most generic - should be <= other lengths
+        lengths.append(
+            ("name", len(tag_name))
+        )  # Most specific - should be >= other lengths
+        lengths.append(("field", len(tag_field)))
+        lengths.append(
+            ("section", len(tag_section))
+        )  # Most generic - should be <= other lengths
 
         # Sort by length, maintaining original order in ties
         ordered_lengths = sorted(lengths, key=lambda x: x[1], reverse=True)
         sorted_keys = [x[0] for x in ordered_lengths]
-        
+
         # Raise an error if there are fewer specific items provided than generic
-        if sorted_keys[0] != 'name':
-            raise ValueError('Not enough tag_names provided for tag_fields or tag_sections')
-            
-        if sorted_keys[1] != 'field':
-            raise ValueError('Not enough tag_fields provided for tag_sections')
-            
+        if sorted_keys[0] != "name":
+            raise ValueError(
+                "Not enough tag_names provided for tag_fields or tag_sections"
+            )
+
+        if sorted_keys[1] != "field":
+            raise ValueError("Not enough tag_fields provided for tag_sections")
+
         # Construct tag data, applying final generic taxonomy to any excess specific items
-        tag_data = [{
-            "action_builder:name": x,
-            "action_builder:field": tag_field[min(i, len(tag_field) - 1)],
-            "action_builder:section": tag_section[min(i, len(tag_section) - 1)],
-        } for i, x in enumerate(tag_name)]
-                       
+        tag_data = [
+            {
+                "action_builder:name": x,
+                "action_builder:field": tag_field[min(i, len(tag_field) - 1)],
+                "action_builder:section": tag_section[min(i, len(tag_section) - 1)],
+            }
+            for i, x in enumerate(tag_name)
+        ]
+
         data = {"add_tags": tag_data}
-                       
+
         return self.upsert_entity(identifiers=identifiers, data=data, campaign=campaign)
-    
+
     def upsert_connection(self, identifiers, tag_data=None, campaign=None):
         """
         Load or update a connection record in Action Builder between two existing entity records.
@@ -296,33 +317,33 @@ class ActionBuilder(object):
                 retrieved or edited. Not necessary if supplied when instantiating the class.
         `Returns:`
             Dict containing Action Builder connection data.
-        """ # noqa: E501
-        
+        """
+
         # Check that there are exactly two identifiers and that campaign is provided first
         if not isinstance(identifiers, list):
-            raise ValueError('Must provide identifiers as a list')
-            
+            raise ValueError("Must provide identifiers as a list")
+
         if len(identifiers) != 2:
-            raise ValueError('Must provide exactly two identifiers')
-            
+            raise ValueError("Must provide exactly two identifiers")
+
         campaign = self._campaign_check(campaign)
-        
-        url = f'campaigns/{campaign}/people/{identifiers[0]}/connections'
-        
+
+        url = f"campaigns/{campaign}/people/{identifiers[0]}/connections"
+
         data = {
             "connection": {
                 # person_id is used even if entity is not Person
                 "person_id": identifiers[1]
             }
         }
-        
+
         if tag_data:
             if isinstance(tag_data, dict):
                 tag_data = [tag_data]
-                
+
             if not isinstance(tag_data[0], dict):
-                raise ValueError('Must provide tag_data as a dict or list of dicts')
-                
+                raise ValueError("Must provide tag_data as a dict or list of dicts")
+
             data["add_tags"] = tag_data
 
         return self.api.post_request(url=url, data=json.dumps(data))

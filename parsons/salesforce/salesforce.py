32c219be08aa980a8eb1c2da19b5c71fab2c9c63
Sophie Waldman <62553142+sjwmoveon@users.noreply.github.com>
2023-06-02 16:10:07 -0400

    Fix Salesforce query output
    The existing code did not match the method description and did not produce a useful result. Salesforce returns a set of nested dicts, similar to JSON; the new method reformats the nested ordered dicts using the json library, similar to describe_fields.

--- parsons/salesforce/salesforce.py
+++ parsons/salesforce/salesforce.py
@@ -80,9 +80,9 @@ class Salesforce:
             list of dicts with Salesforce data
         """  # noqa: E501,E261
 
-        q = Table(self.client.query_all(soql))
-        logger.info(f"Found {q.num_rows} results")
-        return q
+        q = self.client.query_all(soql)
+        q=json.loads(json.dumps(q))
+        logger.info(f"Found {q['totalSize']} results")
 
     def insert_record(self, object, data_table):
         """

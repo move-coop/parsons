64241f8252df22b5748b5492706b04aa9a0a7de2
elyse-weiss <elyse@movementcooperative.org>
2023-06-06 09:58:48 -0500

    Revert "Drop dependencies for cols needs a CASCADE  (#819)"
    This reverts commit 78d7f86ba26487b014223d4c64be5530fc9f40c8.

--- parsons/databases/redshift/redshift.py
+++ parsons/databases/redshift/redshift.py
@@ -221,6 +221,7 @@ class Redshift(
         #        rows in the correct order
 
         with self.cursor(connection) as cursor:
+
             if "credentials" not in sql:
                 logger.debug(f"SQL Query: {sql}")
             cursor.execute(sql, parameters)
@@ -234,6 +235,7 @@ class Redshift(
                 return None
 
             else:
+
                 # Fetch the data in batches, and "pickle" the rows to a temp file.
                 # (We pickle rather than writing to, say, a CSV, so that we maintain
                 # all the type information for each field.)
@@ -395,6 +397,7 @@ class Redshift(
         """
 
         with self.connection() as connection:
+
             if self._create_table_precheck(connection, table_name, if_exists):
                 if template_table:
                     sql = f"CREATE TABLE {table_name} (LIKE {template_table})"
@@ -617,6 +620,7 @@ class Redshift(
             cols = None
 
         with self.connection() as connection:
+
             # Check to see if the table exists. If it does not or if_exists = drop, then
             # create the new table.
             if self._create_table_precheck(connection, table_name, if_exists):
@@ -855,6 +859,7 @@ class Redshift(
         # Generate manifest file
         manifest = {"entries": []}
         for bucket in buckets:
+
             # Retrieve list of files in bucket
             key_list = s3.list_keys(bucket, prefix=prefix)
             for key in key_list:
@@ -977,6 +982,7 @@ class Redshift(
                 raise ValueError("Primary key column contains duplicate values.")
 
         with self.connection() as connection:
+
             try:
                 # Copy to a staging table
                 logger.info(f"Building staging table: {staging_tbl}")
@@ -1081,9 +1087,7 @@ class Redshift(
             tbl = self.query_with_connection(sql_depend, connection)
             dropped_views = [row["table_name"] for row in tbl]
             if dropped_views:
-                sql_drop = "\n".join(
-                    [f"drop view {view} CASCADE;" for view in dropped_views]
-                )
+                sql_drop = "\n".join([f"drop view {view};" for view in dropped_views])
                 tbl = self.query_with_connection(sql_drop, connection)
                 logger.info(f"Dropped the following views: {dropped_views}")
 

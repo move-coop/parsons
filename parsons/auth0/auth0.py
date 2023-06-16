71a96728b0745748995566e39055ca7f402c34c7
talevy42 <85844932+talevy42@users.noreply.github.com>
2023-06-16 11:07:33 -0700

    feat(auth0): Add upsert_user function (#828)
    * feat(auth0): Add upsert_user function
    
    * chore(black): Fix formatting
    
    * chore(test): Create auth0.upsert_user test
    
    * chore(gitignore): ignore swp files
    
    * test(auth0): get correct tests
    
    ---------
    
    Co-authored-by: Tal Levy <tal@secondspectrum.com>

--- parsons/auth0/auth0.py
+++ parsons/auth0/auth0.py
@@ -1,6 +1,8 @@
+import json
+
+import requests
 from parsons.etl.table import Table
 from parsons.utilities import check_env
-import requests
 
 
 class Auth0(object):
@@ -71,3 +73,59 @@ class Auth0(object):
                 params={"email": email},
             ).json()
         )
+
+    def upsert_user(
+        self,
+        email,
+        username=None,
+        given_name=None,
+        family_name=None,
+        app_metadata={},
+        user_metadata={},
+    ):
+        """
+        Upsert Auth0 users by email.
+
+        `Args:`
+            email: str
+                The user email of the record to get.
+            username: optional str
+                Username to set for user
+            given_name: optional str
+                Given to set for user
+            family_name: optional str
+                Family name to set for user
+            app_metadata: optional dict
+                App metadata to set for user
+            user_metadata: optional dict
+                User metadata to set for user
+        `Returns:`
+            Requests Response object
+        """
+        payload = json.dumps(
+            {
+                "email": email.lower(),
+                "given_name": given_name,
+                "family_name": family_name,
+                "username": username,
+                "connection": "Username-Password-Authentication",
+                "app_metadata": app_metadata,
+                "blocked": False,
+                "user_metadata": user_metadata,
+            }
+        )
+        existing = self.get_users_by_email(email.lower())
+        if existing.num_rows > 0:
+            a0id = existing[0]["user_id"]
+            ret = requests.patch(
+                f"{self.base_url}/api/v2/users/{a0id}",
+                headers=self.headers,
+                data=payload,
+            )
+        else:
+            ret = requests.post(
+                f"{self.base_url}/api/v2/users", headers=self.headers, data=payload
+            )
+        if ret.status_code != 200:
+            raise ValueError(f"Invalid response {ret.json()}")
+        return ret

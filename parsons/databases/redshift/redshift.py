0e694268f32cae7118addbcd97a4e52b8419a26e
Kasia Hinkson <52927664+KasiaHinkson@users.noreply.github.com>
2023-06-22 13:02:49 -0500

    debug

--- parsons/databases/redshift/redshift.py
+++ parsons/databases/redshift/redshift.py
@@ -428,6 +428,7 @@ class Redshift(
                         columntypes=columntypes,
                         strict_length=strict_length,
                     )
+                    logger.info(f"Create statement: {sql}")
 
                 self.query_with_connection(sql, connection, commit=False)
                 logger.info(f"{table_name} created.")
@@ -458,6 +459,7 @@ class Redshift(
                 timeformat=timeformat,
                 bucket_region=bucket_region,
             )
+            logger.info(f"Copy statement: {copy_sql}")
 
             self.query_with_connection(copy_sql, connection, commit=False)
             logger.info(f"Data copied to {table_name}.")

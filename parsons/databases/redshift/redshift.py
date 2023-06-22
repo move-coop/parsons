98042e96281647528203670b7a082c85fc062ec5
sharinetmc <sharine.f@movementcooperative.org>
2023-06-22 10:14:54 -1000

    fix linting issue

--- parsons/databases/redshift/redshift.py
+++ parsons/databases/redshift/redshift.py
@@ -414,7 +414,8 @@ class Redshift(
 
                     local_path = s3.get_file(bucket, key)
                     if data_type == "csv":
-                        tbl = Table.from_csv(local_path, delimiter=csv_delimiter, encoding=encoding)
+                        tbl = Table.from_csv(local_path, delimiter=csv_delimiter, 
+                                             encoding=encoding)
                     else:
                         raise TypeError("Invalid data type provided")
 

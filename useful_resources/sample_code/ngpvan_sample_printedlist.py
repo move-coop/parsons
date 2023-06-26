249a7fecac2747a4464820bf2acf339288553de1
sharinetmc <sharine.f@movementcooperative.org>
2023-06-26 08:53:09 -1000

    black formatting

--- useful_resources/sample_code/ngpvan_sample_printedlist.py
+++ useful_resources/sample_code/ngpvan_sample_printedlist.py
@@ -31,29 +31,42 @@ def get_info_from_van(van_folder):
         dataframe
     """
     # Instantiate class
-    van = VAN(db='MyVoters')
+    van = VAN(db="MyVoters")
 
     # Get folder_id for inputed van_folder
     folders = van.get_folders().to_dataframe()
     folder_id = folders.folderId[folders.name == van_folder]
 
     # Get printed list number and turf/list name for all lists in van_folder
-    printed_lists = van.get_printed_lists(folder_name=van_folder).to_dataframe()[['number', 'name']]
+    printed_lists = van.get_printed_lists(folder_name=van_folder).to_dataframe()[
+        ["number", "name"]
+    ]
 
     # Get precinct name, turf door count, and turf people count for each turf in van_folder
     # by calling the saved_list methods
-    saved_lists_raw = van.get_saved_lists(
-        folder_id=folder_id).to_dataframe().drop_duplicates(subset=['name'])
-    saved_lists = saved_lists_raw[saved_lists_raw.name.str.contains('Turf')]
+    saved_lists_raw = (
+        van.get_saved_lists(folder_id=folder_id)
+        .to_dataframe()
+        .drop_duplicates(subset=["name"])
+    )
+    saved_lists = saved_lists_raw[saved_lists_raw.name.str.contains("Turf")]
 
     # Merge the printed list information with the saved list information into a single dataframe
-    saved_printed_merged_raw = saved_lists[[
-        'name', 'listCount', 'doorCount']].merge(printed_lists, on='name')
+    saved_printed_merged_raw = saved_lists[["name", "listCount", "doorCount"]].merge(
+        printed_lists, on="name"
+    )
     saved_printed_merged_renamed = saved_printed_merged_raw.rename(
-        columns={'name': 'Turf Name', 'listCount': 'Person Count', 'doorCount': 'Door Count', 'number': 'List Number'})
-    saved_printed_merged = saved_printed_merged_renamed.drop(columns=['name'])
+        columns={
+            "name": "Turf Name",
+            "listCount": "Person Count",
+            "doorCount": "Door Count",
+            "number": "List Number",
+        }
+    )
+    saved_printed_merged = saved_printed_merged_renamed.drop(columns=["name"])
 
     return saved_printed_merged
+
 
 # ~~~~~~~~~~~~~~ Send master checklist to Google Sheets ~~~~~~~~~~~~~~~~#
 
@@ -75,7 +88,10 @@ def to_gsheet(saved_printed_merged, gsheet_uri):
     sheets = GoogleSheets()
 
     # Send checklist info (i.e. saved_printed_merged dataframe) to google sheet
-    sheets.overwrite_sheet(gsheet_uri, Table.from_dataframe(saved_printed_merged), worksheet='all')
+    sheets.overwrite_sheet(
+        gsheet_uri, Table.from_dataframe(saved_printed_merged), worksheet="all"
+    )
+
 
 # ~~~~~~~~~~~~~~ Generate pdf printouts ~~~~~~~~~~~~~~~~#
 
@@ -97,22 +113,22 @@ def to_pdf(saved_printed_merged):
 
         precinct_txt = f"Turf Name: {row[0]}"
         pdf.set_font_size(28)
-        pdf.cell(w=200, h=15, txt=precinct_txt, ln=1, align='C')
+        pdf.cell(w=200, h=15, txt=precinct_txt, ln=1, align="C")
 
         count_txt = f"People: {row[1]}    Doors: {row[2]}"
         pdf.set_font_size(23)
-        pdf.cell(w=200, h=15, txt=count_txt, ln=1, align='C')
+        pdf.cell(w=200, h=15, txt=count_txt, ln=1, align="C")
 
         list_txt = f"List Number: {row[3]}"
         pdf.set_font_size(19)
-        pdf.cell(w=200, h=15, txt=list_txt, ln=1, align='C')
+        pdf.cell(w=200, h=15, txt=list_txt, ln=1, align="C")
 
-    pdf.output('printouts.pdf')
+    pdf.output("printouts.pdf")
 
 
 @click.command()
-@click.option('--van_folder')
-@click.option('--gsheet_uri')
+@click.option("--van_folder")
+@click.option("--gsheet_uri")
 def main(van_folder, gsheet_uri):
     """
     Loads turf data from NGP VAN to ghseet and creates PDF printouts

8169e87ba44d14c3b3b3022a89e446bd9c0efb60
Brittany Bennett <thebrittanybennett@gmail.com>
2023-07-22 14:51:31 -0400

    param test returning empty dictionary

--- test/test_strive/test_strive.py
+++ test/test_strive/test_strive.py
@@ -1,4 +1,5 @@
 import unittest
+from parsons.utilities.api_connector import APIConnector
 from parsons import Table, Strive
 import os 
 import requests_mock
@@ -16,28 +17,35 @@ class TestConnector(unittest.TestCase):
 
     @requests_mock.Mocker()
     def test_get_members(self, m):
-        headers = {'Authorization': 'Bearer test'}
-        m.get(self.base_uri + '/members', json=mock_member_data, headers=headers)
+        m.get(self.base_uri + '/members', json=mock_member_data)
         response = self.connector.get_members()
-        history = m.request_history[0]
-        import pytest; pytest.set_trace()
-        print(history)
+        history = m.request_history[0] # Get the first (and only) request made
+        # Assert that a get request was made
+        self.assertEqual(history.method, 'GET')
+        # Assert mock object has been called
         assert m.called == True  
+        # Assert mock data was successfully passed and retrieved 
         assert_matching_tables(response, Table(mock_member_data))
-        assert "Bearer test" == history.headers.get("Authorization")
+        # Assert headers passed correctly 
+        assert f"Bearer {self.api_key}" == history.headers.get("Authorization")
 
         
+    
+        # Define the data to be returned by the mock request
+        m.get(self.base_uri + '/members', json=mock_member_data)        
+        # Call the function that makes the HTTP request (replace get_members with the actual function name)
+        response = self.connector.get_members(first_name='eq.brittany', city='eq.Pittsburgh')
+        import pytest; pytest.set_trace()
+        history = m.request_history[0]
+        assert history.qs == {'first_name':'eq.brittany', 'city': 'eq.Pittsburgh'}
 
-        # Want to be sure that Auth header is being passed correctly  
-        # PARAMS are being passed through correctly 
-        # pass in params and assert that those were passed in correctly 
-        # asserting response.params == params, check mock requests documentation 
-        # 
-        # Live tests
-        # Instead of asserting table values 
-        # 
-        # User validate_list to assert that live tests return right data 
-        #  
+
+
+
+
+
+
+      
 
 
         

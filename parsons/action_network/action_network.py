c139dd03f815f7609e8a620ea24d94b95764721b
mattkrausse <106627640+mattkrausse@users.noreply.github.com>
2023-11-08 10:22:15 -0500

    Adds parameter to get_messages
    This adds the ability to unpack the statistics which are returned as a nested dictionary in the response.

--- parsons/action_network/action_network.py
+++ parsons/action_network/action_network.py
@@ -583,7 +583,7 @@ class ActionNetwork(object):
         return self.api.get_request(f"lists/{list_id}")
 
     # Messages
-    def get_messages(self, limit=None, per_page=25, page=None, filter=None):
+    def get_messages(self, limit=None, per_page=25, page=None, filter=None, unpack_statistics=True):
         """
         `Args:`
             limit:
@@ -595,14 +595,23 @@ class ActionNetwork(object):
            filter:
                The OData query for filtering results. E.g. "modified_date gt '2014-03-25'".
                When None, no filter is applied.
+           unpack_statistics:
+                Whether to unpack the statistics dictionary into the table. Default to True.
 
 
         `Returns:`
-            A  JSON with all the messages related entries
+            A Parsons Table with all the messages related entries
         """
+        # Fetch messages
         if page:
-            return self._get_page("messages", page, per_page, filter)
-        return self._get_entry_list("messages", limit, per_page, filter)
+            return self._get_page("messages", page, per_page)
+        tbl = self._get_entry_list("messages", limit, per_page)
+
+        # Unpack statistics
+        if unpack_statistics:
+            tbl.unpack_dict("statistics", prepend=False)
+
+        return tbl
 
     def get_message(self, message_id):
         """

502a3f3bf9ab51391fbd904d48b07bdbb91c3a43
mattkrausse <106627640+mattkrausse@users.noreply.github.com>
2023-11-14 11:23:33 -0800

    updated tests

--- test/test_quickbooks/test_quickbooks.py
+++ test/test_quickbooks/test_quickbooks.py
@@ -12,24 +12,30 @@ class TestQuickBooks(unittest.TestCase):
     def setUp(self, mock_request):
         self.qb = QuickBooks(token="abc123")
         self.qb.url = "https://rest.tsheets.com/api/v1/"
+
     
     def tearDown(self):
         pass
 
+    
     @requests_mock.Mocker()
-    def test_qb_get_request(self, mock_request):
+    def test_qb_get_request(self, mock_request, end_point=None):
         # Arrange
-        end_point = "groups"
+
+        # If no end_point is passed, it will default to the groups endpoint
+        if end_point is None:
+            end_point = "groups"
+
         querystring = {"page": 1}
         mock_request.get(requests_mock.ANY, json=mock_group_data)
 
         # Act
-        result = self.qb.qb_get_request(end_point, querystring)
+        result = self.qb.qb_get_request(end_point=end_point, querystring=querystring)
 
         # Assert
         self.assertIsInstance(result, Table)
-        
-        # TODO: while loop, adding page+1, etc.
+        self.assertIsInstance(end_point, str)
+        self.assertIsInstance(querystring, dict)
 
     @requests_mock.Mocker()
     def test_get_groups(self, mock_request):
@@ -45,7 +51,7 @@ class TestQuickBooks(unittest.TestCase):
     @requests_mock.Mocker()
     def test_get_jobcodes(self, mock_request):
         # Arrange
-        mock_request.get(requests_mock.ANY, json=mock_group_data) # TODO: Change data
+        mock_request.get(requests_mock.ANY, json=mock_jobcodes_data)
 
         # Act
         result = self.qb.get_jobcodes()
@@ -56,10 +62,199 @@ class TestQuickBooks(unittest.TestCase):
     @requests_mock.Mocker()
     def test_get_timesheets(self, mock_request):
         # Arrange
-        mock_request.get(requests_mock.ANY, json=mock_group_data) # TODO: Change data
+        mock_request.get(requests_mock.ANY, json=mock_timesheets_data)
 
         # Act
         result = self.qb.get_timesheets()
+
+        # Assert
+        self.assertIsInstance(result, Table)
+
+    @requests_mock.Mocker()
+    def test_get_users(self, mock_request):
+        # Arrange
+        mock_request.get(requests_mock.ANY, json=mock_user_data)
+
+        # Act
+        result = self.qb.get_users()
+
+        # Assert
+        self.assertIsInstance(result, Table)
+
+    @requests_mock.Mocker()
+    def test_get_schedule_calendars_list(self, mock_request):
+        # Arrange
+        mock_request.get(requests_mock.ANY, json=mock_group_data)
+
+        # Act
+        result = self.qb.get_schedule_calendars_list()
+
+        # Assert
+        self.assertIsInstance(result, Table)
+
+    @requests_mock.Mocker()
+    def test_get_schedule_events(self, mock_request):
+        # Arrange
+        mock_request.get(requests_mock.ANY, json=mock_group_data)
+
+        # Act
+        result = self.qb.get_schedule_events()
+
+        # Assert
+        self.assertIsInstance(result, Table)
+
+    @requests_mock.Mocker()
+    def test_get_jobcodes_with_params(self, mock_request):
+        # Arrange
+        mock_request.get(requests_mock.ANY, json=mock_jobcodes_data)
+        ids = [1, 2, 3]
+        parent_ids = [4, 5, 6]
+        name = "test"
+        type = "test"
+        active = True
+        customfields = {"test": "test"}
+        modified_before = "2022-01-01"
+        modified_since = "2022-01-01"
+        supplemental_data = True
+        per_page = 10
+        page = 1
+
+        # Act
+        result = self.qb.get_jobcodes(
+            ids=ids,
+            parent_ids=parent_ids,
+            name=name,
+            type=type,
+            active=active,
+            customfields=customfields,
+            modified_before=modified_before,
+            modified_since=modified_since,
+            supplemental_data=supplemental_data,
+            per_page=per_page,
+            page=page,
+        )
+
+        # Assert
+        self.assertIsInstance(result, Table)
+
+    @requests_mock.Mocker()
+    def test_get_users_with_params(self, mock_request):
+        # Arrange
+        mock_request.get(requests_mock.ANY, json=mock_user_data)
+        ids = [1, 2, 3]
+        not_ids = [4, 5, 6]
+        employee_numbers = [7, 8, 9]
+        usernames = ["test1", "test2", "test3"]
+        group_ids = [10, 11, 12]
+        not_group_ids = [13, 14, 15]
+        payroll_ids = [16, 17, 18]
+        active = True
+        first_name = "test"
+        last_name = "test"
+        modified_before = "2022-01-01"
+        modified_since = "2022-01-01"
+        supplemental_data = True
+        per_page = 10
+        page = 1
+
+        # Act
+        result = self.qb.get_users(
+            ids=ids,
+            not_ids=not_ids,
+            employee_numbers=employee_numbers,
+            usernames=usernames,
+            group_ids=group_ids,
+            not_group_ids=not_group_ids,
+            payroll_ids=payroll_ids,
+            active=active,
+            first_name=first_name,
+            last_name=last_name,
+            modified_before=modified_before,
+            modified_since=modified_since,
+            supplemental_data=supplemental_data,
+            per_page=per_page,
+            page=page,
+        )
+
+        # Assert
+        self.assertIsInstance(result, Table)
+
+    @requests_mock.Mocker()
+    def test_get_timesheets_with_params(self, mock_request):
+        # Arrange
+        mock_request.get(requests_mock.ANY, json=mock_timesheets_data)
+        ids = [1, 2, 3]
+        jobcode_ids = [4, 5, 6]
+        payroll_ids = [7, 8, 9]
+        user_ids = [10, 11, 12]
+        group_ids = [13, 14, 15]
+        end_date = "2022-01-01"
+        on_the_clock = True
+        jobcode_type = "test"
+        modified_before = "2022-01-01"
+        modified_since = "2022-01-01"
+        supplemental_data = True
+        per_page = 10
+        page = 1
+        start_date = "2022-01-01"
+
+        # Act
+        result = self.qb.get_timesheets(
+            ids=ids,
+            jobcode_ids=jobcode_ids,
+            payroll_ids=payroll_ids,
+            user_ids=user_ids,
+            group_ids=group_ids,
+            end_date=end_date,
+            on_the_clock=on_the_clock,
+            jobcode_type=jobcode_type,
+            modified_before=modified_before,
+            modified_since=modified_since,
+            supplemental_data=supplemental_data,
+            per_page=per_page,
+            page=page,
+            start_date=start_date,
+        )
+
+        # Assert
+        self.assertIsInstance(result, Table)
+
+    @requests_mock.Mocker()
+    def test_get_schedule_events_with_params(self, mock_request):
+        # Arrange
+        mock_request.get(requests_mock.ANY, json=mock_group_data)
+        ids = [1, 2, 3]
+        users_ids = [4, 5, 6]
+        schedule_calendar_ids = [7, 8, 9]
+        jobcode_ids = [10, 11, 12]
+        start = "2022-01-01"
+        end = "2022-01-01"
+        active_users = True
+        active = True
+        draft = True
+        team_events = True
+        modified_before = "2022-01-01"
+        modified_since = "2022-01-01"
+        supplemental_data = True
+        per_page = 10
+
+        # Act
+        result = self.qb.get_schedule_events(
+            ids=ids,
+            users_ids=users_ids,
+            schedule_calendar_ids=schedule_calendar_ids,
+            jobcode_ids=jobcode_ids,
+            start=start,
+            end=end,
+            active_users=active_users,
+            active=active,
+            draft=draft,
+            team_events=team_events,
+            modified_before=modified_before,
+            modified_since=modified_since,
+            supplemental_data=supplemental_data,
+            per_page=per_page,
+        )
 
         # Assert
         self.assertIsInstance(result, Table)

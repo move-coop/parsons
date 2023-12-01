d812efc19da3216a4cc9d662114e01ba630c1bd1
Shauna Gordon-McKeon <shaunagm@gmail.com>
2023-12-01 15:10:45 -0500

    Bump version number to 3.0.0

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ def main():
 
     setup(
         name="parsons",
-        version="2.1.0",
+        version="3.0.0",
         author="The Movement Cooperative",
         author_email="info@movementcooperative.org",
         url="https://github.com/move-coop/parsons",

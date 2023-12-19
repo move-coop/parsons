54a7f763f047891fa07a27856dafd70c8907aaf8
sharinetmc <sharine.f@movementcooperative.org>
2023-12-19 09:50:19 -1000

    add functions

--- parsons/civis/civisclient.py
+++ parsons/civis/civisclient.py
@@ -142,3 +142,115 @@ class CivisClient(object):
             return fut.result()
 
         return fut
+
+    def update_sql_in_civis_script(self, civis_script_id, query):
+        """
+        This function updates the SQL within a SQL script
+        `Args`
+            id: int
+                Civis SQL Script ID
+            query: str
+                The query to replace existing SQL with
+        `Returns`
+            None
+        """
+
+        self.client.scripts.patch_sql(civis_script_id, sql=query)
+        return None
+
+    def get_sql_from_civis_script(self, civis_script_id):
+        """
+        This function grabs the SQL from a Civis SQL Script.
+
+        `Args`
+            civis_script_id: int
+                the Civis SQL Script ID
+
+        `Returns`
+            The SQL included in the Civis SQL Script.
+        """
+
+        sql_script = self.client.scripts.get_sql(civis_script_id)
+
+        return sql_script["sql"]
+
+    def get_sql_from_civis_project(self, civis_project_id):
+        """
+        This function allows you to grab all of the SQL from Civis SQL
+        Scripts in a given Civis Project.
+
+        `Args`
+            civis_project_id: int
+                the Civis Project ID
+
+        `Returns`
+            A Parsons Table with all of the relevant SQL.
+        """
+
+        sql_scripts = self.client.projects.get(civis_project_id)
+        sql_dicts = [
+            {
+                "object_name": x["name"],
+                "sql": self.client.scripts.get_sql(x["object_id"])["sql"],
+            }
+            for x in sql_scripts["all_objects"]
+            if x["object_job_type"] == "Jobjob_types::SqlRunner"
+        ]
+        sql_table = Table(sql_dicts)
+
+        return sql_table
+
+    def trigger_existing_civis_sql_job(self, civis_script_id):
+        """
+        Runs an existing Civis SQL job.
+
+        `Args:`
+            civis_script_id: int
+
+        `Returns:`
+            object
+                The Civis Run ID
+        """
+        id = int(civis_script_id)
+        run = self.client.scripts.post_sql_runs(id)
+
+        return run.id
+
+    def trigger_existing_civis_container_job(self, civis_script_id):
+        """
+        Runs an existing Civis SQL job.
+
+        `Args:`
+            civis_script_id: int
+
+        `Returns:`
+            object
+                The Civis Run ID
+        """
+
+        id = int(civis_script_id)
+        run = self.client.scripts.post_containers_runs(id)
+
+        return run.id
+
+    def trigger_existing_civis_workflow(self, civis_wf_id):
+        """
+        Runs an existing Civis Workflow, waiting for it to complete before returning.
+        `Args:`
+            civis_wf_id: int
+        `Returns:`
+            object
+                The Civis Workflow Execution ID
+        """
+
+        id = int(civis_wf_id)
+        execute = self.client.workflows.post_executions(id)
+
+        # Wait on the workflow to finish
+        poller = self.client.workflows.get_executions
+        poller_args = (id, execute.id)
+        polling_interval = 10
+        future = self.futures.CivisFuture(poller, poller_args, polling_interval)
+        future.result()
+
+        return execute.id

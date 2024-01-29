83f918da6f1f237d9adb1e0b7b8d94167ee307de
Jason Walker <jason@indivisible.org>
2024-01-29 18:28:45 -0500

    Make some more changes

--- pipelines/parsons_pipelines.py
+++ pipelines/parsons_pipelines.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Callable, Optional, Union, Dict
+from typing import Callable, List, Optional, Union, Dict, TypeAlias
 from abc import ABC, abstractmethod
 from enum import Enum
 import os
@@ -10,6 +10,8 @@ from pathlib import Path
 import shutil
 
 PipeResult = Enum("PipeResult", ["Unit", "Serial", "Parallel"])
+StreamKey: TypeAlias = int
+StreamsData: TypeAlias = Dict[StreamKey, Table]
 
 
 class Logger(ABC):
@@ -28,7 +30,7 @@ class Logger(ABC):
         self.pipeline_offset += 1
 
     @abstractmethod
-    def log_data(self, data: Union[Table, Dict[int, Table]], pipe_name: str):
+    def log_data(self, data: Union[Table, StreamsData], pipe_name: str):
         pass
 
 
@@ -41,7 +43,7 @@ class CsvLogger(Logger):
         if not os.path.exists(self.dirname):
             os.makedirs(self.dirname)
 
-    def log_data(self, data: Table | Dict[int, Table], pipe_name: str):
+    def log_data(self, data: Table | StreamsData, pipe_name: str):
         if isinstance(data, Table):
             filename = f"{self.pipeline_offset}_{self.pipe_offset}_{pipe_name}.csv"
             data.to_csv(os.path.join(self.dirname, filename))
@@ -50,11 +52,41 @@ class CsvLogger(Logger):
                 self.log_data(data[i], f"{pipe_name}__stream_{i}")
 
 
+class BreakpointLogger(Logger):
+    def log_data(self, data: Table | StreamsData, pipe_name: str):
+        breakpoint()
+
+
+class Loggers(Logger):
+    def __init__(self, *loggers: Logger):
+        super().__init__()
+        self.loggers = loggers
+
+    def setup(self):
+        super().setup()
+        for logger in self.loggers:
+            logger.setup()
+
+    def next_pipe(self):
+        super().next_pipe()
+        for logger in self.loggers:
+            logger.next_pipe()
+
+    def next_pipeline(self):
+        super().next_pipeline()
+        for logger in self.loggers:
+            logger.next_pipeline()
+
+    def log_data(self, data: Table | StreamsData, pipe_name: str):
+        for logger in self.loggers:
+            logger.log_data(data, pipe_name)
+
+
 class PipeBuilder:
     name: str
     func: Callable
     first_pipe: PipeBuilder
-    next_pipe: Optional[Union[PipeBuilder, PipeBuilder]]
+    next_pipe: Optional[PipeBuilder]
     input_type: PipeResult
     result_type: PipeResult
 
@@ -74,7 +106,7 @@ class PipeBuilder:
         return PipeBuilder(self.name, self.input_type, self.result_type, self.func)
 
     def is_connected(self) -> bool:
-        return self.first_pipe != self
+        return self.first_pipe != self or self.next_pipe is not None
 
     def __call__(self, next: PipeBuilder) -> PipeBuilder:
         if self.next_pipe is not None:
@@ -83,8 +115,8 @@ class PipeBuilder:
             )
         if self.result_type != next.input_type:
             raise RuntimeError(
-                f"Cannot chain a {self.result_type} output pipe"
-                f" into a {next.input_type} input pipe."
+                f"Cannot chain a {self.result_type} output pipe [{self.name}]"
+                f" into a {next.input_type} input pipe [{next.name}]."
             )
 
         if not next.is_connected():
@@ -105,8 +137,8 @@ class Pipeline:
     def __init__(self, final_pipe: PipeBuilder):
         self.final_pipe = final_pipe
 
-    def run(self, logger: Optional[Logger]) -> Union[Table, Dict[int, Table]]:
-        data: Optional[Union[Table, Dict[int, Table]]] = None
+    def run(self, logger: Optional[Logger] = None) -> Union[Table, StreamsData]:
+        data: Optional[Union[Table, StreamsData]] = None
         pipe: Optional[PipeBuilder] = self.final_pipe.first_pipe
 
         if not pipe or pipe.input_type != PipeResult.Unit:
@@ -223,7 +255,8 @@ def load_lotr_books_from_api() -> Table:
 
 @define_pipe("convert")
 def convert(data: Table, *args, **kwargs) -> Table:
-    return data.convert_column(*args, **kwargs)
+    results = data.convert_column(*args, **kwargs)
+    return results
 
 
 @define_pipe("write_csv")
@@ -235,7 +268,8 @@ def write_csv(data: Table, csv_name: str) -> Table:
 @define_pipe("filter_rows")
 def filter_rows(data: Table, filters: Union[Callable, str, dict]) -> Table:
     if isinstance(filters, str) or isinstance(filters, Callable):
-        return data.select_rows(filters)
+        results = data.select_rows(filters)
+        return results
     else:
 
         def __select(row: dict) -> bool:
@@ -248,11 +282,12 @@ def filter_rows(data: Table, filters: Union[Callable, str, dict]) -> Table:
                         return False
             return True
 
-        return data.select_rows(__select)
+        results = data.select_rows(__select)
+        return results
 
 
 @define_pipe("split_data", result_type=PipeResult.Parallel)
-def split_data(data: Table, bucketing_func: Callable[[dict], int]) -> Dict[int, Table]:
+def split_data(data: Table, bucketing_func: Callable[[dict], StreamKey]) -> StreamsData:
     # TODO: Make this a unique column name
     index_col = "bucket_index"
     data.add_column(index_col, bucketing_func)
@@ -267,6 +302,14 @@ def split_data(data: Table, bucketing_func: Callable[[dict], int]) -> Dict[int, 
     return results
 
 
+@define_pipe("copy_data_into_streams", result_type=PipeResult.Parallel)
+def copy_data_into_streams(data: Table, *streams: StreamKey) -> StreamsData:
+    streams_data = {}
+    for i in streams:
+        streams_data[i] = Table(data.to_petl())
+    return streams_data
+
+
 @define_pipe("print_data")
 def print_data(data: Table, greeting: str) -> Table:
     print(greeting)
@@ -278,7 +321,7 @@ def print_data(data: Table, greeting: str) -> Table:
 @define_pipe(
     "all_streams", input_type=PipeResult.Parallel, result_type=PipeResult.Parallel
 )
-def all_streams(data: Dict[int, Table], inner_pipe: PipeBuilder) -> Dict[int, Table]:
+def all_streams(data: StreamsData, inner_pipe: PipeBuilder) -> StreamsData:
     if (
         inner_pipe.input_type != PipeResult.Serial
         or inner_pipe.input_type != PipeResult.Serial
@@ -291,24 +334,30 @@ def all_streams(data: Dict[int, Table], inner_pipe: PipeBuilder) -> Dict[int, Ta
     return data
 
 
+# TODO: Refactor the streams concept to return PipeBuilders that contain other
+# PipeBuilders, instead of running control flow themselves. This will allow the
+# control flow to be handled by the Pipeline object instead.
 @define_pipe(
     "for_streams", input_type=PipeResult.Parallel, result_type=PipeResult.Parallel
 )
 def for_streams(
-    data: Dict[int, Table], inner_pipes: Dict[int, PipeBuilder]
-) -> Dict[int, Table]:
+    data: StreamsData, inner_pipes: Dict[StreamKey, PipeBuilder]
+) -> StreamsData:
     for i in data.keys():
         if i in inner_pipes.keys():
-            inner_pipe = inner_pipes[i]
-            if (
-                inner_pipe.input_type != PipeResult.Serial
-                or inner_pipe.input_type != PipeResult.Serial
-            ):
-                raise RuntimeError(
-                    "Cannot run a non serial-to-serial pipe on all streams"
-                )
+            inner_pipe = inner_pipes[i].first_pipe
 
-            data[i] = inner_pipe.func(data[i])
+            while inner_pipe is not None:
+                if (
+                    inner_pipe.input_type != PipeResult.Serial
+                    or inner_pipe.input_type != PipeResult.Serial
+                ):
+                    raise RuntimeError(
+                        "Cannot run a non serial-to-serial pipe on all streams"
+                    )
+
+                data[i] = inner_pipe.func(data[i])
+                inner_pipe = inner_pipe.next_pipe
 
     return data
 
@@ -318,6 +367,8 @@ def main():
         "after_1975.csv",
         "before_1980.csv",
         "after_1979.csv",
+        "after_1980.csv",
+        "after_1990.csv",
         "lotr_books.csv",
     ]:
         if os.path.exists(csv):
@@ -362,13 +413,13 @@ def main():
         )(
             clean_year()
         )(
-            split_data(lambda row: 1 if row["Year"] >= 1980 else 0)
+            split_data(lambda row: "gte_1980" if row["Year"] >= 1980 else "lt_1980")
         )(
             all_streams(print_data("Split Data"))
         )(
             for_streams({
-                0: write_csv("before_1980.csv"),
-                1: write_csv("after_1979.csv")
+                "lt_1980": write_csv("before_1980.csv"),
+                "gte_1980": write_csv("after_1979.csv")
             })
         )
     )
@@ -380,10 +431,65 @@ def main():
         )
     )
 
-    dashboard = Dashboard(load_after_1975, split_on_1980, save_lotr_books, logger=CsvLogger())
-    dashboard.run()
-    dashboard.generate_report("report.html")
+    copy_into_streams_test = Pipeline(
+        load_from_csv("deniro.csv")
+        (
+            clean_year()
+        )(
+            copy_data_into_streams("0", "1")
+        )(
+            for_streams({
+                "0": filter_rows({
+                    "Year": lambda year: year > 1990
+                })(
+                    write_csv("after_1990.csv")
+                ),
+                "1": write_csv("all_years.csv")
+            })
+        )
+    )
+
+    dashboard = Dashboard(
+        split_on_1980,
+        save_lotr_books,
+        load_after_1975,
+        copy_into_streams_test,
+        logger=Loggers(
+            CsvLogger(),
+            # BreakpointLogger()
+        )
+    )
+    # dashboard.run()
+    # dashboard.generate_report("report.html")
     # fmt: on
+
+    load_after_1975_dask()
+
+
+import dask.dataframe as dd
+
+
+def load_after_1975_dask():
+    # Load data
+    data = dd.read_csv("deniro.csv", quotechar='"')
+
+    # Print raw data
+    print("Raw Data")
+    print(data.head(10))
+
+    # Clean year - remove None and convert to int
+    data = data[data["Year"].notnull()]
+    data["Year"] = data["Year"].astype(int)
+
+    # Filter rows where Year > 1975
+    data = data[data["Year"] > 1975]
+
+    # Print data after filtering
+    print("After 1975")
+    print(data.head(10))
+
+    # Write to CSV
+    data.to_csv("after_1975.csv", single_file=True)
 
 
 if __name__ == "__main__":

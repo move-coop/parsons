c3f44aed1c6f92226cb839eca2cc1f276c74350b
Josh Wenk <joshua.d.wenk@gmail.com>
2024-06-03 16:20:46 -0400

    test_twilio.py changes (in progress)

--- test/test_twilio/test_twilio.py
+++ test/test_twilio/test_twilio.py
@@ -17,40 +17,40 @@ class TestTwilio(unittest.TestCase):
 
         fake_sid = "FAKESID"
         self.twilio.get_account(fake_sid)
-        assert self.twilio.client.api.accounts.called_with(fake_sid)
+        self.twilio.client.api.accounts.assert_called_with(fake_sid)
 
     def test_get_accounts(self):
 
         self.twilio.get_accounts(name="MyOrg", status="active")
-        assert self.twilio.client.api.accounts.list.called_with(
+        self.twilio.client.api.accounts.list.assert_called_with(
             friendly_name="MyOrg", status="active"
         )
 
     def test_get_messages(self):
 
         self.twilio.get_messages(date_sent="2019-10-29")
-        assert self.twilio.client.messages.list.called_with(date_sent="2019-10-29")
+        self.twilio.client.messages.list.assert_called_with(to=None, from_=None, date_sent='2019-10-29', date_sent_before=None, date_sent_after=None)
 
     def test_get_account_usage(self):
 
         # Make sure that it is calling the correct Twilio methods
         self.twilio.get_account_usage(time_period="today")
-        assert self.twilio.client.usage.records.today.list.called_with(time_period="today")
+        self.twilio.client.usage.records.today.list.assert_called_with(time_period="today")
         self.twilio.get_account_usage(time_period="last_month")
-        assert self.twilio.client.usage.records.last_month.list.called_with(
+        self.twilio.client.usage.records.last_month.list.assert_called_with(
             time_period="last_month"
         )
         self.twilio.get_account_usage(time_period="this_month")
-        assert self.twilio.client.usage.records.this_month.list.called_with(
+        self.twilio.client.usage.records.this_month.list.assert_called_with(
             time_period="this_month"
         )
         self.twilio.get_account_usage(time_period="yesterday")
-        assert self.twilio.client.usage.records.today.list.called_with(time_period="yesterday")
+        self.twilio.client.usage.records.today.list.assert_called_with(time_period="yesterday")
 
         # Make sure that it is calling the correct Twilio methods
         self.twilio.get_account_usage(time_period="daily", start_date="10-19-2019")
-        assert self.twilio.client.usage.records.daily.list.called_with(start_date="10-19-2019")
+        self.twilio.client.usage.records.daily.list.assert_called_with(start_date="10-19-2019")
         self.twilio.get_account_usage(time_period="monthly", start_date="10-19-2019")
-        assert self.twilio.client.usage.records.monthly.list.called_with(start_date="10-19-2019")
+        self.twilio.client.usage.records.monthly.list.assert_called_with(start_date="10-19-2019")
         self.twilio.get_account_usage(time_period="yearly", start_date="10-19-2019")
-        assert self.twilio.client.usage.records.yearly.list.called_with(start_date="10-19-2019")
+        self.twilio.client.usage.records.yearly.list.assert_called_with(start_date="10-19-2019")

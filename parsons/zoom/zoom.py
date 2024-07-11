a8751c6022e3634fcb5267e9cd42df520af56ff9
Austin Weisgrau <austinweisgrau@gmail.com>
2024-07-11 16:28:47 -0700

    Enable date filter on Zoom get_meetings method

--- parsons/zoom/zoom.py
+++ parsons/zoom/zoom.py
@@ -1,5 +1,7 @@
+import datetime
 import logging
 import uuid
+from typing import Dict, Literal, Optional
 
 from parsons import Table
 from parsons.utilities import check_env
@@ -164,7 +166,15 @@ class Zoom:
         logger.info(f"Retrieved {tbl.num_rows} users.")
         return tbl
 
-    def get_meetings(self, user_id, meeting_type="scheduled"):
+    def get_meetings(
+        self,
+        user_id,
+        meeting_type: Literal[
+            "scheduled", "live", "upcoming", "upcoming_meetings", "previous_meetings"
+        ] = "scheduled",
+        from_date: Optional[datetime.date] = None,
+        to_date: Optional[datetime.date] = None,
+    ):
         """
         Get meetings scheduled by a user.
 
@@ -192,8 +202,13 @@ class Zoom:
             Parsons Table
                 See :ref:`parsons-table` for output options.
         """
+        params: Dict[str, str] = {"type": meeting_type}
+        if from_date:
+            params["from"] = from_date.isoformat()
+        if to_date:
+            params["to"] = to_date.isoformat()
 
-        tbl = self._get_request(f"users/{user_id}/meetings", "meetings")
+        tbl = self._get_request(f"users/{user_id}/meetings", "meetings", params=params)
         logger.info(f"Retrieved {tbl.num_rows} meetings.")
         return tbl
 

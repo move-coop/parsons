c248e20da89c9cb268d735e9afc9c0ef4a7c1a9f
Austin Weisgrau <austinweisgrau@gmail.com>
2024-07-31 09:34:24 -0700

    Enable GZIP on BigQuery extract to GCS

--- parsons/google/google_bigquery.py
+++ parsons/google/google_bigquery.py
@@ -1375,15 +1375,19 @@ class GoogleBigQuery(DatabaseConnector):
         gcs_bucket: str,
         gcs_blob_name: str,
         project: Optional[str] = None,
+        gzip: bool = False,
     ) -> None:
         dataset_ref = bigquery.DatasetReference(project or self.client.project, dataset)
         table_ref = dataset_ref.table(table_name)
         gs_destination = f"gs://{gcs_bucket}/{gcs_blob_name}"
 
-        extract_job = self.client.extract_table(
-            table_ref,
-            gs_destination,
-        )
+        if gzip:
+            job_config = bigquery.job.ExtractJobConfig()
+            job_config.compression = bigquery.Compression.GZIP
+        else:
+            job_config = None
+
+        extract_job = self.client.extract_table(table_ref, gs_destination, job_config=job_config)
         extract_job.result()  # Waits for job to complete.
 
         logger.info(f"Finished exporting query result to {gs_destination}.")

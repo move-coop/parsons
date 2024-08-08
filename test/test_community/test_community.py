c8438e9f0b2ffd7c7d6ff357805062a78b82ebe1
Kasia Hinkson <52927664+KasiaHinkson@users.noreply.github.com>
2024-08-08 15:03:36 -0500

    maybe this will magically work

--- test/test_community/test_community.py
+++ test/test_community/test_community.py
@@ -7,10 +7,8 @@ from unittest.mock import MagicMock
 TEST_CLIENT_ID = "someuuid"
 TEST_CLIENT_TOKEN = "somesecret"
 
-TEST_ID = "12345"
-
-TEST_CSV_TYPE = "campaigns"
-TEST_URI = f"https://faketestingurl.com/"
+TEST_FILENAME = "campaigns"
+TEST_URI = f"https://faketestingurl.com/{TEST_CLIENT_ID}"
 
 TEST_GET_RESPONSE = """\"LEADER_ID\",\"DATE_DAY\",\"OUTBOUND_MESSAGE_TYPE\",\"MESSAGE_COUNT\",\"SEGMENT_COUNT\"\n\"6e83b266-899f-4a01-b39c-e614a4929df7\",\"2022-10-03\",\"FAN_ONBOARDING\",1,3\n"""
 
@@ -38,16 +36,16 @@ class TestCommunity(unittest.TestCase):
 
     @requests_mock.Mocker()
     def test_successful_get_request(self, m):
-        m.get(f"{TEST_URI}/{TEST_CLIENT_ID}/{TEST_CSV_TYPE}.csv.gz", json=TEST_GET_RESPONSE)
+        m.get(f"{TEST_URI}/{TEST_CLIENT_ID}/{TEST_FILENAME}.csv.gz", json=TEST_GET_RESPONSE)
 
-        assert self.com.get_request(filename=TEST_CSV_TYPE) == TEST_GET_RESPONSE
+        assert self.com.get_request(filename=TEST_FILENAME) == TEST_GET_RESPONSE
 
     # test get resource
     @requests_mock.Mocker()
     def test_successful_get_data_export(self, m):
-        m.get(f"{TEST_URI}/{TEST_CLIENT_ID}/{TEST_CSV_TYPE}.csv.gz", json=TEST_GET_RESPONSE)
+        m.get(f"{TEST_URI}/{TEST_CLIENT_ID}/{TEST_FILENAME}.csv.gz", json=TEST_GET_RESPONSE)
 
         table = self.com.get_data_export(
-            TEST_CSV_TYPE,
+            TEST_FILENAME,
         )
         assert TEST_EXPECTED_COLUMNS == table.columns

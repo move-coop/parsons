a6812d078c04576fe37416ed8c2991ebfa2126ac
Shauna Gordon-McKeon <shaunagm@gmail.com>
2024-12-02 16:42:06 -0500

    Remove md5 security workaround (see issue #1138)

--- parsons/etl/etl.py
+++ parsons/etl/etl.py
@@ -659,21 +659,13 @@ class ETL(object):
             orig.concat(melted_list)
             # Add unique id column by hashing all the other fields
             if "uid" not in self.columns:
-                if sys.version_info.minor >= 9:
-                    orig.add_column(
-                        "uid",
-                        lambda row: hashlib.md5(
-                            str.encode("".join([str(x) for x in row])), usedforsecurity=False
-                        ).hexdigest(),
-                    )
-                elif sys.version_info.minor < 9:
-                    orig.add_column(
-                        "uid",
-                        lambda row: hashlib.md5(  # nosec B324
-                            str.encode("".join([str(x) for x in row]))
-                        ).hexdigest(),
-                    )
-                orig.move_column("uid", 0)
+                orig.add_column(
+                    "uid",
+                    lambda row: hashlib.md5(
+                        str.encode("".join([str(x) for x in row])), usedforsecurity=False
+                    ).hexdigest(),
+                )
+                orig.move_column("uid", 0)             
 
             # Rename value column in case this is done again to this Table
             orig.rename_column("value", f"{column}_value")
@@ -684,18 +676,12 @@ class ETL(object):
         else:
             orig = self.remove_column(column)
             # Add unique id column by hashing all the other fields
-            if sys.version_info.minor >= 9:
-                melted_list.add_column(
-                    "uid",
-                    lambda row: hashlib.md5(
-                        str.encode("".join([str(x) for x in row])), usedforsecurity=False
-                    ).hexdigest(),
-                )
-            elif sys.version_info.minor < 9:
-                melted_list.add_column(
-                    "uid",
-                    lambda row: hashlib.md5(str.encode("".join([str(x) for x in row]))).hexdigest(),  # nosec B324
-                )
+            melted_list.add_column(
+                "uid",
+                lambda row: hashlib.md5(
+                    str.encode("".join([str(x) for x in row])), usedforsecurity=False
+                ).hexdigest(),
+            )
             melted_list.move_column("uid", 0)
             output = melted_list
 

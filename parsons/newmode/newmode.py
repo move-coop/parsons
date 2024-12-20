b813a226e0bf8d8c893542428a89a742dc58cee1
sharinetmc <sharine.f@movementcooperative.org>
2024-12-19 16:06:06 -0800

    edit methods

--- parsons/newmode/newmode.py
+++ parsons/newmode/newmode.py
@@ -9,7 +9,8 @@ logger = logging.getLogger(__name__)
 
 API_URL_V1 = "https://engage.newmode.net/api/"
 API_URL_V2 = "https://base.newmode.net/api/"
-API_AUTH_URL = "https://base.newmode.net/oauth/token"
+API_AUTH_URL = "https://base.newmode.net/oauth/token/"
+API_CAMPAIGNS_V2_URL = "https://base.newmode.net/"
 
 
 class Newmode(object):
@@ -37,7 +38,7 @@ class Newmode(object):
         api_version="v1.0",
     ):
         self.api_version = check_env.check("NEWMODE_API_VERSION", api_version)
-
+        # TODO: delete when v1 depreciated
         if "v1" in self.api_version:
             logger.warning(
                 "Newmode API v1 will no longer be supported starting Feburary 2025."
@@ -58,15 +59,6 @@ class Newmode(object):
                 "NEWMODE_API_CLIENT_SECRET", client_secret
             )
             self.headers = {"content-type": "application/x-www-form-urlencoded"}
-            self.client = OAuth2APIConnector(
-                uri=self.base_url,
-                auto_refresh_url=API_AUTH_URL,
-                client_id=self.client_id,
-                client_secret=self.__client_secret,
-                headers=self.headers,
-                token_url=API_AUTH_URL,
-                grant_type="client_credentials",
-            )
 
     def convert_to_table(self, data):
         """Internal method to create a Parsons table from a data element."""
@@ -79,10 +71,10 @@ class Newmode(object):
 
         return table
 
+    # TODO: delete when v1 deprecated
     def base_request(
         self, method, url, requires_csrf=True, data=None, json=None, params={}
     ):
-
         if requires_csrf:
             csrf = self.get_csrf_token()
             self.headers["X-CSRF-Token"] = csrf
@@ -93,9 +85,24 @@ class Newmode(object):
         elif method == "PATCH":
             response = self.client.patch_request(url=url, params=params)
         elif method == "POST":
-            response = self.client.post_request(url=url, params=params, json=json)
+            response = self.client.post_request(
+                url=url, params=params, json=json, data=data
+            )
         return response
 
+    def base_request_v2(self, method, url, data=None, json=None, params={}):
+        response = None
+        if method == "GET":
+            response = self.client.get_request(url=url, params=params)
+        elif method == "PATCH":
+            response = self.client.patch_request(url=url, params=params)
+        elif method == "POST":
+            response = self.client.post_request(
+                url=url, params=params, json=json, data=data
+            )
+        return response
+
+    # TODO: delete when v1 deprecated
     def converted_request(
         self,
         endpoint,
@@ -123,6 +130,42 @@ class Newmode(object):
         else:
             return response
 
+    def converted_request_v2(
+        self,
+        endpoint,
+        method,
+        supports_version=True,
+        data=None,
+        json=None,
+        params={},
+        convert_to_table=True,
+    ):
+        self.client = OAuth2APIConnector(
+            uri=self.base_url,
+            auto_refresh_url=API_AUTH_URL,
+            client_id=self.client_id,
+            client_secret=self.__client_secret,
+            headers=self.headers,
+            token_url=API_AUTH_URL,
+            grant_type="client_credentials",
+        )
+        
+        url = f"{self.api_version}/{endpoint}" if supports_version else endpoint
+        response = self.base_request_v2(
+            method=method,
+            url=url,
+            json=json,
+            data=data,
+            params=params,
+        )
+        if not response:
+            logging.warning(f"Empty result returned from endpoint: {endpoint}")
+        if convert_to_table:
+            return self.convert_to_table(response)
+        else:
+            return response
+
+    # TODO:delete when v1 deprecated
     def get_csrf_token(self, max_retries=10):
         """
         Retrieve a CSRF token for making API requests
@@ -153,6 +196,37 @@ class Newmode(object):
                     f"Retry {attempt} at getting CSRF Token failed. Retrying. Error: {e}"
                 )
                 time.sleep(attempt + 1)
+
+    # def get_csrf_token_v2(self, max_retries=10):
+    #     """
+    #     Retrieve a CSRF token for making API requests
+    #     `Args:`
+    #         max_retries: int
+    #             The maximum number of attempts to get the CSRF token.
+    #     `Returns:`
+    #         The CSRF token.
+    #     """
+    #     endpoint = "/user/login?_format=json"
+    #     for attempt in range(max_retries):
+    #         # try:
+    #         response = self.converted_request_v2(
+    #             endpoint=endpoint,
+    #             method="POST",
+    #             supports_version=False,
+    #             convert_to_table=False,
+    #         )
+    #         print(response)
+            #     return response
+            # except Exception as e:
+            #     if attempt >= max_retries:
+            #         logger.error(
+            #             (f"Error getting CSRF Token after {max_retries} retries")
+            #         )
+            #         raise e
+            #     logger.warning(
+            #         f"Retry {attempt} at getting CSRF Token failed. Retrying. Error: {e}"
+            #     )
+            #     time.sleep(attempt + 1)
 
     def get_tools(self, params={}):
         """
@@ -246,22 +320,16 @@ class Newmode(object):
 
     def get_campaigns(self, params={}):
         """
-        V1 & V2
+        V1
         Retrieve all campaigns
-        In v2, a campaign is equivalent to Tools or Actions in V1.
         `Args:`
             params: dict
                 Query parameters to include in the request.
         `Returns:`
             Parsons Table containing campaigns data.
         """
-        if "v1" in self.api_version:
-            endpoint = "campaign"
-            requires_csrf = True
-        else:
-            self.api_version = "jsonapi"
-            endpoint = "action/action"
-            requires_csrf = False
+        endpoint = "campaign"
+        requires_csrf = True
         response = self.converted_request(
             endpoint=endpoint, method="GET", params=params, requires_csrf=requires_csrf
         )
@@ -269,10 +337,9 @@ class Newmode(object):
 
     def get_campaign(self, campaign_id, params={}):
         """
-        V1 & V2
+        V1
         Retrieve a specific campaign by ID.
 
-        In v2, a campaign is equivalent to Tools or Actions in V1.
         `Args:`
             campaign_id: str
                 The ID of the campaign to retrieve.
@@ -281,12 +348,8 @@ class Newmode(object):
         `Returns:`
             Parsons Table containing campaign data.
         """
-        if "v1" in self.api_version:
-            endpoint = f"campaign/{campaign_id}"
-            requires_csrf = True
-        else:
-            endpoint = f"/campaign/{campaign_id}/form"
-            requires_csrf = False
+        endpoint = f"campaign/{campaign_id}"
+        requires_csrf = True
         response = self.converted_request(
             endpoint=endpoint, method="GET", params=params, requires_csrf=requires_csrf
         )
@@ -421,6 +484,56 @@ class Newmode(object):
         )
         return response
 
+    def get_campaign_v2(self, campaign_id, params={}):
+        """
+        V2
+        Retrieve a specific campaign by ID.
+
+        In v2, a campaign is equivalent to Tools or Actions in V1.
+        `Args:`
+            campaign_id: str
+                The ID of the campaign to retrieve.
+            params: dict
+                Query parameters to include in the request.
+        `Returns:`
+            Parsons Table containing campaign data.
+        """
+        endpoint = f"/campaign/{campaign_id}/form"
+        response = self.converted_request_v2(
+            endpoint=endpoint,
+            method="GET",
+            params=params,
+        )
+        return response
+
+    def get_campaigns_v2(self, organization_id, params={}):
+        """
+        V2
+        Retrieve all campaigns
+        In v2, a campaign is equivalent to Tools or Actions in V1.
+        `Args:`
+            organization_id: str
+                ID of organization
+            params: dict
+                Query parameters to include in the request.
+        `Returns:`
+            Parsons Table containing campaigns data.
+        """
+        self.base_url = API_CAMPAIGNS_V2_URL
+        self.api_version = "jsonapi"
+        self.headers = {
+            "content-type": "application/vnd.api+json",
+            "accept": "application/vnd.api+json",
+            "authorization": "Bearer 1234567890",
+        }
+        endpoint = f"action/action?filter[field_org_id]={organization_id}"
+        response = self.converted_request_v2(
+            endpoint=endpoint,
+            method="GET",
+            params=params,
+        )
+        return response
+
     def get_recipient(
         self,
         campaign_id,
@@ -466,40 +579,42 @@ class Newmode(object):
             for key, value in address_params.items()
             if value
         }
-        response = self.converted_request(
+        response = self.converted_request_v2(
             endpoint=f"campaign/{campaign_id}/target",
             method="GET",
             params=params,
-            requires_csrf=False,
         )
         return response
 
-    def run_submit(self, campaign_id, json=None, data=None, params={}):
-        """
-        V2 only
-        Pass a submission from a supporter to a campaign
-        that ultimately fills in a petition,
-        sends an email or triggers a phone call
-        depending on your campaign type
+    # TODO: add run_submit method
+    # def run_submit(self, campaign_id, json=None, data=None, params={}):
+    #     """
+    #     V2 only
+    #     Pass a submission from a supporter to a campaign
+    #     that ultimately fills in a petition,
+    #     sends an email or triggers a phone call
+    #     depending on your campaign type
 
-        `Args:`
-            campaign_id: str
-                The ID of the campaign to retrieve.
-            params: dict
-                Query parameters to include in the request.
-        `Returns:`
-            Parsons Table containing submit data.
-        """
-        response = self.converted_request(
-            endpoint=f"campaign/{campaign_id}/submit",
-            method="POST",
-            data=data,
-            json=json,
-            params=params,
-            requires_csrf=False,
-            convert_to_table=False,
-        )
-        return response
+    #     `Args:`
+    #         campaign_id: str
+    #             The ID of the campaign to retrieve.
+    #         params: dict
+    #             Query parameters to include in the request.
+    #     `Returns:`
+    #         Parsons Table containing submit data.
+    #     """
+    #     # json["action_id"] = campaign_id
+    #     self.api_version = 2.0
+    #     response = self.converted_request(
+    #         endpoint=f"action/{campaign_id}/submit/?_format=json",
+    #         method="POST",
+    #         data=data,
+    #         json=json,
+    #         params=params,
+    #         requires_csrf=False,
+    #         convert_to_table=False,
+    #     )
+    #     return response
 
     def get_submissions(self, params={}):
         """
@@ -514,7 +629,11 @@ class Newmode(object):
         `Returns:`
             Parsons Table containing submit data.
         """
-        response = self.converted_request(
-            endpoint="submission", method="GET", params=params, requires_csrf=False
+        # self.base_url = API_AUTH_URL
+        # print(self.base_url)
+        # response=self.converted_request_v2(endpoint="/user/login?_format=json", method="POST", supports_version=False)
+        # self.token = self.token['access_token']
+        response = self.converted_request_v2(
+            endpoint="submission", method="GET", params=params
         )
         return response

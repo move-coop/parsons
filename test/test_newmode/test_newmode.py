4b3986e17a88216a478f328c47708ae5e2c0701e
sharinetmc <128429303+sharinetmc@users.noreply.github.com>
2025-01-27 15:54:32 -0500

    Add newmode v2 (#1227)
    * add init
    
    * edit connectors
    
    * add tests
    
    * add v2
    
    * ruff format
    
    * clean up
    
    * collapse classes and add back submissions
    
    * clean up
    
    * add docstrings
    
    * clean up docstrings + unittests
    
    * clean up
    
    * ruff
    
    * clean up test
    
    * ruff
    
    * clean up
    
    * typo
    
    * fix name
    
    * typo
    
    * typo
    
    * typo
    
    * typo
    
    * typo
    
    * add validation
    
    * revert
    
    * ruff
    
    * clean up
    
    * more clean up
    
    * clean up test
    
    * ruff
    
    * minor adjustments

--- test/test_newmode/test_newmode.py
+++ test/test_newmode/test_newmode.py
@@ -1,10 +1,21 @@
 import os
 import unittest
 import unittest.mock as mock
-from parsons import Newmode
+import requests_mock
+from parsons import Newmode, Table
+from test.utils import assert_matching_tables
+from test.test_newmode import test_newmode_data
+
+CLIENT_ID = "fakeClientID"
+CLIENT_SECRET = "fakeClientSecret"
 
 
-class TestNewmode(unittest.TestCase):
+V2_API_URL = "https://base.newmode.net/api/"
+V2_API_AUTH_URL = "https://base.newmode.net/oauth/token/"
+V2_API_CAMPAIGNS_URL = "https://base.newmode.net/"
+
+
+class TestNewmodeV1(unittest.TestCase):
     def setUp(self):
         os.environ["NEWMODE_API_USER"] = "MYFAKEUSERNAME"
         os.environ["NEWMODE_API_PASSWORD"] = "MYFAKEPASSWORD"
@@ -158,3 +169,68 @@ class TestNewmode(unittest.TestCase):
         response = self.nm.get_outreach(id)
         self.nm.client.getOutreach.assert_called_with(id, params={})
         self.assertEqual(response["name"], "Outreach 1")
+
+
+class TestNewmodeV2(unittest.TestCase):
+    @requests_mock.Mocker()
+    def setUp(self, m):
+        m.post(V2_API_AUTH_URL, json={"access_token": "fakeAccessToken"})
+        api_version = "v2.1"
+        self.nm = Newmode(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, api_version=api_version)
+        self.campaign_id = "fakeCampaignID"
+        self.base_url = f"{V2_API_URL}{api_version}"
+
+    @requests_mock.Mocker()
+    def test_get_campaign(self, m):
+        json_response = test_newmode_data.get_campaign_json_response
+        tbl = Table([json_response])
+        m.post(V2_API_AUTH_URL, json={"access_token": "fakeAccessToken"})
+        m.get(
+            f"{self.base_url}/campaign/{self.campaign_id}/form",
+            json=json_response,
+        )
+        assert_matching_tables(self.nm.get_campaign(campaign_id=self.campaign_id), tbl)
+
+    @requests_mock.Mocker()
+    def test_get_campaign_ids(self, m):
+        lst = ["testCampaingID"]
+        json_response = test_newmode_data.get_campaign_ids_json_response
+        m.post(V2_API_AUTH_URL, json={"access_token": "fakeAccessToken"})
+        m.get(f"{V2_API_CAMPAIGNS_URL}jsonapi/node/action", json=json_response)
+        assert_matching_tables(self.nm.get_campaign_ids(), lst)
+
+    @requests_mock.Mocker()
+    def test_get_recipient(self, m):
+        self.city = "Vancouver"
+        json_response = test_newmode_data.get_recipient_json_response
+
+        tbl = Table([json_response])
+        m.post(V2_API_AUTH_URL, json={"access_token": "fakeAccessToken"})
+        m.get(f"{self.base_url}/campaign/{self.campaign_id}/target", json=json_response)
+        assert_matching_tables(
+            self.nm.get_recipient(campaign_id=self.campaign_id, city=self.city), tbl
+        )
+
+    @requests_mock.Mocker()
+    def test_run_submit(self, m):
+        json_response = test_newmode_data.run_submit_json_response
+        json_input = {
+            "action_id": self.campaign_id,
+            "first_name": "TestFirstName",
+            "last_name": "TestLastName",
+            "email": "test_abc@test.com",
+            "opt_in": 1,
+            "address": {"postal_code": "V6A 2T2"},
+            "subject": "This is my subject",
+            "message": "This is my letter",
+        }
+
+        m.post(V2_API_AUTH_URL, json={"access_token": "fakeAccessToken"})
+        m.post(
+            f"{self.base_url}/campaign/{self.campaign_id}/submit",
+            json=json_response,
+        )
+        assert_matching_tables(
+            self.nm.run_submit(campaign_id=self.campaign_id, json=json_input),
+            json_response,
+        )

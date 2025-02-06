7af468428008b7293611fa3712c947c6556d3b88
Wil T <wil.t.me@pm.me>
2025-02-06 10:21:28 -0500

    fix version number

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ def main():
 
     setup(
         name="parsons",
-        version="4",
+        version="4.0.0",
         author="The Movement Cooperative",
         author_email="info@movementcooperative.org",
         url="https://github.com/move-coop/parsons",

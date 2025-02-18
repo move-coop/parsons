35ce607b256cff9d6214f90498feb2fa6e406f58
mkrausse-ggtx <131683556+mkrausse-ggtx@users.noreply.github.com>
2025-02-18 09:11:30 -0800

    Add QuickBooks connector (#922)
    * adding quickbooks connector
    
    * updated tests
    
    * added doc template
    
    * added todos
    
    * updated test data
    
    * updated tests
    
    * added more pagination logic
    
    * added test data
    
    * added more test coverage
    
    * updated gitignore
    
    * added logging
    
    * added additional tests
    
    * Fix start date default value in QuickBooks class
    
    * adding logging
    
    * added docs
    
    * linted
    
    * updated linting
    
    * lint lint lint lint lint lint lint
    
    * updated qb_get_request method
    
    * added get_geolocations method and tests
    
    * added page arg
    
    * renamed files
    
    * renamed connector
    
    * refactor tests to use QuickBooksTime and enhance assertions for data validation
    
    * update docstring to clarify QuickBooksTime class usage and improve link formatting
    
    * update gitignore
    
    * fix: update QuickBooksTime import path to match new module structure
    
    * chore: reorganize imports in QuickBooksTime and update test imports for consistency
    
    * fix: update mock_groups_data structure for consistency in test cases
    
    * fix: update test assertions to use dictionary values for consistency in QuickBooksTime tests
    
    * docs: update QuickBooks references to QuickBooks Time for clarity
    
    * docs: add QuickBooks to the index
    
    * fix: update default page value to 0 in QuickBooksTime class and adjust documentation
    
    * fix: handle page parameter in QuickBooksTime class and update default value to 0
    
    ---------
    
    Co-authored-by: mattkrausse <106627640+mattkrausse@users.noreply.github.com>
    Co-authored-by: Matthew Krausse <69082853+matthewkrausse@users.noreply.github.com>

--- parsons/__init__.py
+++ parsons/__init__.py
@@ -80,6 +80,7 @@ for module_path, connector_name in (
     ("parsons.pdi.pdi", "PDI"),
     ("parsons.phone2action.p2a", "Phone2Action"),
     ("parsons.quickbase.quickbase", "Quickbase"),
+    ("parsons.quickbooks.quickbookstime", "QuickBooksTime"),
     ("parsons.redash.redash", "Redash"),
     ("parsons.rockthevote.rtv", "RockTheVote"),
     ("parsons.salesforce.salesforce", "Salesforce"),

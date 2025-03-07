6b0ec014af615da75c30a3bf18350346cfc081b1
Wil T <wil.t.me@pm.me>
2025-03-06 22:39:22 -0500

    update docs copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ sys.path.insert(0, os.path.abspath("../"))
 # -- Project information -----------------------------------------------------
 
 project = "Parsons"
-copyright = "2019, The Movement Cooperative"
+copyright = "2025, The Movement Cooperative"
 author = "The Movement Cooperative"
 
 # The short X.Y version

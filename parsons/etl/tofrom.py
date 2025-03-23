20d400c322a7b0a6b21aa14cfdd2d79de03d5bc2
matthewkrausse <106627640+mattkrausse@users.noreply.github.com>
2025-03-23 10:38:17 -0700

    feat(avro): Add support for reading and writing Avro files in Table class

--- parsons/etl/tofrom.py
+++ parsons/etl/tofrom.py
@@ -98,6 +98,138 @@ class ToFrom(object):
 
         return local_path
 
+    def to_avro(
+        self, target, schema=None, sample=9, codec="deflate", compression_level=None, **avro_args
+    ):
+        """
+        Outputs table to an Avro file.
+
+        Write the table into a new avro file according to schema passed.
+
+        This method assume that each column has values with the same type
+        for all rows of the source `table`.
+
+        `Apache Avro`_ is a data
+        serialization framework. It is used in data serialization (especially in
+        Hadoop ecosystem), for dataexchange for databases (Redshift) and RPC
+        protocols (like in Kafka). It has libraries to support many languages and
+        generally is faster and safer than text formats like Json, XML or CSV.
+
+        `Args:`
+            target: str
+                the file path for creating the avro file.
+                Note that if a file already exists at the given location, it will be
+                overwritten.
+            schema: dict
+                defines the rows field structure of the file.
+                Check fastavro `documentation`_ and Avro schema `reference`_ for details.
+            sample: int, optional
+                defines how many rows are inspected
+                for discovering the field types and building a schema for the avro file
+                when the `schema` argument is not passed. Default is 9.
+            codec: str, optional
+                The `codec` argument (string, optional) sets the compression codec used to
+                shrink data in the file. It can be 'null', 'deflate' (default), 'bzip2' or
+                'snappy', 'zstandard', 'lz4', 'xz' (if installed)
+            compression_level: int, optional
+                sets the level of compression to use with the specified codec (if the codec supports it)
+            avro_args: kwargs
+                Additionally there are support for passing extra options in the
+                argument `**avro_args` that are fowarded directly to fastavro. Check the
+                fastavro `documentation`_ for reference.
+
+        The avro file format preserves type information, i.e., reading and writing
+        is round-trippable for tables with non-string data values. However the
+        conversion from Python value types to avro fields is not perfect. Use the
+        `schema` argument to define proper type to the conversion.
+
+        The following avro types are supported by the schema: null, boolean,
+        string, int, long, float, double, bytes, fixed, enum,
+        :ref:`array <array_schema>`, map, union, record, and recursive types
+        defined in :ref:`complex schemas <complex_schema>`.
+
+        Also :ref:`logical types <logical_schema>` are supported and translated to
+        coresponding python types: long timestamp-millis, long timestamp-micros, int date,
+        bytes decimal, fixed decimal, string uuid, int time-millis, long time-micros.
+
+        Example usage for writing files::
+
+            >>> # set up a Avro file to demonstrate with
+            >>> table2 = [['name', 'friends', 'age'],
+            ...           ['Bob', 42, 33],
+            ...           ['Jim', 13, 69],
+            ...           ['Joe', 86, 17],
+            ...           ['Ted', 23, 51]]
+            ...
+            >>> schema2 = {
+            ...     'doc': 'Some people records.',
+            ...     'name': 'People',
+            ...     'namespace': 'test',
+            ...     'type': 'record',
+            ...     'fields': [
+            ...         {'name': 'name', 'type': 'string'},
+            ...         {'name': 'friends', 'type': 'int'},
+            ...         {'name': 'age', 'type': 'int'},
+            ...     ]
+            ... }
+            ...
+            >>> # now demonstrate what writing with toavro()
+            >>> import petl as etl
+            >>> etl.toavro(table2, 'example.file2.avro', schema=schema2)
+            ...
+            >>> # this was what was saved above
+            >>> tbl2 = etl.fromavro('example.file2.avro')
+            >>> tbl2
+            +-------+---------+-----+
+            | name  | friends | age |
+            +=======+=========+=====+
+            | 'Bob' |      42 |  33 |
+            +-------+---------+-----+
+            | 'Jim' |      13 |  69 |
+            +-------+---------+-----+
+            | 'Joe' |      86 |  17 |
+            +-------+---------+-----+
+            | 'Ted' |      23 |  51 |
+            +-------+---------+-----+
+        """
+
+        return petl.toavro(
+            self.table,
+            target,
+            schema=schema,
+            sample=sample,
+            codec=codec,
+            compression_level=compression_level,
+            **avro_args,
+        )
+
+    def append_avro(self, target, schema=None, sample=9, **avro_args):
+        """
+        Append table to an existing Avro file.
+
+        Write the table into an existing avro file according to schema passed.
+
+        This method assume that each column has values with the same type
+        for all rows of the source `table`.
+
+        `Args:`
+            target: str
+                the file path for creating the avro file.
+            schema: dict
+                defines the rows field structure of the file.
+                Check fastavro `documentation`_ and Avro schema `reference`_ for details.
+            sample: int, optional
+                defines how many rows are inspected
+                for discovering the field types and building a schema for the avro file
+                when the `schema` argument is not passed. Default is 9.
+            avro_args: kwargs
+                Additionally there are support for passing extra options in the
+                argument `**avro_args` that are fowarded directly to fastavro. Check the
+                fastavro `documentation`_ for reference.
+        """
+
+        return petl.appendavro(self.table, target, schema=schema, sample=sample, **avro_args)
+
     def to_csv(
         self,
         local_path=None,
@@ -714,6 +846,28 @@ class ToFrom(object):
             wait=wait,
             **civisargs,
         )
+
+    @classmethod
+    def from_avro(cls, local_path, limit=None, skips=0, **avro_args):
+        """
+        Create a ``parsons table`` from an Avro file.
+
+        `Args:`
+            local_path: str
+                The path to the Avro file.
+            limit: int, optional
+                The maximum number of rows to extract. Default is ``None`` (all rows).
+            skips: int, optional
+                The number of rows to skip from the start. Default is 0.
+            \**avro_args: kwargs
+                Additional arguments passed to `fastavro.reader`.
+
+        `Returns:`
+            Parsons Table
+                See :ref:`parsons-table` for output options.
+        """
+
+        return cls(petl.fromavro(local_path, limit=limit, skips=skips, **avro_args))
 
     @classmethod
     def from_csv(cls, local_path, **csvargs):

7a175954d80d5f517780ca852feec29d63c5097a
Charlie Kramer <ckramer36@gatech.edu>
2025-04-24 14:21:26 -0400

    updated census tests

--- test/test_census/test_census.py
+++ test/test_census/test_census.py
@@ -1,11 +1,10 @@
 import unittest
-
+from unittest.mock import patch
 import requests_mock
-
 from parsons import Census, Table
 from test.utils import mark_live_test
 
-
+@patch.dict(os.environ, {"CENSUS_API_KEY": "mock_api_key"})
 class TestCensus(unittest.TestCase):
     def setUp(self):
         self.census = Census()
@@ -24,13 +23,13 @@ class TestCensus(unittest.TestCase):
         self.assertEqual(table[0]["NAME"], "Illinois")
         self.assertIsInstance(table, Table)
 
-        @requests_mock.Mocker()
-        def test_get_census_mock_test(self, m):
-            year = "2019"
-            dataset_acronym = "/acs/acs1"
-            variables = "NAME,B01001_001E"
-            location = "for=us:1"
-            test_json = {"NAME": "United States", "B01001_001E": "328239523", "us": "1"}
-            table = m.census.get_census(year, dataset_acronym, variables, location, json=test_json)
-            self.assertEqual(table[0]["B01001_001E"], "328239523")
-            self.assertEqual(table[0]["NAME"], "United States")
+    @requests_mock.Mocker()
+    def test_get_census_mock_test(self, m):
+        year = "2019"
+        dataset_acronym = "/acs/acs1"
+        variables = "NAME,B01001_001E"
+        location = "for=us:1"
+        test_json = {"NAME": "United States", "B01001_001E": "328239523", "us": "1"}
+        table = m.census.get_census(year, dataset_acronym, variables, location, json=test_json)
+        self.assertEqual(table[0]["B01001_001E"], "328239523")
+        self.assertEqual(table[0]["NAME"], "United States")

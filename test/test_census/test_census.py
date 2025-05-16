5f7be64b80ea93f5d6c42e6e099955219ea4de55
Charlie Kramer <ckramer36@gatech.edu>
2025-05-16 12:41:02 -0400

    fixed mock test

--- test/test_census/test_census.py
+++ test/test_census/test_census.py
@@ -31,11 +31,25 @@ class TestCensus(unittest.TestCase):
     @patch.dict(os.environ, {"CENSUS_API_KEY": "mock_api_key"})
     @requests_mock.Mocker()
     def test_get_census_mock_test(self, m):
-        m.census = Census()
+        census = Census()
         year = "2019"
         dataset_acronym = "/acs/acs1"
         variables = "NAME,B01001_001E"
         location = "for=us:1"
-        table = m.census.get_census(year, dataset_acronym, variables, location)
+
+        # This must match what get_census() will call under the hood
+        expected_url = (
+            "https://api.census.gov/data/2019/acs/acs1"
+            "?get=NAME,B01001_001E&for=us:1&key=mock_api_key"
+        )
+
+        # Mock the actual HTTP response
+        m.get(expected_url, json=[
+            ["NAME", "B01001_001E", "us"],
+            ["United States", "328239523", "1"]
+        ])
+
+        table = census.get_census(year, dataset_acronym, variables, location)
+
         self.assertEqual(table[0]["B01001_001E"], "328239523")
         self.assertEqual(table[0]["NAME"], "United States")

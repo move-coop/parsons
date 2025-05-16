589471ffce5978cbcc9f58059b5e4aa2c09528dc
Charlie Kramer <ckramer36@gatech.edu>
2025-05-16 14:48:40 -0400

    issue with mock address

--- test/test_census/test_census.py
+++ test/test_census/test_census.py
@@ -15,7 +15,7 @@ class TestCensus(unittest.TestCase):
     def tearDown(self):
         pass
 
-    @mark_live_test 
+    @mark_live_test
     @patch.dict(os.environ, {"CENSUS_API_KEY": "mock_api_key"})
     def test_get_census_live_test(self):
         self.census = Census()

fc7938bbb7cbb141a14495ee562b974b868a617f
ci-build <ci-build@movementcooperative.org>
2025-05-22 19:20:44 +0000

    [skip ci] Updates

--- conf.py
+++ conf.py
@@ -95,6 +95,10 @@ html_theme_options = {
     "display_version": True,
 }
 
+html_theme_options["analytics"] = {
+    "google_analytics_id": "G-L2YB7WHTRG",
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".

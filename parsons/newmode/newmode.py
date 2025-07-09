bc8110327a939968a943b974780cb8350e3f8dae
cody braun <codybraun@gmail.com>
2025-07-09 13:30:27 -0700

    dont pass around multiple clients

--- parsons/newmode/newmode.py
+++ parsons/newmode/newmode.py
@@ -431,6 +431,18 @@ class NewmodeV2:
         self.client_secret: str = check_env.check("NEWMODE_API_CLIENT_SECRET", client_secret)
         self.headers: Dict[str, str] = {"content-type": "application/json"}
         self.default_client: OAuth2APIConnector = self.get_default_oauth_client()
+        self.campaigns_client: OAuth2APIConnector = self.get_campaigns_oauth_client()
+
+    def get_campaigns_oauth_client(self) -> OAuth2APIConnector:
+        return OAuth2APIConnector(
+            uri=V2_API_CAMPAIGNS_URL,
+            auto_refresh_url=None,
+            client_id=self.client_id,
+            client_secret=self.client_secret,
+            headers=V2_API_CAMPAIGNS_HEADERS,
+            token_url=V2_API_AUTH_URL,
+            grant_type="client_credentials",
+        )
 
     def get_default_oauth_client(self) -> OAuth2APIConnector:
         return OAuth2APIConnector(
@@ -461,7 +473,7 @@ class NewmodeV2:
         self,
         method: str,
         url: str,
-        client: OAuth2APIConnector,
+        use_campaigns_client: bool = False,
         data: Optional[Dict[str, Any]] = None,
         json: Optional[Dict[str, Any]] = None,
         params: Optional[Dict[str, Any]] = None,
@@ -473,6 +485,8 @@ class NewmodeV2:
         """
         if params is None:
             params = {}
+
+        client = self.default_client if not use_campaigns_client else self.campaigns_client
 
         for attempt in range(retries + 1):
             try:
@@ -497,7 +511,7 @@ class NewmodeV2:
         self,
         method: str,
         endpoint: str,
-        client: OAuth2APIConnector,
+        use_campaigns_client: bool = False,
         data_key: str = RESPONSE_DATA_KEY,
         data: Optional[Dict[str, Any]] = None,
         json: Optional[Dict[str, Any]] = None,
@@ -518,7 +532,7 @@ class NewmodeV2:
             response = self.base_request(
                 method=method,
                 url=url,
-                client=client,
+                use_campaigns_client=use_campaigns_client,
                 data=data,
                 json=json,
                 params=params,
@@ -545,14 +559,13 @@ class NewmodeV2:
         params: Optional[Dict[str, Any]] = None,
         convert_to_table: bool = True,
         data_key: Optional[str] = None,
-        client: Optional[OAuth2APIConnector] = None,
+        use_campaigns_client: bool = False,
         override_api_version: Optional[str] = None,
     ) -> Union[Table, Dict[str, Any]]:
         """Internal method to make a call to the Newmode API and convert the result to a Parsons table."""
 
         if params is None:
             params = {}
-        client = client if client else self.default_client
         response = self.paginate_request(
             method=method,
             json=json,
@@ -561,12 +574,12 @@ class NewmodeV2:
             data_key=data_key,
             supports_version=supports_version,
             endpoint=endpoint,
-            client=client,
+            use_campaigns_client=use_campaigns_client,
             override_api_version=override_api_version,
         )
         if response:
             if convert_to_table:
-                return client.convert_to_table(data=response)
+                return self.default_client.convert_to_table(data=response)
             else:
                 return response
 
@@ -608,22 +621,13 @@ class NewmodeV2:
         if params is None:
             params = {}
         endpoint = "node/action"
-        campaigns_client = OAuth2APIConnector(
-            uri=V2_API_CAMPAIGNS_URL,
-            auto_refresh_url=None,
-            client_id=self.client_id,
-            client_secret=self.client_secret,
-            headers=V2_API_CAMPAIGNS_HEADERS,
-            token_url=V2_API_AUTH_URL,
-            grant_type="client_credentials",
-        )
 
         data = self.converted_request(
             endpoint=endpoint,
             method="GET",
             params=params,
             data_key=RESPONSE_DATA_KEY,
-            client=campaigns_client,
+            use_campaigns_client=True,
             override_api_version=V2_API_CAMPAIGNS_VERSION,
         )
         return data["id"]

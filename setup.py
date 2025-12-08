c533c2d6945a2ff827841fbc0e437fabd555e81f
cody braun <codybraun@gmail.com>
2025-12-08 10:16:32 -0800

    one more dep

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def main():
             "airtable": ["pyairtable"],
             "alchemer": ["surveygizmo"],
             "azure": ["azure-storage-blob"],
-            "box": ["boxsdk < 10, >=4.1.0"],
+            "box": ["boxsdk < 10, >=4.1.0", "requests-toolbelt>=1.0.0"],
             "braintree": ["braintree"],
             "catalist": ["paramiko"],
             "civis": ["civis"],

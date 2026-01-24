c84b230503eaf85974b68f40679158d17301a8a2
Ramona T <ramona.t.me@pm.me>
2026-01-23 22:40:08 -0500

    remove censusgeocode requirement

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ def main():
             "dbt-snowflake": ["dbt-snowflake >= 1.5.0"],
             "facebook": ["joblib", "facebook-business"],
             "geocode": [
-                "censusgeocode @ git+https://github.com/fitnr/censusgeocode.git@1824f5d558ff6378dc4359b44c9cf535a2ba205f",
                 "urllib3==1.26.19;python_version<'3.10'",
                 "urllib3==2.6.0;python_version>='3.10'",
             ],

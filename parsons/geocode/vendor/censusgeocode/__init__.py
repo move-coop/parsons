3a1aa4172124a3d2034054438b038c467122dca4
Ramona T <ramona.t.me@pm.me>
2026-01-23 23:41:51 -0500

    add FutureWarning when importing CensusGeocode

--- parsons/geocode/vendor/censusgeocode/__init__.py
+++ parsons/geocode/vendor/censusgeocode/__init__.py
@@ -7,9 +7,19 @@
 # http://opensource.org/licenses/LGPL-3.0
 # Copyright (c) 2015-9, Neil Freeman <contact@fakeisthenewreal.org>
 
+import warnings
 from .censusgeocode import CensusGeocode
 
 __version__ = '0.5.2'
+
+warnings.warn(
+    (
+        "CensusGeocoder will be removed in parsons version 6.0.0, "
+        "unless the censusgeocode package is updated to allow newer versions of urllib3."
+    ),
+    category=FutureWarning,
+    stacklevel=2,
+)
 
 cg = CensusGeocode()
 

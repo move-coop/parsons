eb8c8fcd2a47d6157e1773249904262a3bcae7dc
Ramona T <ramona.t.me@pm.me>
2026-01-29 13:17:41 -0500

    Add xmltodict to mobilecommons dependencies (#1699)

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ EXTRA_DEPENDENCIES = {
     ],
     "newmode": ["newmode"],
     "ngpvan": ["suds-py3"],
-    "mobilecommons": ["beautifulsoup4"],
+    "mobilecommons": ["beautifulsoup4", "xmltodict"],
     "postgres": [
         "psycopg2-binary >= 2.0.0",
         "sqlalchemy >= 1.4.22",

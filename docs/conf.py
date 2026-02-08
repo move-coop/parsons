8ef5cf32e52a096bc93f740da5c1f9dbbcfbf5d2
Ramona T <ramona.t.me@pm.me>
2026-02-08 14:57:29 -0500

    enable napoleon sphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
     "sphinx.ext.imgmath",
+    "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "myst_parser",
     "sphinx_multiversion",

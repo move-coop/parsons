4c0d93c06313c9b4004e9f25931d8d14ca914168
Ramona T <ramona.t.me@pm.me>
2026-02-12 20:59:13 -0500

    resolve unsupported theme option 'display_version' given

--- docs/conf.py
+++ docs/conf.py
@@ -77,14 +77,6 @@ pygments_style = "sphinx"
 #
 html_theme = "sphinx_rtd_theme"
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-html_theme_options = {
-    "display_version": True,
-}
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".

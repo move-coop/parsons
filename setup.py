6863792a8064ee060fb38a7e8f5d8ef6458874b5
Ramona T <ramona.t.me@pm.me>
2026-03-30 18:52:25 -0400

    limit boxsdk to <4.4 due to incompatiblity (until #1723 is merged)

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ EXTRA_DEPENDENCIES = {
     "alchemer": ["surveygizmo >= 1"],
     "avro": ["fastavro >= 1.12"],
     "azure": ["azure-storage-blob >= 12"],
-    "box": ["boxsdk[jwt] >= 4, < 10", "requests-toolbelt >= 1"],
+    "box": ["boxsdk[jwt] >= 4, < 4.4.0, < 10", "requests-toolbelt >= 1"],
     "braintree": ["braintree >= 4"],
     "catalist": ["paramiko >= 3"],
     "civis": ["civis >= 2"],

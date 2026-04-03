5225c462782bb8212ad79df4671d225a935acbc2
Ramona T <ramona.t.me@pm.me>
2026-04-03 10:51:00 -0400

    import all connectors normally during TYPE_CHECKING

--- parsons/__init__.py
+++ parsons/__init__.py
@@ -3,6 +3,7 @@ import importlib
 import logging
 import os
 import warnings
+from typing import TYPE_CHECKING
 
 from parsons.etl.table import Table
 
@@ -110,7 +111,83 @@ for module_path, connector_name in (
     ("parsons.zoom.zoom", "Zoom"),
 ):
     try:
-        globals()[connector_name] = getattr(importlib.import_module(module_path), connector_name)
+        module = importlib.import_module(module_path)
+        globals()[connector_name] = getattr(module, connector_name)
         __all__.append(connector_name)
     except ImportError as e:
         logger.debug(f"Could not import {module_path}.{connector_name} with {e}; skipping")
+
+if TYPE_CHECKING:
+    # ruff: noqa: F401
+    # This block is seen by IDEs (PyCharm, VS Code) but ignored at runtime.
+    # Add new connectors here, along with in the for loop below
+    from parsons.actblue.actblue import ActBlue
+    from parsons.action_builder.action_builder import ActionBuilder
+    from parsons.action_kit.action_kit import ActionKit
+    from parsons.action_network.action_network import ActionNetwork
+    from parsons.airmeet.airmeet import Airmeet
+    from parsons.airtable.airtable import Airtable
+    from parsons.alchemer.alchemer import Alchemer
+    from parsons.alchemer.alchemer import Alchemer as SurveyGizmo
+    from parsons.auth0.auth0 import Auth0
+    from parsons.aws.s3 import S3
+    from parsons.azure.azure_blob_storage import AzureBlobStorage
+    from parsons.bill_com.bill_com import BillCom
+    from parsons.bloomerang.bloomerang import Bloomerang
+    from parsons.box.box import Box
+    from parsons.braintree.braintree import Braintree
+    from parsons.capitol_canary.capitol_canary import CapitolCanary
+    from parsons.catalist.catalist import CatalistMatch
+    from parsons.census.census import Census
+    from parsons.civis.civisclient import CivisClient
+    from parsons.community.community import Community
+    from parsons.controlshift.controlshift import Controlshift
+    from parsons.copper.copper import Copper
+    from parsons.crowdtangle.crowdtangle import CrowdTangle
+    from parsons.databases.database_connector import DatabaseConnector
+    from parsons.databases.db_sync import DBSync
+    from parsons.databases.discover_database import discover_database
+    from parsons.databases.mysql.mysql import MySQL
+    from parsons.databases.postgres.postgres import Postgres
+    from parsons.databases.redshift.redshift import Redshift
+    from parsons.databases.sqlite.sqlite import Sqlite
+    from parsons.donorbox.donorbox import Donorbox
+    from parsons.empower.empower import Empower
+    from parsons.facebook_ads.facebook_ads import FacebookAds
+    from parsons.formstack.formstack import Formstack
+    from parsons.freshdesk.freshdesk import Freshdesk
+    from parsons.geocode.census_geocoder import CensusGeocoder
+    from parsons.github.github import GitHub
+    from parsons.google.google_admin import GoogleAdmin
+    from parsons.google.google_bigquery import GoogleBigQuery
+    from parsons.google.google_civic import GoogleCivic
+    from parsons.google.google_cloud_storage import GoogleCloudStorage
+    from parsons.google.google_docs import GoogleDocs
+    from parsons.google.google_drive import GoogleDrive
+    from parsons.google.google_sheets import GoogleSheets
+    from parsons.hustle.hustle import Hustle
+    from parsons.mailchimp.mailchimp import Mailchimp
+    from parsons.mobilecommons.mobilecommons import MobileCommons
+    from parsons.mobilize_america.ma import MobilizeAmerica
+    from parsons.nation_builder.nation_builder import NationBuilder
+    from parsons.newmode.newmode import Newmode
+    from parsons.ngpvan.van import VAN
+    from parsons.notifications.gmail import Gmail
+    from parsons.notifications.slack import Slack
+    from parsons.notifications.smtp import SMTP
+    from parsons.pdi.pdi import PDI
+    from parsons.phone2action.p2a import Phone2Action
+    from parsons.quickbase.quickbase import Quickbase
+    from parsons.quickbooks.quickbookstime import QuickBooksTime
+    from parsons.redash.redash import Redash
+    from parsons.rockthevote.rtv import RockTheVote
+    from parsons.salesforce.salesforce import Salesforce
+    from parsons.scytl.scytl import Scytl
+    from parsons.sftp.sftp import SFTP
+    from parsons.shopify.shopify import Shopify
+    from parsons.sisense.sisense import Sisense
+    from parsons.targetsmart.targetsmart_api import TargetSmartAPI
+    from parsons.targetsmart.targetsmart_automation import TargetSmartAutomation
+    from parsons.turbovote.turbovote import TurboVote
+    from parsons.twilio.twilio import Twilio
+    from parsons.zoom.zoom import Zoom

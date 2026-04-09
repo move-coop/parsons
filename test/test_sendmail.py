50e505c969a13cbef01b56ea501e91e86dfdded0
Ramona T <ramona.t.me@pm.me>
2026-04-09 16:27:23 -0400

    Add email with accented characters to test_passes_valid_emails

--- test/test_sendmail.py
+++ test/test_sendmail.py
@@ -131,7 +131,7 @@ class TestSendMailValidateEmailString:
             dummy_sendmail._validate_email_string(bad_email)
 
     @pytest.mark.parametrize(
-        "good_email", ["a@b.com", "a+b@c.com", "a@b.org", "a@b.c.org", "a+b@c.d.net"]
+        "good_email", ["a@b.com", "a+b@c.com", "a@b.org", "a@b.c.org", "a+b@c.d.net", "á@ḅ.co"]
     )
     def test_passes_valid_emails(self, dummy_sendmail, good_email: str):
         dummy_sendmail._validate_email_string(good_email)

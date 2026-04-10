511293f61698848a6727b60fecfa454092d6b2b4
Ramona T <ramona.t.me@pm.me>
2026-04-10 18:21:59 -0400

    Add comprehensive docstring to check()

--- parsons/utilities/check_env.py
+++ parsons/utilities/check_env.py
@@ -1,18 +1,33 @@
 import os
 
 
-def check(env: str, field: str | None, optional: bool | None = False) -> str | None:
+def check(env: str, value: str | bool | int | None = None, optional: bool = False) -> str | None:
     """
-    Check if an environment variable has been set. If it has not been set
-    and the passed field or arguments have not been passed, then raise an
-    error.
+    Check if an environment variable has been set or value has been provided.
+
+    Args:
+        env: str
+            Name of environment variable to check.
+        value: str | bool | int, optional
+            If provided, ignore environment variable and use this.
+        optional: bool, optional
+            If true, do not raise an error if no value is found or provided.
+
+    Raises:
+        KeyError
+            If there is no matching environment variable or provided value.
+
     """
-    if field:
-        return field
+    if value is not None:
+        return value
+
     try:
         return os.environ[env]
+
     except KeyError as e:
-        if not optional:
-            raise KeyError(
-                f"No {env} found. Store as environment variable or pass as an argument."
-            ) from e
+        if optional:
+            return None
+
+        raise KeyError(
+            f"No {env} found. Store as environment variable or pass as an argument."
+        ) from e

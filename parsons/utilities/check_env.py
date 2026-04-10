c292205e38c878851fd1133892eea6972d505eb5
Ramona T <ramona.t.me@pm.me>
2026-04-10 18:22:19 -0400

    require optional to be keyword arg

--- parsons/utilities/check_env.py
+++ parsons/utilities/check_env.py
@@ -1,7 +1,7 @@
 import os
 
 
-def check(env: str, value: str | bool | int | None = None, optional: bool = False) -> str | None:
+def check(env: str, value: str | bool | int | None = None, *, optional: bool = False) -> str | None:
     """
     Check if an environment variable has been set or value has been provided.
 
@@ -10,6 +10,8 @@ def check(env: str, value: str | bool | int | None = None, optional: bool = Fals
             Name of environment variable to check.
         value: str | bool | int, optional
             If provided, ignore environment variable and use this.
+
+    Keyword Args:
         optional: bool, optional
             If true, do not raise an error if no value is found or provided.
 

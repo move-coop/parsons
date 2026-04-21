bf4f5cc243291b20ece4ce55c6ee992456769900
Ramona T <ramona.t.me@pm.me>
2026-04-21 15:08:45 -0400

    Configure linkcheck_ignore list

--- docs/conf.py
+++ docs/conf.py
@@ -35,8 +35,8 @@ nitpick_ignore = {
     ("py:class", "petl.util.base.Table"),  # this class is not in petl's sphinx documentation
 }
 nitpick_ignore_regex = {
-    (r"py:.*", r"dbt\..+"),  # dbt references (no linkable sphinx documentation)
     (r"py:.*", r"box_sdk_gen\..+"),  # box references (no linkable sphinx documentation)
+    (r"py:.*", r"dbt\..+"),  # dbt references (no linkable sphinx documentation)
     (r"py:.*", r"test\..+"),  # test files are currently not linkable
 }
 
@@ -96,24 +96,44 @@ intersphinx_mapping_extras = {
 intersphinx_mapping = intersphinx_mapping_core | intersphinx_mapping_extras
 
 # -- Linkcheck ---------------------------------------------------------------
-linkcheck_ignore = [
-    r"https://api.bluevote.com/docs/index#.*",
-    r"https://bloomerang.com/api/rest-api/#.*",
-    r"https://business.facebook.com",
-    r"https://cloud.google.com/python/docs/reference/bigquery/latest#.*",
+linkcheck_ignore_blocked_domains = [
+    r"https://actionnetwork.org/mirroring/docs",
+    r"https://app.periscopedata.com/app",
     r"https://boto3.amazonaws.com/v1/documentation/api/.*",
+    r"https://business.facebook.com",
+    r"https://cloud.google.com/python/docs/reference/bigquery/latest.*",
+    r"https://console..*.google.com/.*",
     r"https://developer.salesforce.com/docs/.*",
     r"https://developers.facebook.com.*",
-    r"https://docs.everyaction.com/reference/people-common-models#.*",
     r"https://docs.google.com/.*",
+    r"https://hub.docker.com/repository/docker/.*",
     r"https://learn.microsoft.com/en-us/python/api/azure-storage-blob/.*",
-    r"https://secure.actblue.com/docs/csv_api#.*",
-    r"https://sisense.dev/guides/rest/using-rest-api.html#.*",
+    r"https://secure.mcommons.com/.*",
     r"https://support.civisanalytics.com/hc/en-us/articles/.*",
+    r"https://www.airtable.com/create/tokens",
     r"https://www.alchemer.com.*",
-    r"https://www.parsonsproject.org/.*",
     r"https://www.facebook.com/business/.*",
+    r"https://www.parsonsproject.org/.*",
+    r"https://www.sisense.com/blog/periscope-data-is-now-sisense-for-cloud-data-teams/",
 ]
+linkcheck_ignore_inaccessible_anchors = [
+    r"https://api.bluevote.com/docs/index#.*",
+    r"https://bloomerang.com/api/rest-api/#.*",
+    r"https://docs.everyaction.com/reference/people-common-models#.*",
+    r"https://docs.python.org/3/library/csv.html#.*",
+    r"https://github.com/.*#.*",
+    r"https://secure.actblue.com/docs/csv_api#.*",
+]
+linkcheck_ignore_broken_connectors = [
+    r"https://.*.crowdtangle.com/.*",
+    r"https://capitolcanary.com/.*",
+    r"https://github.com/CrowdTangle",
+]
+linkcheck_ignore = (
+    linkcheck_ignore_blocked_domains
+    + linkcheck_ignore_inaccessible_anchors
+    + linkcheck_ignore_broken_connectors
+)
 
 # -- HTML Output (Furo Theme) ------------------------------------------------
 html_theme = "furo"

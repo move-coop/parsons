ea6ef7815a4c837148673dbfb421f6c507d607da
Ramona T <ramona.t.me@pm.me>
2026-07-05 18:33:53 -0400

    Create RateLimitedAPIConnector class

---
+++ parsons/solidarity_tech/solidarity_tech.py
@@ -0,0 +1,23 @@
+import requests
+from pyrate_limiter import Duration, Limiter, Rate
+
+from parsons.utilities.api_connector import APIConnector
+
+API_URL_BASE = "https://api.solidarity.tech/v1/"
+
+
+rates = [
+    Rate(60, Duration.SECOND * 30),  # 60 requests per 30 seconds
+]
+limiter = Limiter(rates)
+
+
+class RateLimitedAPIConnector(APIConnector):
+    @limiter.as_decorator(name="api_call", weight=1)
+    def request(
+        self,
+        *args,
+        **kwargs,
+    ) -> requests.Response:
+        """Make a request with pyrate-limiter."""
+        return super().request(*args, **kwargs)

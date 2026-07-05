449b34d46f7f40e5558d97e123fa481a82005030
Ramona T <ramona.t.me@pm.me>
2026-07-05 18:35:22 -0400

    Add boilerplate SolidarityTech class with no specific methods

--- parsons/solidarity_tech/solidarity_tech.py
+++ parsons/solidarity_tech/solidarity_tech.py
@@ -1,6 +1,7 @@
 import requests
 from pyrate_limiter import Duration, Limiter, Rate
 
+from parsons.utilities import check_env
 from parsons.utilities.api_connector import APIConnector
 
 API_URL_BASE = "https://api.solidarity.tech/v1/"
@@ -10,6 +11,26 @@ rates = [
     Rate(60, Duration.SECOND * 30),  # 60 requests per 30 seconds
 ]
 limiter = Limiter(rates)
+
+
+class SolidarityTech:
+    def __init__(self, api_token=None) -> None:
+        """
+        Instantiate the SolidarityTech class.
+
+        Args:
+            api_token:
+                A valid Bearer token for authorization.
+                Not required if the `SOLIDARITY_TECH_TOKEN` env variable is set.
+
+        """
+        self.api_token = check_env.check("SOLIDARITY_TECH_TOKEN", api_token)
+        self.headers = {
+            "accept": "application/json",
+            "Authorization": self.api_token,
+        }
+        self.api_url = API_URL_BASE
+        self.api = RateLimitedAPIConnector(self.api_url, headers=self.headers)
 
 
 class RateLimitedAPIConnector(APIConnector):

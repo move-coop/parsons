8e9bb73d209b54b6a36f57528f6f97b52955d2fd
Ramona T <ramona.t.me@pm.me>
2026-07-26 22:18:02 -0400

    Ensure api token is saved in connector

--- test/test_solidarity_tech/test_solidarity_tech_init.py
+++ test/test_solidarity_tech/test_solidarity_tech_init.py
@@ -5,14 +5,19 @@ import pytest
 
 from parsons.solidarity_tech.solidarity_tech import SolidarityTech
 
+PLACEHOLDER_TOKEN = "SOME_API_KEY"
 
 def test_init_with_arg() -> None:
-    SolidarityTech(api_token="SOME_API_KEY")
+    st = SolidarityTech(api_token=PLACEHOLDER_TOKEN)
+    assert st.api_token == PLACEHOLDER_TOKEN
+    assert st.headers.get("authorization") == f"Bearer {PLACEHOLDER_TOKEN}"
 
 
-@mock.patch.dict(os.environ, {"SOLIDARITY_TECH_TOKEN": "SOME_API_KEY"})
+@mock.patch.dict(os.environ, {"SOLIDARITY_TECH_TOKEN": PLACEHOLDER_TOKEN})
 def test_init_with_env() -> None:
-    SolidarityTech()
+    st = SolidarityTech()
+    assert st.api_token == PLACEHOLDER_TOKEN
+    assert st.headers.get("authorization") == f"Bearer {PLACEHOLDER_TOKEN}"
 
 
 def test_init_with_no_api_token() -> None:

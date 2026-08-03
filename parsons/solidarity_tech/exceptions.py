e3ac4c2668355a78ee3a1fa71c4f6f18b45bf3ff
Ramona T <ramona.t.me@pm.me>
2026-08-02 20:47:10 -0400

    Improve exceptions

--- parsons/solidarity_tech/exceptions.py
+++ parsons/solidarity_tech/exceptions.py
@@ -2,25 +2,34 @@ import requests
 from requests.exceptions import HTTPError
 
 
-class STUnexpectedResponseError(HTTPError):
+class STResponseError(HTTPError):
+    """Base exception for all Solidarity Tech response errors."""
+
+    def __init__(
+        self, message: str | None = None, *args, err_msg: str, response: requests.Response, **kwargs
+    ) -> None:
+        if response.status_code:
+            err_msg += f" (Status Code {response.status_code})"
+        if message:
+            err_msg += f" -- {message}"
+        super().__init__(err_msg, *args, **kwargs)
+
+
+class STUnexpectedResponseError(STResponseError):
     """Status code is not expected."""
 
-    def __init__(self, message: str | None = None, *, response: requests.Response) -> None:
+    def __init__(
+        self, message: str | None = None, *args, response: requests.Response, **kwargs
+    ) -> None:
         err_msg = "Unexpected Response"
-        if response and response.status_code:
-            err_msg += f" (Status Code {response.status_code})"
-        if message:
-            err_msg += f" -- {message}"
-        super().__init__(err_msg, response=response)
+        super().__init__(message, *args, err_msg=err_msg, response=response, **kwargs)
 
 
-class STFailedResponseError(HTTPError):
+class STFailedResponseError(STResponseError):
     """Status code indicates a known failure."""
 
-    def __init__(self, message: str, *, response: requests.Response) -> None:
+    def __init__(
+        self, message: str | None = None, *args, response: requests.Response, **kwargs
+    ) -> None:
         err_msg = "Request Failed"
-        if response and response.status_code:
-            err_msg += f" (Status Code {response.status_code})"
-        if message:
-            err_msg += f" -- {message}"
-        super().__init__(err_msg, response=response)
+        super().__init__(message, *args, err_msg=err_msg, response=response, **kwargs)

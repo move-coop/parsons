61452dd8b6bd3e4dbbf3e03b99631a338ee97335
Ramona T <ramona.t.me@pm.me>
2026-08-07 10:33:29 -0400

    Document and improve SolidarityTech tests

--- test/test_solidarity-tech/test_st_base.py
+++ test/test_solidarity-tech/test_st_base.py
@@ -1,17 +1,25 @@
+from __future__ import annotations
+
 import re
 from datetime import datetime, timezone
+from typing import TYPE_CHECKING
 
 import pytest
 import requests
-from pytest_mock import MockerFixture
 from requests_mock import GET, POST, Mocker
 
-from parsons.solidarity_tech import SolidarityTech
 from parsons.solidarity_tech.exceptions import STFailedResponseError, STUnexpectedResponseError
+
+if TYPE_CHECKING:
+    from pytest_mock import MockerFixture
+
+    from parsons.solidarity_tech import SolidarityTech
+    from parsons.utilities.api_connector import _JsonType, _ParamsType
 
 
 @pytest.fixture
 def known_status_codes() -> dict[int, tuple[bool, str]]:
+    """Known status codes and their expected outcomes."""
     return {
         200: (True, "OK"),
         201: (True, "updated resource"),
@@ -21,51 +29,86 @@ def known_status_codes() -> dict[int, tuple[bool, str]]:
 
 
 class Test_Post_Request:
+    """Tests for the _post_request method."""
+
+    @pytest.mark.parametrize(
+        "endpoint", ["custom_user_properties", "event_sessions/295876/hosts", "field_survey_urls"]
+    )
+    def test_get_single_resource_handles_varied_endpoints(
+        self, st: SolidarityTech, requests_mock: Mocker, mocker: MockerFixture, endpoint: str
+    ) -> None:
+        """Make a POST request to varied endpoints."""
+        endpoint_url = f"{st.api_url}/{endpoint}"
+
+        requests_mock.post(endpoint_url)
+        spy = mocker.spy(st.api, "request")
+
+        st._post_request(endpoint)
+
+        spy.assert_called_once_with(url=endpoint_url, req_type=POST)
+
     def test_get_single_resource_makes_request_with_payload(
         self, st: SolidarityTech, requests_mock: Mocker, mocker: MockerFixture
     ) -> None:
-        payload = {"user_id": 654123}
+        """Makes a POST request with payload."""
+        payload: _JsonType = {"user_id": 654123}
 
         requests_mock.post(st.api_url)
         spy = mocker.spy(st.api, "request")
 
         st._post_request(st.api_url, payload=payload)
+
         spy.assert_called_once_with(url=st.api_url, req_type=POST, json=payload)
 
     def test_get_single_resource_makes_request_with_params(
         self, st: SolidarityTech, requests_mock: Mocker, mocker: MockerFixture
     ) -> None:
-        params = {"automation_id": 35876}
+        """Make a POST request with params."""
+        params: _ParamsType = {"automation_id": 35876}
 
         requests_mock.post(st.api_url)
         spy = mocker.spy(st.api, "request")
 
         st._post_request(st.api_url, params=params)
+
         spy.assert_called_once_with(url=st.api_url, req_type=POST, json=None, params=params)
 
 
 class Test_Get_Single_Resource:
+    """Tests for the _get_single_resource method."""
+
     def test_get_single_resource_makes_request_with_id(
         self, st: SolidarityTech, requests_mock: Mocker, mocker: MockerFixture
     ) -> None:
+        """Make a GET request with an ID."""
         id = 42
-        endpoint = f"{st.api_url}/{id}"
+        endpoint = "users"
+        endpoint_url = f"{st.api_url}/{endpoint}/{id}"
 
-        requests_mock.get(endpoint)
+        requests_mock.get(endpoint_url)
         spy = mocker.spy(st.api, "request")
 
-        st._get_single_resource(st.api_url, id)
-        spy.assert_called_once_with(url=endpoint, req_type=GET)
+        st._get_single_resource(endpoint, id)
+
+        spy.assert_called_once_with(url=endpoint_url, req_type=GET)
 
 
 class Test_Get_Resources:
+    """Tests for the _get_resources method."""
+
+    @pytest.mark.parametrize("endpoint", ["activities", "agent_assignments", "users/124876"])
     def test_get_resources_makes_request(
-        self, st: SolidarityTech, requests_mock: Mocker, mocker: MockerFixture
+        self, st: SolidarityTech, requests_mock: Mocker, mocker: MockerFixture, endpoint: str
     ) -> None:
-        requests_mock.get(st.api_url)
+        """Make a GET request to varied endpoints."""
+        endpoint_url = f"{st.api_url}/{endpoint}"
+
+        requests_mock.get(endpoint_url)
         spy = mocker.spy(st.api, "request")
-        st._get_resources(st.api_url)
-        spy.assert_called_once_with(url=st.api_url, req_type=GET)
+
+        st._get_resources(endpoint)
+
+        spy.assert_called_once_with(url=endpoint_url, req_type=GET)
 
     def test_get_resources_datetime(
         self,
@@ -73,6 +116,7 @@ class Test_Get_Resources:
         requests_mock: Mocker,
         mocker: MockerFixture,
     ) -> None:
+        """Convert datetime-typed ``since``."""
         now_datetime = datetime.now(tz=timezone.utc)
         now_timestamp = int(now_datetime.timestamp())
 
@@ -95,8 +139,10 @@ class Test_Get_Resources:
         requests_mock: Mocker,
         mocker: MockerFixture,
     ) -> None:
+        """Integrate special query names provided as keyword arguments."""
         requests_mock.get(st.api_url)
         spy = mocker.spy(st.api, "request")
+
         st._get_resources(
             st.api_url,
             limit=123456,
@@ -105,6 +151,7 @@ class Test_Get_Resources:
             since=456321,
             include_count=123654,
         )
+
         spy.assert_called_once_with(
             url=st.api_url,
             req_type=GET,
@@ -122,12 +169,16 @@ class Test_Get_Resources:
         st: SolidarityTech,
         requests_mock: Mocker,
     ) -> None:
+        """Raise a :class:`KeyError` when a query passed in keyword arguments collides with one passed in params."""
         requests_mock.get(st.api_url)
+
         with pytest.raises(KeyError, match="Request param '_limit' already exists"):
             st._get_resources(st.api_url, limit=15, params={"_limit": 30})
 
 
 class Test_Add_If_Field_Not_Empty:
+    """Test the ``_add_if_field_not_empty`` method."""
+
     @pytest.mark.parametrize(
         ("key", "value", "expected"),
         [
@@ -143,11 +194,13 @@ class Test_Add_If_Field_Not_Empty:
         value: str | int | None,
         expected: dict[str, str | int],
     ) -> None:
+        """Add only items with a value."""
         init_dict = {}
         result = st._add_if_field_not_empty(init_dict, key, value)
         assert result == expected
 
     def test_add_if_field_not_empty_overwrite(self, st: SolidarityTech) -> None:
+        """Overwrite existing keys when ``overwrite`` is ``True``."""
         init_dict = {"test_key": "original_value"}
         result = st._add_if_field_not_empty(
             init_dict, "test_key", "overwrite_value", overwrite=True
@@ -155,17 +208,21 @@ class Test_Add_If_Field_Not_Empty:
         assert result["test_key"] == "overwrite_value"
 
     def test_add_if_field_not_empty_no_overwrite_default(self, st: SolidarityTech) -> None:
+        """Don't overwrite existing keys when ``overwrite`` is not provided."""
         init_dict = {"test_key": "original_value"}
         with pytest.raises(KeyError, match="'test_key' already exists"):
             st._add_if_field_not_empty(init_dict, "test_key", "overwrite_value")
 
     def test_add_if_field_not_empty_no_overwrite(self, st: SolidarityTech) -> None:
+        """Raise a :class`KeyError` when ``overwrite`` is ``False`` and the key already exists."""
         init_dict = {"test_key": "original_value"}
         with pytest.raises(KeyError, match="'test_key' already exists"):
             st._add_if_field_not_empty(init_dict, "test_key", "overwrite_value", overwrite=False)
 
 
 class Test_Handle_Status_Codes:
+    """Test the ``_handle_status_codes`` method."""
+
     @pytest.mark.parametrize(
         "status_code",
         [200, 201, 404, 422],
@@ -177,6 +234,13 @@ class Test_Handle_Status_Codes:
         known_status_codes: dict[int, tuple[bool, str]],
         status_code: int,
     ) -> None:
+        """
+        Handle known status codes.
+
+        Raise a :class:`STFailedResponseError` if parsing a known failure status code,
+        return ``True`` if parsing a known success status code.
+
+        """
         requests_mock.get("https://api.example.com", status_code=status_code)
         res = requests.get("https://api.example.com")
 
@@ -197,6 +261,7 @@ class Test_Handle_Status_Codes:
         requests_mock: Mocker,
         known_status_codes: dict[int, tuple[bool, str]],
     ) -> None:
+        """Raise a :class:`STUnexpectedResponseError` if parsing an unrecognized status code."""
         status_code = 500
 
         requests_mock.get("https://api.example.com", status_code=status_code)
--- test/test_solidarity-tech/test_st_init.py
+++ test/test_solidarity-tech/test_st_init.py
@@ -1,21 +1,29 @@
+from __future__ import annotations
+
 import os
+from typing import TYPE_CHECKING
 
 import pytest
-from pytest_mock import MockerFixture
 
 from parsons.solidarity_tech import SolidarityTech
+
+if TYPE_CHECKING:
+    from pytest_mock import MockerFixture
+
 
 TOKEN_ENV_NAME = "SOLIDARITY_TECH_BEARER_KEY"
 TOKEN_PLACEHOLDER = "SOME_BEARER_KEY"
 
 
 def test_init_with_arg() -> None:
+    """Set api_token property and header when initialized via an argument."""
     st = SolidarityTech(api_token=TOKEN_PLACEHOLDER)
     assert st.api_token == TOKEN_PLACEHOLDER
     assert st.headers.get("authorization") == f"Bearer {TOKEN_PLACEHOLDER}"
 
 
 def test_init_with_env(mocker: MockerFixture) -> None:
+    """Set api_token property and header when initialized via environment variable."""
     mocker.patch.dict(os.environ, {TOKEN_ENV_NAME: TOKEN_PLACEHOLDER})
     st = SolidarityTech()
     assert st.api_token == TOKEN_PLACEHOLDER
@@ -23,9 +31,11 @@ def test_init_with_env(mocker: MockerFixture) -> None:
 
 
 def test_init_with_no_api_token() -> None:
+    """Raise :class:`KeyError` when no API token is provided and the environment variable is not set."""
     with pytest.raises(KeyError, match=f"No '{TOKEN_ENV_NAME}' found."):
         SolidarityTech()
 
 
 def test_init_api_url(st: SolidarityTech) -> None:
+    """Set api_url property."""
     assert st.api_url == "https://api.solidarity.tech/v1"

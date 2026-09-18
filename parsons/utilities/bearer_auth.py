47f8ad935290bb50654cd3787e54e69db86a8d9a
Ramona T <ramona.t.me@pm.me>
2026-09-18 17:52:18 -0400

    Allow header configuration

--- parsons/utilities/bearer_auth.py
+++ parsons/utilities/bearer_auth.py
@@ -28,34 +28,79 @@ class BearerAuth(AuthBase):
     """
 
     api_key: str
+    token_divider: str | None
+    header_name: str
+    token_name: str | None
 
     def __init__(
         self,
         api_key: str,
+        *,
+        header_name: str = "Authorization",
+        token_name: str | None = "Bearer",
+        token_divider: str | None = " ",
     ) -> None:
         """
         Initialize handler with the API key.
 
         Args:
             api_key: The API key to use for authentication.
+            header_name:
+                Header name to use in authentication header key.
+                Defaults to "Authorization".
+            token_name:
+                Token name to use in authentication header value, before `api_key`.
+                Defaults to "Bearer". Can be set to ``None`` to omit the token name and use `api_key` directly.
+            token_divider:
+                Divider to use in authentication header value, between `token_name` and `api_key`.
+                Defaults to " ". If `token_name` is ``None``, this is ignored.
 
         """
         self.api_key = api_key.strip()
+        self.header_name = header_name
+        self.token_name = token_name
+        self.token_divider = token_divider
 
     def __eq__(self, other: object) -> bool:
         """Check if two instances have the same API key."""
-        return self.api_key == getattr(other, "api_key", None)
+        same_api_key = self.api_key == getattr(other, "api_key", None)
+        same_header_name = self.header_name == getattr(other, "header_name", None)
+        same_token_name = self.token_name == getattr(other, "token_name", None)
+        same_token_divider = self.token_divider == getattr(other, "token_divider", None)
+
+        return same_api_key and same_header_name and same_token_name and same_token_divider
 
     def __hash__(self) -> int:
         """Ensure that two instsances with the same configuration have the same hash."""
-        return hash(self.api_key)
+        return hash(
+            self.api_key + self.header_name + str(self.token_name) + str(self.token_divider)
+        )
 
     def __repr__(self) -> str:
         """Return a string representation of the instance."""
-        return f"<BearerAuth api_key={self.api_key}>"
+        representation = f"api_key={self.api_key}"
+        representation += f" header_name={self.header_name}"
+        if self.token_name:
+            representation += f" token_name={self.token_name}"
+        if self.token_divider:
+            representation += f" token_divider={self.token_divider}"
+
+        return f"<BearerAuth {representation}>"
 
     def __call__(self, r: requests.PreparedRequest) -> requests.PreparedRequest:
-        """Add authorization header to the supplied request."""
-        r.headers["Authorization"] = f"Bearer {self.api_key}"
+        """
+        Add authorization header to the supplied request.
+
+        Header key is determined by `self.header_name`.
+        Header value is determined by `self.token_name` and `self.api_key`, separated by `self.token_divider`.
+        If `self.token_name` is not available, `self.api_key` is used as the header value and `self.token_divider` is not used.
+
+        """
+        header_value = (
+            f"{self.token_name}{self.token_divider}{self.api_key}"
+            if self.token_name
+            else self.api_key
+        )
+        r.headers[self.header_name] = header_value
 
         return r
